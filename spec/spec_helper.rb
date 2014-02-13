d7f8f957f8120a7e276948d33f422954e687ee0c
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-02-13 14:06:06 +0000

    Update test load paths.
    Allows running outside of a loader such as Zeus.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..')
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
