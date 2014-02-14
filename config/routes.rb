cf66c6f1a5d777b322d1da414232fccc4f73d34b
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-02-14 09:58:38 +0000

    Add redirect from /

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 SpecialistPublisher::Application.routes.draw do
   resources :specialist_documents, except: [:show], path: 'specialist-documents'
+
+  root to: redirect('/specialist-documents')
 end
