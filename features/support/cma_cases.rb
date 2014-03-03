b338d6385fb301f8cb45f41c08362d650867f171
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2014-03-03 14:08:06 +0000

    Use parens on RSpec matchers

--- features/support/cma_cases.rb
+++ features/support/cma_cases.rb
@@ -59,7 +59,7 @@ def check_for_cma_cases(*titles)
 end
 
 def check_currently_on_publisher_index_page
-  current_path.should eq specialist_documents_path
+  current_path.should eq(specialist_documents_path)
 end
 
 def go_to_edit_page_for_most_recent_case
