4428b233412db3b7cc4e0c459d8ba2ae8b83eb5b
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-19 13:12:33 +0000

    Refactor to make code clearer

--- app/lib/finder_schema.rb
+++ app/lib/finder_schema.rb
@@ -12,12 +12,7 @@ class FinderSchema
   end
 
   def options_for(facet_name)
-    facet_data_for(facet_name).fetch("allowed_values", []).map do |value|
-      [
-        value.fetch("label", ""),
-        value.fetch("value", "")
-      ]
-    end
+    allowed_values_as_option_tuples(allowed_values_for(facet_name))
   end
 
 private
@@ -29,5 +24,18 @@ private
     schema.fetch("facets", []).find do |facet_record|
       facet_record.fetch("key") == facet_name.to_s
     end || {}
+  end
+
+  def allowed_values_for(facet_name)
+    facet_data_for(facet_name).fetch("allowed_values", [])
+  end
+
+  def allowed_values_as_option_tuples(allowed_values)
+    allowed_values.map do |value|
+      [
+        value.fetch("label", ""),
+        value.fetch("value", "")
+      ]
+    end
   end
 end
