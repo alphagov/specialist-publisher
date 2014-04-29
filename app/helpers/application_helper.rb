ff0b00a4ace9dbd6480c79d670e7c88cb9798d73
Dominic Baggott <dominic.baggott@gmail.com>
2014-04-29 11:27:03 +0100

    Show withdrawn state on documents index
    Extract the state label logic to a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def facet_options(facet)
     finder_schema.options_for(facet)
   end
+
+  def document_state(document)
+    state = document.publication_state
+
+    if %w(published withdrawn).include?(state) && document.draft?
+      state << ' with new draft'
+    end
+
+    state
+  end
 end
