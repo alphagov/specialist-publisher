5a0b3c55c98b5b994e6d83fc0678d02b4f202d15
Dominic Baggott <dominic.baggott@gmail.com>
2014-06-02 11:02:25 +0100

    [WIP] Work around GdsApi::Response#fetch failing
    This would be better solved by making GdsApi::Response respond to #fetch
    correctly and then bumping the version of that gem in this app.

--- app/lib/panopticon_registerer.rb
+++ app/lib/panopticon_registerer.rb
@@ -32,7 +32,7 @@ class PanopticonRegisterer
       resource_id: artefact.resource_id,
       resource_type: artefact.kind,
       slug: artefact.slug,
-      panopticon_id: response.fetch("id"),
+      panopticon_id: response['id'],
     )
   end
 
