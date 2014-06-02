570a07051f2c627b64dcf5cb93016447fa094d0a
Stephen Best <bestie@gmail.com>
2014-06-02 15:10:20 +0100

    Clarify repository comment

--- app/repositories/specialist_document_repository.rb
+++ app/repositories/specialist_document_repository.rb
@@ -44,9 +44,10 @@ class SpecialistDocumentRepository
   end
 
   def store(document)
-    # This actually isn't necessary because only the latest edition ever changes
-    # I think it's safer to perform the save anyway as there is potential for
-    # the previous editions to change
+    # It is actually only necessary to save the latest edition, however, I
+    # think it's safer to save latest two as both are exposed to the and have
+    # potential to change. This extra write may save a potential future
+    # headache.
     document.editions.last(2).each(&:save!)
 
     self
