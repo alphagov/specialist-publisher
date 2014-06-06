bc70d691395fcba7f50f6d5f2bf634a8ce1e56f2
Stephen Best <bestie@gmail.com>
2014-06-06 14:31:39 +0100

    Rubocop Autocorrect: TrailingWhitespace TrailingBlankLines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ SpecialistPublisher::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
     # Enable JSON-style logging
   config.logstasher.enabled = true
   config.logstasher.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.json.log")
