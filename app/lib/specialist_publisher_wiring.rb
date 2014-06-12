e2f042af51eec4452a249565e429c274ca42fdab
Dominic Baggott <dominic.baggott@gmail.com>
2014-06-12 11:10:18 +0100

    Remove unused wiring

--- app/lib/specialist_publisher_wiring.rb
+++ app/lib/specialist_publisher_wiring.rb
@@ -82,7 +82,6 @@ SpecialistPublisherWiring = DependencyContainer.new do
     }
   }
 
-  define_instance(:artefacts) { Artefact }
   define_singleton(:panopticon_api) do
     GdsApi::Panopticon.new(Plek.current.find("panopticon"), PANOPTICON_API_CREDENTIALS)
   end
@@ -173,7 +172,6 @@ SpecialistPublisherWiring = DependencyContainer.new do
   define_singleton(:id_generator) { SecureRandom.method(:uuid) }
 
   define_singleton(:edition_factory) { SpecialistDocumentEdition.method(:new) }
-  define_singleton(:attachment_factory) { Attachment.method(:new) }
 
   define_factory(:cma_case_builder) {
     CmaCaseBuilder.new(
