85e8275b228db4b5556c8ac79f4f9ebf7f9768b6
Dominic Baggott <dominic.baggott@gmail.com>
2014-07-11 20:11:07 +0100

    Extract AbstractDocumentServiceRegistry
    CmaCaseServiceRegistry and AaibReportServiceRegistry do the same thing
    with variation for just three dependencies -- repository, builder and
    observers.

--- app/services/aaib_report_service_registry.rb
+++ app/services/aaib_report_service_registry.rb
@@ -1,78 +1,16 @@
+require "abstract_document_service_registry"
 require "specialist_publisher_wiring"
-require "list_documents_service"
-require "show_document_service"
-require "new_document_service"
-require "preview_document_service"
-require "create_document_service"
-require "update_document_service"
-require "publish_document_service"
-require "withdraw_document_service"
 
-class AaibReportServiceRegistry
-  def list
-    ListDocumentsService.new(
-      aaib_report_repository,
-    )
-  end
-
-  def show(document_id)
-    ShowDocumentService.new(
-      aaib_report_repository,
-      document_id,
-    )
-  end
-
-  def new
-    NewDocumentService.new(
-      aaib_report_builder,
-    )
-  end
-
-  def preview(document_id, attributes)
-    PreviewDocumentService.new(
-      aaib_report_repository,
-      aaib_report_builder,
-      document_renderer,
-      document_id,
-      attributes,
-    )
-  end
-
-  def create(attributes)
-    CreateDocumentService.new(
-      aaib_report_builder,
-      aaib_report_repository,
-      observers.creation,
-      attributes,
-    )
-  end
-
-  def update(document_id, attributes)
-    UpdateDocumentService.new(
-      repo: aaib_report_repository,
-      listeners: observers.update,
-      document_id: document_id,
-      attributes: attributes,
-    )
-  end
-
-  def publish(document_id)
-    PublishDocumentService.new(
-      aaib_report_repository,
-      observers.publication,
-      document_id,
-    )
-  end
-
-  def withdraw(document_id)
-    WithdrawDocumentService.new(
-      aaib_report_repository,
-      observers.withdrawal,
-      document_id,
-    )
-  end
-
+class AaibReportServiceRegistry < AbstractDocumentServiceRegistry
 private
+  def document_repository
+    SpecialistPublisherWiring.get(:aaib_report_repository)
+  end
+
+  def document_builder
+    SpecialistPublisherWiring.get(:aaib_report_builder)
+  end
+
   def observers
     all_observers = SpecialistPublisherWiring.get(:observers)
 
@@ -82,17 +20,5 @@ private
       publication: all_observers.aaib_report_publication,
       withdrawal: all_observers.aaib_report_withdrawal,
     )
-  end
-
-  def aaib_report_repository
-    SpecialistPublisherWiring.get(:aaib_report_repository)
-  end
-
-  def aaib_report_builder
-    SpecialistPublisherWiring.get(:aaib_report_builder)
-  end
-
-  def document_renderer
-    SpecialistPublisherWiring.get(:specialist_document_renderer)
   end
 end
---
+++ app/services/abstract_document_service_registry.rb
@@ -0,0 +1,91 @@
+require "specialist_publisher_wiring"
+require "list_documents_service"
+require "show_document_service"
+require "new_document_service"
+require "preview_document_service"
+require "create_document_service"
+require "update_document_service"
+require "publish_document_service"
+require "withdraw_document_service"
+
+class AbstractDocumentServiceRegistry
+  def list
+    ListDocumentsService.new(
+      document_repository,
+    )
+  end
+
+  def show(document_id)
+    ShowDocumentService.new(
+      document_repository,
+      document_id,
+    )
+  end
+
+  def new
+    NewDocumentService.new(
+      document_builder,
+    )
+  end
+
+  def preview(document_id, attributes)
+    PreviewDocumentService.new(
+      document_repository,
+      document_builder,
+      document_renderer,
+      document_id,
+      attributes,
+    )
+  end
+
+  def create(attributes)
+    CreateDocumentService.new(
+      document_builder,
+      document_repository,
+      observers.creation,
+      attributes,
+    )
+  end
+
+  def update(document_id, attributes)
+    UpdateDocumentService.new(
+      repo: document_repository,
+      listeners: observers.update,
+      document_id: document_id,
+      attributes: attributes,
+    )
+  end
+
+  def publish(document_id)
+    PublishDocumentService.new(
+      document_repository,
+      observers.publication,
+      document_id,
+    )
+  end
+
+  def withdraw(document_id)
+    WithdrawDocumentService.new(
+      document_repository,
+      observers.withdrawal,
+      document_id,
+    )
+  end
+
+private
+  def document_renderer
+    SpecialistPublisherWiring.get(:specialist_document_renderer)
+  end
+
+  def document_repository
+    raise NotImplementedError
+  end
+
+  def document_builder
+    raise NotImplementedError
+  end
+
+  def observers
+    raise NotImplementedError
+  end
+end
