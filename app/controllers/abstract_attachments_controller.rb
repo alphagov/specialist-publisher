ec9efe6b3bf8b9220d9b6266b7afe0ce822508f0
Andrew Mitchell <themitchell@me.com>
2014-07-23 13:56:16 +0100

    Abstract common attachment controller code
    The only thing that is now not common in the controller code is the redirect path. Once we move this into a 'redirect_path' helper we can use the same code across the board for all attachments

--- app/controllers/abstract_attachments_controller.rb
+++ app/controllers/abstract_attachments_controller.rb
@@ -1,5 +1,43 @@
 class AbstractAttachmentsController < ApplicationController
 
+  def new
+    document, attachment = attachment_services.new_attachment(document_id).call
+
+    render("attachments/new", locals: {
+      document: view_adapter(document),
+      attachment: attachment,
+    })
+  end
+
+  def create
+    document, attachment = attachment_services.create_attachment(self, document_id).call
+
+    redirect_to(redirect_path(document))
+  end
+
+  def edit
+    # TODO: action not tested
+    document, attachment = attachment_services.show_attachment(self, document_id).call
+
+    render("attachments/edit", locals: {
+      document: view_adapter(document),
+      attachment: attachment,
+    })
+  end
+
+  def update
+    document, attachment = attachment_services.update_attachment(self, document_id).call
+
+    if attachment.persisted?
+      redirect_to(redirect_path(document))
+    else
+      render("attachments/edit", locals: {
+        document: view_adapter(document),
+        attachment: attachment,
+      })
+    end
+  end
+
 protected
   def view_adapter(document)
     raise NotImplementedError
@@ -13,4 +51,7 @@ protected
     raise NotImplementedError
   end
 
+  def redirect_path(document)
+    root_path
+  end
 end
--- app/controllers/cma_case_attachments_controller.rb
+++ app/controllers/cma_case_attachments_controller.rb
@@ -1,41 +1,4 @@
 class CmaCaseAttachmentsController < AbstractAttachmentsController
-  def new
-    document, attachment = attachment_services.new_attachment(document_id).call
-
-    render("attachments/new", locals: {
-      document: view_adapter(document),
-      attachment: attachment,
-    })
-  end
-
-  def create
-    document, attachment = attachment_services.create_attachment(self, document_id).call
-
-    redirect_to edit_cma_case_path(document)
-  end
-
-  def edit
-    # TODO: action not tested
-    document, attachment = attachment_services.show_attachment(self, document_id).call
-
-    render("attachments/edit", locals: {
-      document: view_adapter(document),
-      attachment: attachment,
-    })
-  end
-
-  def update
-    document, attachment = attachment_services.update_attachment(self, document_id).call
-
-    if attachment.persisted?
-      redirect_to(edit_cma_case_path(document))
-    else
-      render("attachments/edit", locals: {
-        document: view_adapter(document),
-        attachment: attachment,
-      })
-    end
-  end
 
 protected
   def view_adapter(document)
@@ -48,5 +11,9 @@ protected
 
   def attachment_services
     SpecialistPublisherWiring.get(:cma_case_attachment_services)
+  end
+
+  def redirect_path(document)
+    edit_cma_case_path(document)
   end
 end
