a9b8d1c1450d762ca471364aa14fce397eaf18ce
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-08-13 10:20:42 +0100

    Add publish and withdraw permission check

--- app/lib/permission_checker.rb
+++ app/lib/permission_checker.rb
@@ -1,12 +1,21 @@
 class PermissionChecker
   GDS_EDITOR_PERMISSION = "gds_editor"
+  EDITOR_PERMISSION = "editor"
 
   def initialize(user)
     @user = user
   end
 
   def can_edit?(format)
-    is_gds_editor? || user_organisation_owns_format?(format)
+    is_gds_editor? || can_access_format?(format)
+  end
+
+  def can_publish?(format)
+    is_gds_editor? || is_editor? && can_access_format?(format)
+  end
+
+  def can_withdraw?(format)
+    can_publish?(format)
   end
 
 private
@@ -14,6 +23,14 @@ private
 
   def is_gds_editor?
     user.has_permission?(GDS_EDITOR_PERMISSION)
+  end
+
+  def is_editor?
+    user.has_permission?(EDITOR_PERMISSION)
+  end
+
+  def can_access_format?(format)
+    format == "manual" || user_organisation_owns_format?(format)
   end
 
   def user_organisation_owns_format?(format)
