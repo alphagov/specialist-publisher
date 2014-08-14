ef6021fa07b3c0f2e0bbf6f3ab11ec905ebf0e37
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-08-14 16:45:04 +0100

    Add observers and services for DSU

---
+++ app/observers/drug_safety_update_observers_registry.rb
@@ -0,0 +1,56 @@
+class DrugSafetyUpdateObserversRegistry
+  def publication
+    [
+      content_api_exporter,
+      finder_api_exporter,
+      panopticon_exporter,
+      rummager_exporter,
+    ]
+  end
+
+  def update
+    []
+  end
+
+  def creation
+    []
+  end
+
+  def withdrawal
+    [
+      content_api_withdrawer,
+      finder_api_withdrawer,
+      panopticon_exporter,
+      rummager_withdrawer,
+    ]
+  end
+
+private
+  def content_api_exporter
+    SpecialistPublisherWiring.get(:drug_safety_update_content_api_exporter)
+  end
+
+  def panopticon_exporter
+    SpecialistPublisherWiring.get(:drug_safety_update_panopticon_registerer)
+  end
+
+  def rummager_withdrawer
+    SpecialistPublisherWiring.get(:drug_safety_update_rummager_deleter)
+  end
+
+  def rummager_exporter
+    SpecialistPublisherWiring.get(:drug_safety_update_rummager_indexer)
+  end
+
+  def finder_api_exporter
+    SpecialistPublisherWiring.get(:finder_api_notifier)
+  end
+
+  def finder_api_withdrawer
+    SpecialistPublisherWiring.get(:finder_api_withdrawer)
+  end
+
+  def content_api_withdrawer
+    SpecialistPublisherWiring.get(:specialist_document_content_api_withdrawer)
+  end
+end
