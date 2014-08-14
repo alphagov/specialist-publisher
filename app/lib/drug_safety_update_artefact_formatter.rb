ee5229222f3ff3e30f9755b612f64c902fe15c61
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-08-14 16:39:52 +0100

    Add formatters for DSU

---
+++ app/lib/drug_safety_update_artefact_formatter.rb
@@ -0,0 +1,20 @@
+require "abstract_artefact_formatter"
+
+class DrugSafetyUpdateArtefactFormatter < AbstractArtefactFormatter
+
+  def state
+    state_mapping.fetch(entity.publication_state)
+  end
+
+  def kind
+    "drug_safety_update"
+  end
+
+  def rendering_app
+    "specialist-frontend"
+  end
+
+  def organisation_slugs
+    ["medicines-and-healthcare-products-regulatory-agency"]
+  end
+end
---
+++ app/lib/drug_safety_update_indexable_formatter.rb
@@ -0,0 +1,18 @@
+require "abstract_indexable_formatter"
+
+class DrugSafetyUpdateIndexableFormatter < AbstractIndexableFormatter
+  def type
+    "drug_safety_update"
+  end
+
+private
+  def extra_attributes
+    {
+      therapeutic_area: entity.therapeutic_area
+    }
+  end
+
+  def organisation_slugs
+    ["medicines-and-healthcare-products-regulatory-agency"]
+  end
+end
