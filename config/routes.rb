d6fb6880ac36de46c1d6fca5d83f0022d7e47e3c
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-08-14 16:05:57 +0100

    DRY up routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,45 +6,31 @@ SpecialistPublisher::Application.routes.draw do
     mount Sidekiq::Web => "/sidekiq"
   end
 
-  resources :cma_cases, except: :destroy, path: "cma-cases" do
-    resources :attachments, controller: :cma_case_attachments, only: [:new, :create, :edit, :update]
-    post :withdraw, on: :member
-    post :publish, on: :member
+  document_types = %w(
+    aaib_reports
+    cma_cases
+    international_development_funds
+  )
 
-    # This is for persisted cma cases
-    post :preview, on: :member
+  document_types.each do |type|
+    type_slug = type.to_s.gsub("_", "-")
+
+    resources type.to_sym, except: :destroy, path: type_slug do
+      resources :attachments, controller: "#{type.singularize}_attachments", only: [:new, :create, :edit, :update]
+      post :withdraw, on: :member
+      post :publish, on: :member
+
+      # This is for persisted documents
+      post :preview, on: :member
+    end
+
+    # This is for new documents
+    post "#{type_slug}/preview" => "#{type}#preview", as: "preview_new_#{type}"
   end
-
-  # This is for new cma cases
-  post "cma-cases/preview" => "cma_cases#preview", as: "preview_new_cma_cases"
 
   # Redirect old specialist-document routes to cma-cases
   get "/specialist-documents", to: redirect("/cma-cases")
   get "/specialist-documents/(*path)", to: redirect { |params, _| "/cma-cases/#{params[:path]}" }
-
-  resources :aaib_reports, except: :destroy, path: "aaib-reports" do
-    resources :attachments, controller: :aaib_report_attachments, only: [:new, :create, :edit, :update]
-    post :withdraw, on: :member
-    post :publish, on: :member
-
-    # This is for persisted aaib reports
-    post :preview, on: :member
-  end
-
-  # This is for new aaib reports
-  post "aaib-reports/preview" => "aaib_reports#preview", as: "preview_new_aaib_reports"
-
-  resources :international_development_funds, except: :destroy, path: "international-development-funds" do
-    resources :attachments, controller: :international_development_fund_attachments, only: [:new, :create, :edit, :update]
-    post :withdraw, on: :member
-    post :publish, on: :member
-
-    # This is for persisted international development funds
-    post :preview, on: :member
-  end
-
-  # This is for new international development funds
-  post "international-development-funds/preview" => "international_development_funds#preview", as: "preview_new_international_development_funds"
 
   resources :manuals, except: :destroy do
     resources :documents, except: :destroy, path: "sections", controller: "ManualDocuments" do
