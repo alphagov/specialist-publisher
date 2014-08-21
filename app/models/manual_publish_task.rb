95ed8581809c3d0eeb091360bb301732cda03ce1
Stephen Best <bestie@gmail.com>
2014-08-21 15:18:47 +0100

    Publish task ordering implemented incorrectly
    Everyone be good and read your mongoid docs :)

--- app/models/manual_publish_task.rb
+++ app/models/manual_publish_task.rb
@@ -10,7 +10,7 @@ class ManualPublishTask
   field :error, type: String
 
   scope :for_manual, ->(manual) { where(manual_id: manual.id) }
-  scope :by_newest_version_number, order_by("version_number DESC")
+  scope :by_newest_version_number, order_by([:version_number, :desc])
 
   state_machine initial: :queued do
     event :start! do
