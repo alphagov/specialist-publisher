51fc78646a78b14b73fff094f83235ef7c4c1ca8
Andrew Mitchell <themitchell@me.com>
2014-09-04 14:54:08 +0100

    Update SafeHtmlValidator to match content_models
    Updated the validator to match the SafeHtml validator used in govuk_content_models this should be pushed to govuk_content_models in order to remove the duplication although it will create a breaking change. The main difference here is that the version in govuk_content_models validates all fields on an object no simply a specified field. This works much more like a railsesque validator and only validates a single field.

--- features/support/document_helpers.rb
+++ features/support/document_helpers.rb
@@ -234,7 +234,7 @@ module DocumentHelpers
   end
 
   def check_for_javascript_usage_error(field)
-    expect(page).to have_content("#{field} cannot include invalid HTML or JavaScript")
+    expect(page).to have_content("#{field} cannot include invalid Govspeak, invalid HTML, any JavaScript or images hosted on sites except for")
   end
 
 end
