d4c03f4243dd7acda052691ed8f32741835b1fed
Andrew Mitchell <themitchell@me.com>
2014-09-18 11:13:58 +0100

    Add EmailAlertExporter to publication observers
    Wire up the EmailAlertExporter in the publication observers chain for SpecialistDocuments. This should be the last thing in the chain so that we only notify users of a new SpecialistDocument publication once it is fully available on the frontend.

--- app/observers/abstract_specialist_document_observers_registry.rb
+++ app/observers/abstract_specialist_document_observers_registry.rb
@@ -1,3 +1,6 @@
+require "email_alert_exporter"
+require "formatters/specialist_document_publication_alert_formatter"
+
 class AbstractSpecialistDocumentObserversRegistry
   def creation
     []
@@ -12,6 +15,7 @@ class AbstractSpecialistDocumentObserversRegistry
       content_api_exporter,
       panopticon_exporter,
       rummager_exporter,
+      document_publication_alert_exporter,
     ]
   end
 
@@ -42,5 +46,18 @@ private
 
   def content_api_withdrawer
     raise NotImplementedError
+  end
+
+  def delivery_api
+    SpecialistPublisherWiring.get(:delivery_api)
+  end
+
+  def document_publication_alert_exporter
+    ->(document) {
+      EmailAlertExporter.new(
+        delivery_api: delivery_api,
+        formatter: SpecialistDocumentPublicationAlertFormatter.new(document),
+      ).call
+    }
   end
 end
