458eb6fa447e20cafbe68600ab4eaa2c563b1a9e
Tom Russell <tom@cishub.co.uk>
2014-10-22 11:25:11 +0100

    Add cucumber tests for basic search functionality
    Some of our document formats (AAIB and Drug Safety Updates, for
    example) have many documents, and our publishing tool doesn't easily
    allow users to find a document to amend other than by paging through the
    results.
    
    We should allow users to search by either slug or title using a form on
    the list page.

---
+++ features/step_definitions/searching_documents_steps.rb
@@ -0,0 +1,68 @@
+Given(/^multiple draft AAIB reports exist$/) do
+  @aaib_reports_data = [
+    {title: "First AAIB report", slug: "aaib-reports/first-aaib-report"},
+    {title: "Second AAIB report", slug: "aaib-reports/second-aaib-report"},
+    {title: "Third AAIB report", slug: "aaib-reports/third-aaib-report"},
+  ]
+
+  titles = @aaib_reports_data.map { |r| r[:title] }
+
+  create_multiple_aaib_reports(titles)
+end
+
+When(/^I search for an exact slug$/) do
+  visit aaib_reports_path
+
+  @searching_for = @aaib_reports_data.first
+
+  fill_in "Search term", with: @searching_for[:slug]
+  choose "Slug"
+  click_on "Search"
+end
+
+When(/^I search for a partial slug$/) do
+  visit aaib_reports_path
+
+  @searching_for = @aaib_reports_data.first
+
+  # 0..18 => "aaib-reports/first-"
+  fill_in "Search term", with: @searching_for[:slug][0..18]
+  choose "Slug"
+  click_on "Search"
+end
+
+Then(/^I see the matching AAIB reports in the list$/) do
+  aaib_report_is_visible(@searching_for[:title])
+
+  non_matching_titles = (@aaib_reports_data - [@searching_for]).map { |r| r[:title] }
+
+  aaib_reports_are_not_visible(non_matching_titles)
+end
+
+When(/^I search for a partial title$/) do
+  visit aaib_reports_path
+
+  @searching_for = @aaib_reports_data.last
+
+  # 0..4 => "Third"
+  fill_in "Search term", with: @searching_for[:title][0..4]
+  choose "Title"
+  click_on "Search"
+end
+
+Given(/^a search has been performed$/) do
+  visit aaib_reports_path
+
+  fill_in "Search term", with: "Any data"
+  choose "Title"
+  click_on "Search"
+end
+
+When(/^I clear the search term field$/) do
+  fill_in "Search term", with: ""
+  click_on "Search"
+end
+
+Then(/^I see all AAIB reports in the list$/) do
+  aaib_reports_are_visible(@aaib_reports_data.map { |r| r[:title] })
+end
