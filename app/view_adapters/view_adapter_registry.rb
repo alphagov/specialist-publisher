d8b81ce8f6ff44a3c0ac7fa865325034e34ebef6
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-10-24 15:52:06 +0100

    Adding views for MAIB Reports

--- app/view_adapters/view_adapter_registry.rb
+++ app/view_adapters/view_adapter_registry.rb
@@ -11,6 +11,7 @@ class ViewAdapterRegistry
       "cma_case" => CmaCaseViewAdapter,
       "drug_safety_update" => DrugSafetyUpdateViewAdapter,
       "international_development_fund" => InternationalDevelopmentFundViewAdapter,
+      "maib_report" => MaibReportViewAdapter,
       "medical_safety_alert" => MedicalSafetyAlertViewAdapter,
     }.fetch(type)
   end
