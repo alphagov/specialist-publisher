04d241a7a922f0f178af4bdb2908e7faee3b626c
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-10-27 11:23:40 +0000

    Remove object-specific builders for parameter-specific builders
    Many of the builders that inherit from `SpecialistDocumentBuilder`
    tend to do the same thing which can be summarised as the following bit
    of code:
    
    ```
    def call(attrs)
      super(attrs.merge(document_type: "some_document_type_label"))
    end
    ```
    
    They're adding a `document_type` key to the `attrs` hash we pass in.
    
    Rather than create a number of objects for the same task, instead
    provide the difference as a parameter so that we can better understand
    not only what's different about our builders, but also what's the
    same.

--- spec/models/builders/specialist_document_builder_spec.rb
+++ spec/models/builders/specialist_document_builder_spec.rb
@@ -2,18 +2,17 @@ require "spec_helper"
 
 describe SpecialistDocumentBuilder do
   subject(:builder) {
-    SpecialistDocumentBuilder.new(document_factory)
+    SpecialistDocumentBuilder.new(document_type, document_factory)
   }
 
+  let(:document_type)     { "dummy_document" }
   let(:document_factory)  { double(:document_factory, call: document) }
-  let(:document_id)       { double(:document_id) }
-  let(:attrs)             { double(:attrs) }
   let(:document)          { double(:document, update: nil) }
 
   describe "#call" do
     it "creates a new document" do
-      expect(builder.call(attrs)).to eq(document)
-      expect(document).to have_received(:update).with(attrs)
+      expect(builder.call({})).to eq(document)
+      expect(document).to have_received(:update).with({document_type: document_type})
     end
   end
 end
