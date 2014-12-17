d3c7922380f17e4f6d81e538dc7082c5215363d6
Tom Russell <tom@cishub.co.uk>
2014-12-17 11:02:38 +0000

    Do not send email alerts for Drug Safety Update
    The Drug Safety Update is a monthly newsletter sent by the MHRA with custom
    content (the full content of the document body). Therefore we're going to allow
    the MHRA to send this out manually rather than alerting the user each time a
    new one is published.

--- features/step_definitions/email_alert_steps.rb
+++ features/step_definitions/email_alert_steps.rb
@@ -1,3 +1,7 @@
 Then(/^a publication notification should have been sent$/) do
   check_email_alert_api_notified_of_publish
 end
+
+Then(/^a publication notification should not have been sent$/) do
+  check_email_alert_api_is_not_notified_of_publish
+end
