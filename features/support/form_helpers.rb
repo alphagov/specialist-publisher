7d04ebcab699b434becd88f5c3495d88a3ff77b9
Dominic Baggott <dominic.baggott@gmail.com>
2015-04-01 17:52:17 +0100

    Extract fill_in_field method
    Make the existing `fill_in_fields` method responsible only for iterating
    through a hash of names and values.

--- features/support/form_helpers.rb
+++ features/support/form_helpers.rb
@@ -1,15 +1,19 @@
 module FormHelpers
-  def fill_in_fields(field_names)
-    field_names.each do |field_name, value|
-      human_field_name = field_name.to_s.humanize
-      tag_name = page.find_field(field_name.to_s.humanize).tag_name
+  def fill_in_fields(names_and_values)
+    names_and_values.each do |field_name, value|
+      fill_in_field(field_name, value)
+    end
+  end
 
-      case tag_name
-      when "select"
-        select value, from: human_field_name
-      else
-        fill_in human_field_name, with: value
-      end
+  def fill_in_field(field_name, value)
+    human_field_name = field_name.to_s.humanize
+    tag_name = page.find_field(field_name.to_s.humanize).tag_name
+
+    case tag_name
+    when "select"
+      select value, from: human_field_name
+    else
+      fill_in human_field_name, with: value
     end
   end
 end
