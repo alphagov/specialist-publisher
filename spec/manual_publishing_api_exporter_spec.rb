eed4c20c24598c10aaf0b5cedafef22c67529320
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-05-11 13:44:16 +0000

    Send manual_id for content_id to publishing-api
    In theory, every content item in the publishing-api/content-store should have a
    content_id so that it can be linked to. In practice, it isn't required,
    although the format of it is validated when it is sent.

--- spec/manual_publishing_api_exporter_spec.rb
+++ spec/manual_publishing_api_exporter_spec.rb
@@ -26,6 +26,7 @@ describe ManualPublishingAPIExporter do
   let(:manual) {
     double(
       :manual,
+      id: "52ab9439-95c8-4d39-9b83-0a2050a0978b",
       attributes: manual_attributes,
       documents: documents,
     )
@@ -104,6 +105,7 @@ describe ManualPublishingAPIExporter do
     expect(export_recipent).to have_received(:put_content_item).with(
       "/guidance/my-first-manual",
       hash_including(
+        content_id: "52ab9439-95c8-4d39-9b83-0a2050a0978b",
         format: "manual",
         title: "My first manual",
         description: "This is my first manual",
