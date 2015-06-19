828a13d6534f7e6e0d1db4bd6c90a59aa1b8e2cc
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-06-19 19:19:10 +0100

    Give ManualDocumentServiceRegistry its own #show
    We're using the ShowManualDocumentService inside controllers and from a bin
    script, so it might be best to have a separate service setup for each.

--- app/services/manual_document_service_registry.rb
+++ app/services/manual_document_service_registry.rb
@@ -1,6 +1,14 @@
+require "show_manual_document_service"
 require "remove_manual_document_service"
 
 class ManualDocumentServiceRegistry
+  def show(context)
+    ShowManualDocumentService.new(
+      manual_repository,
+      context,
+    )
+  end
+
   def remove(context)
     RemoveManualDocumentService.new(
       manual_repository,
