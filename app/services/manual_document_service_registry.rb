0544ff3f2c5ab9ac8963c35493cac34ed20549e6
Dominic Baggott <dominic.baggott@gmail.com>
2015-06-26 10:58:59 +0100

    Restore ListManualDocumentsService
    This is used by the manual_documents_controller#reorder method and
    shouldn't have been deleted.
    
    This highlights the need for tests around reordering of sections.

--- app/services/manual_document_service_registry.rb
+++ app/services/manual_document_service_registry.rb
@@ -3,6 +3,7 @@ require "create_manual_document_service"
 require "update_manual_document_service"
 require "show_manual_document_service"
 require "new_manual_document_service"
+require "list_manual_documents_service"
 
 class ManualDocumentServiceRegistry
   def preview(context)
@@ -38,6 +39,13 @@ class ManualDocumentServiceRegistry
 
   def new(context)
     NewManualDocumentService.new(
+      manual_repository(context),
+      context,
+    )
+  end
+
+  def list(context)
+    ListManualDocumentsService.new(
       manual_repository(context),
       context,
     )
