c6166ea6dc3228920b36c306d6ef86148dbd43a6
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-07-01 12:00:17 +0100

    Duplicate 'org' services in vanilla registries
    For Manual services and Manual Document services there are currently 2 types of
    Service Registry: a vanilla registry and an organisational registry.
    
    In general, the organisational registry is (unfortunately) more likely to be
    dependent on being used from a controller. While the non-organisational
    registry might've been created to host services that were being used in scripts
    or workers.
    
    Until the day comes that we can pay back the technical debt by possibly
    consolidating these two types of registry (and just injecting the repository it
    uses??), there are situations where the vanilla registry will need to make use
    of those controller-oriented services that are currently exclusive to the
    organisational registry.

--- app/services/manual_document_service_registry.rb
+++ app/services/manual_document_service_registry.rb
@@ -1,9 +1,60 @@
+require "preview_manual_document_service"
+require "create_manual_document_service"
+require "update_manual_document_service"
 require "show_manual_document_service"
+require "new_manual_document_service"
+require "list_manual_documents_service"
+require "reorder_manual_documents_service"
 require "remove_manual_document_service"
 
 class ManualDocumentServiceRegistry
+  def preview(context)
+    PreviewManualDocumentService.new(
+      manual_repository,
+      manual_document_builder,
+      document_renderer,
+      context,
+    )
+  end
+
+  def create(context)
+    CreateManualDocumentService.new(
+      manual_repository: manual_repository,
+      listeners: [],
+      context: context,
+    )
+  end
+
+  def update(context)
+    UpdateManualDocumentService.new(
+      manual_repository,
+      context,
+    )
+  end
+
   def show(context)
     ShowManualDocumentService.new(
+      manual_repository,
+      context,
+    )
+  end
+
+  def new(context)
+    NewManualDocumentService.new(
+      manual_repository,
+      context,
+    )
+  end
+
+  def list(context)
+    ListManualDocumentsService.new(
+      manual_repository,
+      context,
+    )
+  end
+
+  def update_order(context)
+    ReorderManualDocumentsService.new(
       manual_repository,
       context,
     )
@@ -17,6 +68,14 @@ class ManualDocumentServiceRegistry
   end
 
 private
+  def document_renderer
+    SpecialistPublisherWiring.get(:specialist_document_renderer)
+  end
+
+  def manual_document_builder
+    SpecialistPublisherWiring.get(:manual_document_builder)
+  end
+
   def manual_repository
     SpecialistPublisherWiring.get(:repository_registry).manual_repository
   end
--- app/services/manual_service_registry.rb
+++ app/services/manual_service_registry.rb
@@ -1,4 +1,57 @@
 class ManualServiceRegistry
+  def list(context)
+    ListManualsService.new(
+      manual_repository: manual_repository,
+      context: context,
+    )
+  end
+
+  def new(context)
+    ->() { manual_builder.call(title: "") }
+  end
+
+  def create(attributes)
+    CreateManualService.new(
+      manual_repository: manual_repository,
+      manual_builder: manual_builder,
+      listeners: observers.creation,
+      attributes: attributes,
+    )
+  end
+
+  def update(manual_id, attributes)
+    UpdateManualService.new(
+      manual_repository: manual_repository,
+      manual_id: manual_id,
+      attributes: attributes,
+    )
+  end
+
+  def show(manual_id)
+    ShowManualService.new(
+      manual_repository: manual_repository,
+      manual_id: manual_id,
+    )
+  end
+
+  def queue_publish(manual_id)
+    QueuePublishManualService.new(
+      PublishManualWorker,
+      manual_repository,
+      manual_id,
+    )
+  end
+
+  def preview(manual_id, attributes)
+    PreviewManualService.new(
+      repository: manual_repository,
+      builder: manual_builder,
+      renderer: manual_renderer,
+      manual_id: manual_id,
+      attributes: attributes,
+    )
+  end
+
   def publish(manual_id, version_number)
     PublishManualService.new(
       manual_repository: manual_repository,
@@ -25,6 +78,14 @@ class ManualServiceRegistry
   end
 
 private
+  def manual_renderer
+    SpecialistPublisherWiring.get(:manual_renderer)
+  end
+
+  def manual_builder
+    SpecialistPublisherWiring.get(:manual_builder)
+  end
+
   def manual_repository
     SpecialistPublisherWiring.get(:repository_registry).manual_repository
   end
