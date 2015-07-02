b3eb74137e38c6d44d0b9c0ecb4d2714f97eb2d0
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-07-02 11:23:56 +0100

    Use different params for creating/updating actions
    Creating a manual requires the current_user's organisation_slug. None of the
    other ManualsController actions do.

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -30,7 +30,7 @@ class ManualsController < ApplicationController
   end
 
   def create
-    manual = services.create(manual_params).call
+    manual = services.create(create_manual_params).call
     manual = manual_form(manual)
 
     if manual.valid?
@@ -49,7 +49,7 @@ class ManualsController < ApplicationController
   end
 
   def update
-    manual = services.update(manual_id, manual_params).call
+    manual = services.update(manual_id, update_manual_params).call
     manual = manual_form(manual)
 
     if manual.valid?
@@ -71,7 +71,7 @@ class ManualsController < ApplicationController
   end
 
   def preview
-    manual = services.preview(params["id"], manual_params).call
+    manual = services.preview(params["id"], update_manual_params).call
 
     manual.valid? # Force validation check or errors will be empty
 
@@ -95,13 +95,21 @@ private
     params.fetch("id")
   end
 
-  def manual_params
-    params
-      .fetch("manual", {})
-      .slice(*valid_params)
+  def create_manual_params
+    base_manual_params
       .merge(
         organisation_slug: current_organisation_slug,
       )
+  end
+
+  def update_manual_params
+    base_manual_params
+  end
+
+  def base_manual_params
+    params
+      .fetch("manual", {})
+      .slice(*valid_params)
       .symbolize_keys
   end
 
