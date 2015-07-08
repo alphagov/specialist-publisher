f4d70f400148f3d8bcc344eac236fbad9538a30c
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-07-08 15:26:14 +0100

    Deduplicate Manual Service Registries' common bits
    These registries were providing an almost identical set of services, with each
    shared service duplicating the exact same setup requirements.
    
    The difference between the two types of registry is the repository they use.
    The ManualServiceRegistry uses a privileged access-anything-it-likes
    repository, whereas the OrganisationalManualServiceRegistry uses a repository
    that's restricted by organisation.
    
    It's hard to know when's the right time to use class inheritance. There are
    only a few uses of it in Specialist Publisher. An alternative approach might
    look like
    
      ManualServiceRegistry.new(
        repository: privileged_repository,
        ...
      )
    
    and
    
      organisation_repository = repository_for_organisation(my_organisation_slug)
    
      ManualServiceRegistry.new(
        repository: organisation_repository,
        ...
      )
    
    every time we need the service registry.
    
    Or, the "config" (of injecting the correct repository) could be setup for us in
    a method publicly available in a registry of its own. RegistryRegistry, anyone?
    
    Most other options seemed to require that one repository was the default, and
    the other was treat as an override of that default - e.g. in the case of
    "decorating" one registry to behave in a more or less privileged manner.
    
    So I've chosen to hide each registry's "config" inside its own class, with the
    "organisational" class handling the task of taking an `organisation_slug` and
    finding the right repository.

--- app/services/manual_service_registry.rb
+++ app/services/manual_service_registry.rb
@@ -1,101 +1,12 @@
-class ManualServiceRegistry
-  def list(context)
-    ListManualsService.new(
-      manual_repository: associationless_manual_repository,
-      context: context,
-    )
-  end
-
-  def new(context)
-    ->() { manual_builder.call(title: "") }
-  end
-
-  def create(attributes)
-    CreateManualService.new(
-      manual_repository: manual_repository,
-      manual_builder: manual_builder,
-      listeners: observers.creation,
-      attributes: attributes,
-    )
-  end
-
-  def update(manual_id, attributes)
-    UpdateManualService.new(
-      manual_repository: manual_repository,
-      manual_id: manual_id,
-      attributes: attributes,
-    )
-  end
-
-  def show(manual_id)
-    ShowManualService.new(
-      manual_repository: manual_repository,
-      manual_id: manual_id,
-    )
-  end
-
-  def queue_publish(manual_id)
-    QueuePublishManualService.new(
-      PublishManualWorker,
-      manual_repository,
-      manual_id,
-    )
-  end
-
-  def preview(manual_id, attributes)
-    PreviewManualService.new(
-      repository: manual_repository,
-      builder: manual_builder,
-      renderer: manual_renderer,
-      manual_id: manual_id,
-      attributes: attributes,
-    )
-  end
-
-  def publish(manual_id, version_number)
-    PublishManualService.new(
-      manual_repository: manual_repository,
-      listeners: observers.publication,
-      manual_id: manual_id,
-      version_number: version_number,
-    )
-  end
-
-  def republish(manual_id)
-    RepublishManualService.new(
-      manual_repository: manual_repository,
-      listeners: observers.republication,
-      manual_id: manual_id,
-    )
-  end
-
-  def withdraw(manual_id)
-    WithdrawManualService.new(
-      manual_repository: manual_repository,
-      listeners: observers.withdrawal,
-      manual_id: manual_id,
-    )
-  end
+class ManualServiceRegistry < AbstractManualServiceRegistry
 
 private
-  def manual_renderer
-    SpecialistPublisherWiring.get(:manual_renderer)
+  def associationless_repository
+    SpecialistPublisherWiring.get(:repository_registry)
+      .associationless_manual_repository
   end
 
-  def manual_builder
-    SpecialistPublisherWiring.get(:manual_builder)
-  end
-
-  def associationless_manual_repository
-    SpecialistPublisherWiring.get(:repository_registry).
-      associationless_manual_repository
-  end
-
-  def manual_repository
+  def repository
     SpecialistPublisherWiring.get(:repository_registry).manual_repository
-  end
-
-  def observers
-    @observers ||= ManualObserversRegistry.new
   end
 end
--- app/services/organisational_manual_service_registry.rb
+++ app/services/organisational_manual_service_registry.rb
@@ -1,93 +1,26 @@
-class OrganisationalManualServiceRegistry
+class OrganisationalManualServiceRegistry < AbstractManualServiceRegistry
   def initialize(dependencies)
     @organisation_slug = dependencies.fetch(:organisation_slug)
-  end
-
-  def list(context)
-    ListManualsService.new(
-      manual_repository: associationless_manual_repository,
-      context: context,
-    )
-  end
-
-  def new(context)
-    ->() { manual_builder.call(title: "") }
-  end
-
-  def create(attributes)
-    CreateManualService.new(
-      manual_repository: manual_repository,
-      manual_builder: manual_builder,
-      listeners: observers.creation,
-      attributes: attributes,
-    )
-  end
-
-  def update(manual_id, attributes)
-    UpdateManualService.new(
-      manual_repository: manual_repository,
-      manual_id: manual_id,
-      attributes: attributes,
-    )
-  end
-
-  def show(manual_id)
-    ShowManualService.new(
-      manual_repository: manual_repository,
-      manual_id: manual_id,
-    )
-  end
-
-  def queue_publish(manual_id)
-    QueuePublishManualService.new(
-      PublishManualWorker,
-      manual_repository,
-      manual_id,
-    )
-  end
-
-  def preview(manual_id, attributes)
-    PreviewManualService.new(
-      repository: manual_repository,
-      builder: manual_builder,
-      renderer: manual_renderer,
-      manual_id: manual_id,
-      attributes: attributes,
-    )
   end
 
 private
   attr_reader :organisation_slug
 
-  def manual_renderer
-    SpecialistPublisherWiring.get(:manual_renderer)
-  end
-
-  def manual_builder
-    # TODO Use ManualBuilder.new instead
-    SpecialistPublisherWiring.get(:manual_builder)
-  end
-
-  def manual_repository_factory
-    SpecialistPublisherWiring.get(:repository_registry).
-      organisation_scoped_manual_repository_factory
-  end
-
-  def manual_repository
+  def repository
     manual_repository_factory.call(organisation_slug)
   end
 
-  def associationless_manual_repository_factory
-    SpecialistPublisherWiring.get(:repository_registry).
-      associationless_organisation_scoped_manual_repository_factory
-  end
-
-  def associationless_manual_repository
+  def associationless_repository
     associationless_manual_repository_factory.call(organisation_slug)
   end
 
-  def observers
-    @observers ||= ManualObserversRegistry.new
+  def manual_repository_factory
+    SpecialistPublisherWiring.get(:repository_registry)
+      .organisation_scoped_manual_repository_factory
   end
 
+  def associationless_manual_repository_factory
+    SpecialistPublisherWiring.get(:repository_registry)
+      .associationless_organisation_scoped_manual_repository_factory
+  end
 end
