7730e5293b4b5e330173e7bc1718130dbf04a59d
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-09-21 15:20:43 +0100

    Don't index preview_only Finders on Production
    In order to prevent Finders going into Elastic Search on Production we use
    the `preview_only` flag to decide if it should be published or not. This
    commit follows the same logic in the `PublishingApiFinderPublisher` but
    keeps them separate. There's nothing to stop these being merged in
    future but as this is preventing deploys I thought I'd go down the
    simplest route now.
    
    [Ticket](https://trello.com/c/Nn0DoEN2/120-preview-only-finders-should-not-be-published-to-rummager).

--- lib/rummager_finder_publisher.rb
+++ lib/rummager_finder_publisher.rb
@@ -9,8 +9,14 @@ class RummagerFinderPublisher
 
   def call
     metadatas.each do |metadata|
-      if metadata[:file].has_key?("content_id")
+      if metadata[:file].has_key?("content_id") && !preview_only?(metadata)
         export_finder(metadata)
+      elsif preview_only?(metadata)
+        if preview_domain_or_not_production?
+          export_finder(metadata)
+        else
+          logger.info("didn't publish #{metadata[:file]["name"]} because it is preview_only")
+        end
       else
         # Even though rummager doesn't use the content_id we only want to push
         # to rummager if this is live in content-store, so this needs to
@@ -21,7 +27,16 @@ class RummagerFinderPublisher
   end
 
 private
+
   attr_reader :metadatas, :logger
+
+  def preview_only?(metadata)
+    metadata[:file]["preview_only"] == true
+  end
+
+  def preview_domain_or_not_production?
+    ENV.fetch("GOVUK_APP_DOMAIN", "")[/preview/] || !Rails.env.production?
+  end
 
   def export_finder(metadata)
     presenter = FinderRummagerPresenter.new(metadata[:file], metadata[:timestamp])
--- spec/lib/rummager_finder_publisher_spec.rb
+++ spec/lib/rummager_finder_publisher_spec.rb
@@ -110,5 +110,118 @@ describe RummagerFinderPublisher do
 
       RummagerFinderPublisher.new(metadata, logger: test_logger).call
     end
+
+    context 'with preview_only false metadata and RAILS_ENV is "production"' do
+      let(:metadata) do
+        [
+          {
+            file: {
+              "base_path" => "/finder-with-preview-only-true",
+              "content_id" => SecureRandom.uuid,
+              "name" => "finder with preview only true",
+              "format" => "a_report_format",
+              "format_name" => "a report format",
+              "preview_only" => false,
+            },
+            timestamp: "2015-01-05T10:45:10.000+00:00",
+          },
+        ]
+      end
+
+      it "does publish finder" do
+        rummager = double("rummager")
+
+        production = ActiveSupport::StringInquirer.new("production")
+        allow(Rails).to receive(:env).and_return(production)
+
+        expect(GdsApi::Rummager).to receive(:new)
+          .with(Plek.new.find("rummager"))
+          .and_return(rummager)
+
+        expect(rummager).to receive(:add_document)
+          .with(anything, "/finder-with-preview-only-true", anything)
+
+        RummagerFinderPublisher.new(metadata, logger: test_logger).call
+      end
+    end
+
+    context "with preview_only true metadata" do
+      let(:metadata) do
+        [
+          {
+            file: {
+              "base_path" => "/finder-with-preview-only-true",
+              "name" => "finder with preview only true",
+              "format" => "a_report_format",
+              "format_name" => "a report format",
+              "preview_only" => true,
+            },
+            timestamp: "2015-01-05T10:45:10.000+00:00",
+          },
+        ]
+      end
+
+      context 'and RAILS_ENV is not "production"' do
+        it "publishes finder" do
+          rummager = double("rummager")
+          expect(GdsApi::Rummager).to receive(:new)
+            .with(Plek.new.find("rummager"))
+            .and_return(rummager)
+
+          expect(rummager).to receive(:add_document)
+            .with(anything, "/finder-with-preview-only-true", anything)
+
+          RummagerFinderPublisher.new(metadata, logger: test_logger).call
+        end
+      end
+
+      context 'and RAILS_ENV is "production"' do
+        let(:metadata) do
+          [
+            {
+              file: {
+                "base_path" => "/finder-with-preview-only-true",
+                "content_id" => SecureRandom.uuid,
+                "name" => "finder with preview only true",
+                "format" => "a_report_format",
+                "format_name" => "a report format",
+                "preview_only" => true,
+              },
+              timestamp: "2015-01-05T10:45:10.000+00:00",
+            },
+          ]
+        end
+
+        let(:rummager) { double("rummager") }
+
+        before do
+          production = ActiveSupport::StringInquirer.new("production")
+          allow(Rails).to receive(:env).and_return(production)
+
+          allow(GdsApi::Rummager).to receive(:new)
+            .with(Plek.new.find("rummager"))
+            .and_return(rummager)
+        end
+
+        context 'and GOVUK_APP_DOMAIN does not contain "preview"' do
+          it "does not publish finder" do
+            expect(rummager).not_to receive(:add_document)
+              .with(anything, "/finder-with-preview-only-true", anything)
+
+            RummagerFinderPublisher.new(metadata, logger: test_logger).call
+          end
+        end
+
+        context 'and GOVUK_APP_DOMAIN contains "preview"' do
+          it "publishes finder" do
+            allow(ENV).to receive(:fetch).with("GOVUK_APP_DOMAIN", "").and_return("preview")
+            expect(rummager).to receive(:add_document)
+              .with(anything, "/finder-with-preview-only-true", anything)
+
+            RummagerFinderPublisher.new(metadata, logger: test_logger).call
+          end
+        end
+      end
+    end
   end
 end
