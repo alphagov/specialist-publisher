uk-gov-mirror/ministryofjustice.specialist-publisher

Add Employment Tribunal decisions finder
Add a finder for Employment Tribunal decisions.
The Employment Tribunal publishes decisions that
describe the outcome of a tribunal case.

Tribunal decision finders are used by public, professionals and the
tribunals themselves to search for past decisions.

Published decisions form a part of case law and are used for research
purposes and to prepare for current or future cases.

app/models/validators/employment_tribunal_decision_validator.rb
@@ -1,0 +1,16 @@
+require "delegate"
+require "validators/date_validator"
+require "validators/safe_html_validator"
+
+class EmploymentTribunalDecisionValidator < SimpleDelegator
+  include ActiveModel::Validations
+
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :body, presence: true, safe_html: true
+
+  validates :tribunal_decision_category, presence: true
+  validates :tribunal_decision_country, presence: true
+  validates :tribunal_decision_decision_date, presence: true, date: true
+
+end

alphagov/specialist-publisher

app/models/validators/employment_tribunal_decision_validator.rb
@@ -1,0 +1,16 @@
+require "delegate"
+require "validators/date_validator"
+require "validators/safe_html_validator"
+
+class EmploymentTribunalDecisionValidator < SimpleDelegator
+  include ActiveModel::Validations
+
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :body, presence: true, safe_html: true
+
+  validates :tribunal_decision_categories, presence: true
+  validates :tribunal_decision_country, presence: true
+  validates :tribunal_decision_decision_date, presence: true, date: true
+
+end