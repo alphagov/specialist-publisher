1afe2f33d3d8b05cec5c5b1edb2c8559c846d71a
Kushal Pisavadia <kushi.p@gmail.com>
2015-11-11 11:32:09 +0000

    Run the tests against govuk-content-schemas

--- jenkins.sh
+++ jenkins.sh
@@ -38,10 +38,15 @@ git merge --no-commit origin/master || git merge --abort
 export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
 
-# Uncomment if this app uses a database
-# bundle exec rake db:drop db:create db:schema:load
+# Clone govuk-content-schemas depedency for contract tests
+rm -rf tmp/govuk-content-schemas
+git clone git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
 
-if bundle exec rake ${TEST_TASK:-"default"}; then
+GOVUK_CONTENT_SCHEMAS_PATH=tmp/govuk-content-schemas RAILS_ENV=test bundle exec rake
+
+export EXIT_STATUS=$?
+
+if [ "$EXIT_STATUS" == "0" ]; then
   github_status success "succeeded on Jenkins"
 else
   github_status failure "failed on Jenkins"
