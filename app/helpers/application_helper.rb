ef04ab6d9f69ef9b832ca5aefaeb1f92d19ae217
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-11-23 16:11:00 +0000

    Views and assets for `Documents#show`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def facet_options(form, facet)
     form.object.facet_options(facet)
   end
+
+  def published_document_path(document)
+    Plek.current.find("website-root") + document.base_path
+  end
 end
