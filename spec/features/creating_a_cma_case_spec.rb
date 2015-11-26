97562cd9a6c3cf3dc9fbaa464b19f0984402f0a7
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-11-26 13:40:07 +0000

    Add tests to check for validation
    This test checks the validation conditions for CMA Cases, for both no
    data being entered and present but invalid data. The latter test is
    testing the 2 custom validators.

--- spec/features/creating_a_cma_case_spec.rb
+++ spec/features/creating_a_cma_case_spec.rb
@@ -75,4 +75,35 @@ RSpec.feature "Creating a CMA case", type: :feature do
 
     expect(page.status_code).to eq(200)
   end
+
+  scenario "with no data" do
+
+    visit "/cma-cases/new"
+
+    click_button "Save as draft"
+
+    expect(page.status_code).to eq(422)
+
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Summary can't be blank")
+    expect(page).to have_content("Body can't be blank")
+    expect(page).to have_content("Market sector can't be blank")
+  end
+
+  scenario "with invalid data" do
+    visit "/cma-cases/new"
+
+    fill_in "Title", with: "Example CMA Case"
+    fill_in "Summary", with: "This is the summary of an example CMA case"
+    fill_in "Body", with: "<script>alert('hello')</script>"
+    fill_in "Opened date", with: "Not a date"
+    select "Energy", from: "Market sector"
+
+    click_button "Save as draft"
+
+    expect(page.status_code).to eq(422)
+
+    expect(page).to have_content("Opened date should be formatted YYYY-MM-DD")
+    expect(page).to have_content("Body cannot include invalid Govspeak")
+  end
 end
