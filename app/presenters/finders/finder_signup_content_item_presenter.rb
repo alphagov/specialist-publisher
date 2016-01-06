545784830d15e3f13b719d0b1419eb40e9557d91
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2016-01-06 16:57:35 +0000

    Add Presenters for Finders
    This includes Presenters for the Finder Content Item itself, the Signup
    Pages and the Links.

---
+++ app/presenters/finders/finder_signup_content_item_presenter.rb
@@ -0,0 +1,83 @@
+class FinderSignupContentItemPresenter < Struct.new(:schema, :timestamp)
+  def to_json
+    {
+      base_path: base_path,
+      format: format,
+      content_id: content_id,
+      title: title,
+      description: description,
+      public_updated_at: public_updated_at,
+      update_type: update_type,
+      publishing_app: publishing_app,
+      rendering_app: rendering_app,
+      routes: routes,
+      details: details,
+    }
+  end
+
+  def content_id
+    schema.fetch("signup_content_id")
+  end
+
+private
+
+  def title
+    schema.fetch("signup_title", schema.fetch("name"))
+  end
+
+  def base_path
+    "#{schema.fetch("base_path")}/email-signup"
+  end
+
+  def description
+    schema.fetch("signup_copy", nil)
+  end
+
+  def format
+    "finder_email_signup"
+  end
+
+  def related
+    [
+      schema.fetch("content_id"),
+    ]
+  end
+
+  def routes
+    [
+      {
+        path: base_path,
+        type: "exact",
+      }
+    ]
+  end
+
+  def details
+    {
+      "beta" => schema.fetch("signup_beta", false),
+      "email_signup_choice" => schema.fetch("email_signup_choice", []),
+      "email_filter_by" => schema.fetch("email_filter_by", nil),
+      "subscription_list_title_prefix" => schema.fetch("subscription_list_title_prefix", {})
+    }
+  end
+
+  def publishing_app
+    "specialist-publisher"
+  end
+
+  def rendering_app
+    "finder-frontend"
+  end
+
+  def organisations
+    schema.fetch("organisations", [])
+  end
+
+  def update_type
+    "minor"
+  end
+
+  def public_updated_at
+    timestamp
+  end
+end
---
+++ app/presenters/finders/finder_signup_links_presenter.rb
@@ -0,0 +1,23 @@
+class FinderSignupLinksPresenter < Struct.new(:file)
+
+  def to_json
+    {
+      content_id: file.fetch("signup_content_id"),
+      links: {
+        organisations: organisations,
+        related: related,
+      }
+    }
+  end
+
+private
+
+  def organisations
+    file.fetch("organisations", [])
+  end
+
+  def related
+    [file.fetch("content_id")]
+  end
+
+end
