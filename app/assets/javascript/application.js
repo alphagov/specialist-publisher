666e3e91a30dbfd2cb871265093695e477d48c74
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2016-01-18 14:11:53 +0000

    Improve update form
    The previous one was a bit rubbish and didn't have separate inputs for
    different update types. This new one has different radio buttons for
    each update type, a hidden (through JS) change note textarea and some JS
    to enable the toggling of the textarea.

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -5,4 +5,14 @@ jQuery(function($) {
   $(".select2").select2({
     placeholder: $(this).data('placeholder')
   });
+
+  $('.js-hidden').hide();
+
+  $('.js-update-type-major').click(function() {
+    $('.js-change-note').show();
+  });
+
+  $('.js-update-type-minor').click(function() {
+    $('.js-change-note').hide();
+  });
 });
