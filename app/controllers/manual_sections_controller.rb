6dc1a917b19dc8e6793f67640503c09b5477ddaf
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-02-25 11:11:17 +0000

    Validate requested section belongs to the specified manual...
    The manual_section#show action is nested within the context
    of a manual, so validate that the section we find belongs to
    the manual specified by params[:manual_content_id].

--- app/controllers/manual_sections_controller.rb
+++ app/controllers/manual_sections_controller.rb
@@ -1,6 +1,15 @@
 class ManualSectionsController < ApplicationController
   def show
-    @section = Section.find(content_id: params[:content_id])
+    @section = Section.find(
+      content_id: params[:content_id],
+      manual_content_id: params[:manual_content_id]
+    )
+  rescue Section::RecordNotFound => e
+    flash[:danger] = e.message
+
+    redirect_to manuals_path
+
+    Airbrake.notify(e)
   end
 
   def edit
--- spec/features/viewing_a_manual_section_spec.rb
+++ spec/features/viewing_a_manual_section_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 RSpec.feature "Viewing a Manual and its Sections", type: :feature do
-  def log_in_as_editor(editor)
-    user = FactoryGirl.create(editor)
-    GDS::SSO.test_user = user
-  end
 
   context 'as a GDS editor' do
     def manual_content_item
@@ -206,7 +202,7 @@ RSpec.feature "Viewing a Manual and its Sections", type: :feature do
       content_items = [manual_content_item] + section_content_items
 
       content_items.each do |payload|
-       publishing_api_has_item(payload)
+        publishing_api_has_item(payload)
       end
 
       links = [manual_links] + section_links
@@ -239,6 +235,23 @@ RSpec.feature "Viewing a Manual and its Sections", type: :feature do
 
       expect(page.status_code).to eq(200)
       expect(page).to have_content("This is a manual's first section")
+    end
+
+    scenario "requesting a Section with the wrong content id" do
+      invalid_content_id = SecureRandom.uuid
+      publishing_api_does_not_have_item(invalid_content_id)
+
+      visit "/manuals/#{manual_content_item["content_id"]}/sections/#{invalid_content_id}"
+
+      expect(current_path).to eq(manuals_path)
+      expect(page).to have_content "Section not found"
+    end
+
+    scenario "requesting a Section with the wrong manual content id" do
+      visit "/manuals/#{SecureRandom.uuid}/sections/#{section_content_items.first["content_id"]}"
+
+      expect(page.current_path).to eq(manuals_path)
+      expect(page).to have_content "Section does exist, but not within the supplied manual"
     end
   end
 end
