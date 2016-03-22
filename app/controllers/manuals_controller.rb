8e3594e5ea0d5cd5802891fef20541baf7c31edc
Christoph Wong <chwong@thoughtworks.com>
2016-03-22 16:06:39 +0000

    Add update manual feature
    - Manuals can now be edited and then saved.
    
    - There are some questions about the use case for update type and
     also the correct data format for an update.

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -23,6 +23,28 @@ class ManualsController < ApplicationController
     @manual = Manual.new
   end
 
+  def edit
+    @manual = Manual.find(content_id: params[:content_id])
+  end
+
+  def update
+    @manual = Manual.find(content_id: params[:content_id])
+
+    manual_content = params["manual"]
+
+    @manual.title = manual_content["title"]
+    @manual.summary = manual_content["summary"]
+    @manual.body = manual_content["body"]
+
+    if @manual.save
+      flash[:success] = "#{@manual.title} has been updated"
+      redirect_to manual_path(@manual.content_id)
+    else
+      flash.now[:danger] = "There was an error updating #{@manual.title}. Please try again later."
+      render :edit
+    end
+  end
+
   def create
     @manual = Manual.new(params["manual"])
 
