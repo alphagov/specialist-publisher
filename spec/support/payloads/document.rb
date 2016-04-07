76573c95c8046c7adffc2576d257630ab03a4d1c
Christoph Wong <chwong@thoughtworks.com>
2016-04-07 17:49:08 +0100

    (1/3) Render Govspeak -  Payloads for content items
    This is first of 3 commits because there was a lot of refactoring as
    part of this work.
    I've extracted these content items and changed the body from a single
    string to an array that contains the govspeak and html version.

--- spec/support/payloads/document.rb
+++ spec/support/payloads/document.rb
@@ -1,4 +1,91 @@
 module Payloads
+  def self.aaib_report_content_item(attrs = {})
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/aaib-reports/example-aaib-report",
+      "title" => "Example AAIB Report",
+      "description" => "This is the summary of example AAIB Report",
+      "document_type" => "aaib_report",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example AAIB Report" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example AAIB Report</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "date_of_occurrence" => "2015-10-10",
+          "document_type" => "aaib_report"
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/aaib-reports/example-aaib-report",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.employment_appeal_tribunal_decision_content_item(attrs = {})
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/employment-appeal-tribunal-decisions/example-employment-appeal-tribunal-decision",
+      "title" => "Example Employment Appeal Tribunal Decision",
+      "description" => "This is the summary of example Employment Appeal Tribunal Decision",
+      "document_type" => "employment_appeal_tribunal_decision",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Employment Appeal Tribunal Decision" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Employment Appeal Tribunal Decision</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "tribunal_decision_categories" => ["age-discrimination"],
+          "tribunal_decision_decision_date" => "2015-07-30",
+          "tribunal_decision_landmark" => "landmark",
+          "tribunal_decision_sub_categories" => ["contract-of-employment-apprenticeship"],
+          "document_type" => "employment_appeal_tribunal_decision",
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/employment-appeal-tribunal-decisions/example-employment-appeal-tribunal-decision",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
   def self.cma_case_content_item(attrs = {})
     {
       "content_id" => SecureRandom.uuid,
@@ -13,7 +100,16 @@ module Payloads
       "phase" => "live",
       "public_updated_at" => "2015-12-03T16:59:13+00:00",
       "details" => {
-        "body" => "## Header" + ("\r\n\r\nThis is the long body of an example CMA case" * 10),
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example CMA case" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example CMA case</p>\n" * 10)
+          }
+        ],
         "metadata" => {
           "opened_date" => "2014-01-01",
           "case_type" => "ca98-and-civil-cartels",
@@ -39,6 +135,215 @@ module Payloads
     }.deep_merge(attrs)
   end
 
+  def self.countryside_stewardship_grant_content_item(attrs = {})
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/countryside-stewardship-grants/example-countryside-stewardship-grant",
+      "title" => "Example Countryside Stewardship Grant",
+      "description" => "This is the summary of example Countryside Stewardship Grant",
+      "document_type" => "countryside_stewardship_grant",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Countryside Stewardship Grant" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Countryside Stewardship Grant</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "document_type" => "countryside_stewardship_grant"
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/countryside-stewardship-grants/example-countryside-stewardship-grant",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.drug_safety_update_content_item(attrs = {})
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/drug-safety-update/example-drug-safety-update",
+      "title" => "Example Drug Safety Update",
+      "description" => "This is the summary of an example Drug Safety Update",
+      "document_type" => "drug_safety_update",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Drug Safety Update" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Drug Safety Update</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "document_type" => "drug_safety_update",
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/drug-safety-update/example-drug-safety-update",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.employment_tribunal_decision_content_item(attrs)
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/employment-tribunal-decisions/example-employment-tribunal-decision",
+      "title" => "Example Employment Tribunal Decision",
+      "description" => "This is the summary of example Employment Tribunal Decision",
+      "document_type" => "employment_tribunal_decision",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Employment Tribunal Decision" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Employment Tribunal Decision</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "tribunal_decision_categories" => ["age-discrimination"],
+          "tribunal_decision_country" => "england-and-wales",
+          "tribunal_decision_decision_date" => "2015-07-30",
+          "document_type" => "employment_tribunal_decision",
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/employment-tribunal-decisions/example-employment-tribunal-decision",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.esi_fund_content_item(attrs)
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/european-structural-investment-funds/example-esi-fund",
+      "title" => "Example ESI Fund",
+      "description" => "This is the summary of example ESI Fund",
+      "document_type" => "esi_fund",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example ESI Fund" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example ESI Fund</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "closing_date" => "2016-01-01",
+          "document_type" => "esi_fund",
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/european-structural-investment-funds/example-esi-fund",
+          "type" => "exact",
+        },
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.maib_report_content_item(attrs)
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/maib-reports/example-maib-report",
+      "title" => "Example MAIB Report",
+      "description" => "This is the summary of example MAIB Report",
+      "document_type" => "maib_report",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example MAIB Report" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example MAIB Report</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "date_of_occurrence" => "2015-10-10",
+          "document_type" => "maib_report"
+        },
+      },
+      "routes" => [
+        {
+          "path" => "/maib-reports/example-maib-report",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
   def self.medical_safety_alert_content_item(attrs = {})
     {
       "content_id" => SecureRandom.uuid,
@@ -54,7 +359,16 @@ module Payloads
       "public_updated_at" => "2015-11-16T11:53:30",
       "publication_state" => "draft",
       "details" => {
-        "body" => "## Header" + ("\r\n\r\nThis is the long body of an example Medical Safety Alert" * 10),
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Medical Safety Alert" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Medical Safety Alert</p>\n" * 10)
+          }
+        ],
         "metadata" => {
           "alert_type" => "company-led-drugs",
           "issued_date" => "2016-02-01",
@@ -65,6 +379,134 @@ module Payloads
       "routes" => [
         {
           "path" => "/drug-device-alerts/example-medical-safety-alert",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.raib_report_content_item(attrs)
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/raib-reports/example-raib-report",
+      "title" => "Example RAIB Report",
+      "description" => "This is the summary of example RAIB Report",
+      "document_type" => "raib_report",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example RAIB Report" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example RAIB Report</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "date_of_occurrence" => "2015-10-10",
+          "document_type" => "raib_report"
+        },
+      },
+      "routes" => [
+        {
+          "path" => "/raib-reports/example-raib-report",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.tax_tribunal_decision_content_item(attrs)
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/tax-and-chancery-tribunal-decisions/example-tax-tribunal-decision",
+      "title" => "Example Tax Tribunal Decision",
+      "description" => "This is the summary of example Tax Tribunal Decision",
+      "document_type" => "tax_tribunal_decision",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Tax Tribunal Decision" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Tax Tribunal Decision</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "tribunal_decision_category" => "banking",
+          "tribunal_decision_decision_date" => "2015-07-30",
+          "document_type" => "tax_tribunal_decision",
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/tax-and-chancery-tribunal-decisions/example-tax-tribunal-decision",
+          "type" => "exact",
+        }
+      ],
+      "redirects" => [],
+      "update_type" => "major",
+    }.deep_merge(attrs)
+  end
+
+  def self.vehicle_recalls_and_faults_alert_content_item(attrs)
+    {
+      "content_id" => SecureRandom.uuid,
+      "base_path" => "/vehicle-recalls-faults/example-vehicle-recalls-and-faults",
+      "title" => "Example Vehicle Recalls And Faults",
+      "description" => "This is the summary of example Vehicle Recalls And Faults",
+      "document_type" => "vehicle_recalls_and_faults_alert",
+      "schema_name" => "specialist_document",
+      "publishing_app" => "specialist-publisher",
+      "rendering_app" => "specialist-frontend",
+      "locale" => "en",
+      "phase" => "live",
+      "public_updated_at" => "2015-11-16T11:53:30",
+      "publication_state" => "draft",
+      "details" => {
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example Vehicle Recalls And Faults" * 10)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example Vehicle Recalls And Faults</p>\n" * 10)
+          }
+        ],
+        "metadata" => {
+          "alert_issue_date" => "2015-04-28",
+          "build_start_date" => "2015-04-28",
+          "build_end_date" => "2015-06-28",
+          "document_type" => "vehicle_recalls_and_faults_alert"
+        },
+        "change_history" => [],
+      },
+      "routes" => [
+        {
+          "path" => "/vehicle-recalls-faults/example-vehicle-recalls-and-faults",
           "type" => "exact",
         }
       ],
