smelike/manuals-publisher

Paginate content items
Paginates content items using the pagination functionality from the publishing-api. The presentation of the pagination in the views is handled using Kaminari.

app/presenters/pagination_presenter.rb
@@ -1,0 +1,20 @@
+require 'ostruct'
+
+class PaginationPresenter < SimpleDelegator
+  attr_reader :current_page, :total_pages, :total_count, :limit_value
+
+  def initialize(api_response, per_page)
+    # actually delegate to the API response's `results` array
+    super(present_results(api_response.results))
+
+    @current_page = api_response.current_page
+    @total_pages = api_response.pages
+    @total_count = api_response.total
+    @limit_value = per_page
+  end
+
+private
+  def present_results(results)
+    results.map { |entry| OpenStruct.new(entry) }
+  end
+end

alphagov/specialist-publisher

app/presenters/pagination_presenter.rb
@@ -1,0 +1,21 @@
+require 'ostruct'
+
+class PaginationPresenter < SimpleDelegator
+  attr_reader :current_page, :total_pages, :total_count, :limit_value
+
+  def initialize(api_response, _per_page)
+    # actually delegate to the API response's `results` array
+    super(present_results(api_response.results))
+
+    @current_page = api_response.current_page
+    @total_pages = api_response.pages
+    @total_count = api_response.total
+    @limit_value = per_page
+  end
+
+private
+
+  def present_results(results)
+    results.map { |entry| OpenStruct.new(entry) }
+  end
+end