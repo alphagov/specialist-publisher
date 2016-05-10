f9e0d574888cd81fb68aec48a57587b26c3defcc
Rosa-Fox <rosafox89@gmail.com>
2016-05-10 16:24:41 +0000

    Email alerts display the same info as V1
    - Subject now only appends the text 'updated' if a document has actually been updated (previously it was also displaying when a document was 1st published)
    - Name of the organisation displays within the h1 as opposed to "Documents" which was previously hard-coded into the h1 in v2
    - 'Further information' paragraph dynamically informs the user if the document is published or updated aka 'For further information on this published case:' as opposed to 'For further information on this updated document' which was previously hardcoded in v2

--- spec/presenters/email_alert_presenter_spec.rb
+++ spec/presenters/email_alert_presenter_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 describe EmailAlertPresenter do
   let(:cma_case_payload) { Payloads.cma_case_content_item }
   let(:medical_safety_payload) { Payloads.medical_safety_alert_content_item }
+  let(:cma_case_redrafted_payload) { Payloads.cma_case_content_item("publication_state" => "redrafted") }
 
   describe "#to_json" do
     context "any finders document" do
       before do
         publishing_api_has_item(cma_case_payload)
+        publishing_api_has_item(cma_case_redrafted_payload)
       end
 
       it "has correct information" do
@@ -15,9 +17,17 @@ describe EmailAlertPresenter do
         email_alert_presenter = EmailAlertPresenter.new(cma_case)
         presented_data = email_alert_presenter.to_json
 
+        redrafted_cma_case = CmaCase.find(cma_case_redrafted_payload["content_id"])
+        email_alert_presenter_redrafted = EmailAlertPresenter.new(redrafted_cma_case)
+        presented_data_redrafted = email_alert_presenter_redrafted.to_json
+
         expect(presented_data[:subject]).to include(cma_case_payload["title"])
+        expect(presented_data[:subject]).not_to include("updated")
+        expect(presented_data_redrafted[:subject]).to include("updated")
         expect(presented_data[:body]).to include(cma_case_payload["description"])
         expect(presented_data[:body]).to include(cma_case_payload["title"])
+        expect(presented_data[:body]).to include("For further information on this published case")
+        expect(presented_data_redrafted[:body]).to include("For further information on this updated case")
         expect(presented_data[:tags][:format]).to eq(cma_case_payload["document_type"])
         expect(presented_data[:tags][:case_type]).to eq(cma_case_payload["details"]["metadata"]["case_type"])
         expect(presented_data[:document_type]).to eq(cma_case_payload["document_type"])
