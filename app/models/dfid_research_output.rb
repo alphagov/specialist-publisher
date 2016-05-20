957420bc0d2ebfe80527df1cacb6b4681133cba0
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-05-20 16:28:27 +0100

    Add a filterable "first_published_at" field
    This needs to be separate and distinct from the publishing platform's
     "Published" date field, which is updated in lockstep with major updates
    and will not appear when items are `bulk_published`. There is already
     a "first_published_at" field in rummager, so we just use this.
    
    This gives editors the opportunity to enter the real published date
    of a research output.

--- app/models/dfid_research_output.rb
+++ app/models/dfid_research_output.rb
@@ -1,7 +1,8 @@
 class DfidResearchOutput < Document
   validates :country, presence: true
+  validates :first_published_at, presence: true, date: true
 
-  FORMAT_SPECIFIC_FIELDS = %i(country)
+  FORMAT_SPECIFIC_FIELDS = %i(country first_published_at)
 
   attr_accessor(*FORMAT_SPECIFIC_FIELDS)
 
