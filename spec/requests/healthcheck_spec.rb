d18da25605f8bead66a1cb0ffd01df50439d03ad
Chris Patuzzo <chris@patuzzo.co.uk>
2016-05-23 13:08:36 +0000

    Add a healthcheck specific to this application
    When the "frankenstein" app is running in production with
    both v1 and v2, it will be useful to check that the
    routing to v2 is working correctly. We will add this rule
    to the Nginx config, too.

--- spec/requests/healthcheck_spec.rb
+++ spec/requests/healthcheck_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "healthcheck path", type: :request do
     expect(response.status).to eq(200)
     expect(response.body).to eq("OK")
   end
+
+  it "should respond with 'OK'" do
+    get "/rebuild-healthcheck"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to eq("OK")
+  end
 end
