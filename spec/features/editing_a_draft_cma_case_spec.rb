db28c9fe5739ee9bf797fe4cc979ffa3a9cc5961
Jake Benilov <benilov@gmail.com>
2016-05-30 13:26:36 +0000

    Fix up test data in draft editing feature
    - make test data more specific to the scenario where it's needed
    - use FactoryGirl instead of Payloads
    - rename local variables for clarity

--- spec/features/editing_a_draft_cma_case_spec.rb
+++ spec/features/editing_a_draft_cma_case_spec.rb
@@ -1,18 +1,107 @@
 require 'spec_helper'
 
 RSpec.feature "Editing a draft CMA case", type: :feature do
-  let(:file_name) { "cma_case_image.jpg" }
-  let(:asset_url) { "http://assets-origin.dev.gov.uk/media/56c45553759b740609000000/#{file_name}" }
-  let(:asset_manager_response) {
-    {
-      id: 'http://asset-manager.dev.gov.uk/assets/another_image_id',
-      file_url: asset_url
-    }
+  let(:cma_case) {
+    FactoryGirl.create(:cma_case,
+      title: "Example CMA Case",
+      publication_state: "draft")
   }
 
-  let(:cma_case) {
-    Payloads.cma_case_content_item("details" => {
-      "attachments" => [
+  let(:content_id) { cma_case['content_id'] }
+
+  before do
+    log_in_as_editor(:cma_editor)
+
+    stub_any_publishing_api_put_content
+    stub_any_publishing_api_patch_links
+
+    publishing_api_has_content([cma_case], hash_including(document_type: CmaCase.document_type))
+    publishing_api_has_item(cma_case)
+
+    Timecop.freeze(Time.parse("2015-12-03T16:59:13+00:00"))
+
+    visit "/cma-cases/#{content_id}"
+    click_link "Edit document"
+  end
+
+  after do
+    Timecop.return
+  end
+
+  scenario "with some changed attributes" do
+    updated_cma_case = cma_case.deep_merge(
+      "title" => "Changed title",
+      "description" => "Changed summary",
+      "public_updated_at" => "2015-12-03T16:59:13+00:00",
+      "details" => {
+        "metadata" => {
+          "opened_date" => "2014-01-01",
+          "market_sector" => ["energy"],
+        },
+        "body" => [
+          {
+            "content_type" => "text/govspeak",
+            "content" => "## Header" + ("\r\n\r\nThis is the long body of an example CMA case" * 2)
+          },
+          {
+            "content_type" => "text/html",
+            "content" => ("<h2 id=\"header\">Header</h2>\n" + "\n<p>This is the long body of an example CMA case</p>\n" * 2)
+          }
+        ],
+        "headers" => [{
+          "text" => "Header",
+          "level" => 2,
+          "id" => "header",
+        }],
+      }
+    )
+    expected_sent_payload = saved_for_the_first_time(write_payload(updated_cma_case))
+
+    fill_in "Title", with: "Changed title"
+    fill_in "Summary", with: "Changed summary"
+    fill_in "Body", with: "## Header" + ("\n\nThis is the long body of an example CMA case" * 2)
+    fill_in "Opened date", with: "2014-01-01"
+    select "Energy", from: "Market sector"
+
+    expect(page).to have_css('div.govspeak-help')
+    expect(page).to have_content('Add attachment')
+
+    click_button "Save as draft"
+
+    assert_publishing_api_put_content(content_id, expected_sent_payload)
+
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content("Updated Changed title")
+  end
+
+  scenario "with some invalid changed attributes" do
+    fill_in "Title", with: "Changed title"
+    fill_in "Summary", with: "Changed summary"
+    fill_in "Body", with: "<script>alert('hello')</script>"
+    fill_in "Opened date", with: "Not a date"
+    select "Energy", from: "Market sector"
+
+    click_button "Save as draft"
+
+    expect(page).to have_css('.elements-error-summary')
+    expect(page).to have_css('.elements-error-message')
+
+    expect(page).to have_content("Opened date should be formatted YYYY-MM-DD")
+    expect(page).to have_content("Body cannot include invalid Govspeak")
+    expect(page).to have_content("Please fix the following errors")
+
+    expect(page.status_code).to eq(422)
+  end
+
+  context "with attachments" do
+    let(:asset_manager_response) {
+      {
+        id: 'http://asset-manager.dev.gov.uk/assets/another_image_id',
+        file_url: "http://assets-origin.dev.gov.uk/media/56c45553759b740609000000/cma_case_image.jpg",
+      }
+    }
+    let(:existing_attachments) {
+      [
         {
           "content_id" => "77f2d40e-3853-451f-9ca3-a747e8402e34",
           "url" => "https://assets.digital.cabinet-office.gov.uk/media/513a0efbed915d425e000002/asylum-support-image.jpg",
@@ -30,131 +119,46 @@ RSpec.feature "Editing a draft CMA case", type: :feature do
           "updated_at" => "2015-12-03T16:59:13+00:00"
         }
       ]
-    },
-    "publication_state" => "draft")
-  }
-  let(:content_id) { cma_case['content_id'] }
+    }
 
-  before do
-    log_in_as_editor(:cma_editor)
+    let(:cma_case) {
+      FactoryGirl.create(:cma_case,
+        title: "Example CMA Case",
+        publication_state: "draft",
+        details: { "attachments" => existing_attachments })
+    }
 
-    stub_any_publishing_api_put_content
-    stub_any_publishing_api_patch_links
+    before do
+      stub_request(:post, "#{Plek.find('asset-manager')}/assets")
+        .with(body: %r{.*})
+        .to_return(body: asset_manager_response.to_json, status: 201)
+    end
 
-    publishing_api_has_content([cma_case], hash_including(document_type: CmaCase.document_type))
+    scenario "adding an attachment" do
+      click_link "Add attachment"
+      expect(page.status_code).to eq(200)
 
-    publishing_api_has_item(cma_case)
+      fill_in "Title", with: "New cma case image"
+      page.attach_file('attachment_file', "spec/support/images/cma_case_image.jpg")
 
+      click_button "Save attachment"
+      expect(page.status_code).to eq(200)
 
-    @changed_json = cma_case.merge(
-      "title" => "Changed title",
-      "description" => "Changed summary",
-      "public_updated_at" => "2015-12-03T16:59:13+00:00",
-    )
+      expect(page).to have_content("Editing Example CMA Case")
+    end
 
-    @changed_json["details"].merge!(
-      "change_history" => [
-        {
-          "public_timestamp" => "2015-12-03T16:59:13+00:00",
-          "note" => "First published.",
-        }
-      ]
-    )
+    scenario "editing an attachment" do
+      find('.attachments').first(:link, "edit").click
 
-    @changed_json.delete("publication_state")
-    @changed_json.delete("updated_at")
-    @changed_json.delete("first_published_at")
-    Timecop.freeze(Time.parse("2015-12-03T16:59:13+00:00"))
+      expect(page.status_code).to eq(200)
 
-    stub_request(:post, "#{Plek.find('asset-manager')}/assets")
-      .with(body: %r{.*})
-      .to_return(body: JSON.dump(asset_manager_response), status: 201)
-  end
+      fill_in "Title", with: "Updated cma case image"
+      page.attach_file('attachment_file', "spec/support/images/updated_cma_case_image.jpg")
 
-  after do
-    Timecop.return
-  end
+      click_button("Save attachment")
 
-  scenario "with some changed attributes" do
-    visit "/cma-cases/#{content_id}"
-
-    click_link "Edit document"
-
-    fill_in "Title", with: "Changed title"
-    fill_in "Summary", with: "Changed summary"
-    fill_in "Body", with: "## Header" + ("\n\nThis is the long body of an example CMA case" * 10)
-    fill_in "Opened date", with: "2014-01-01"
-    select "Energy", from: "Market sector"
-
-    expect(page).to have_css('div.govspeak-help')
-    expect(page).to have_content('Add attachment')
-
-    click_button "Save as draft"
-
-    assert_publishing_api_put_content(content_id, request_json_includes(@changed_json))
-    expect(@changed_json["content_id"]).to eq(content_id)
-    expect(@changed_json["public_updated_at"]).to eq("2015-12-03T16:59:13+00:00")
-
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Updated Changed title")
-  end
-
-  scenario "with some invalid changed attributes" do
-    visit "/cma-cases/#{content_id}"
-
-    click_link "Edit document"
-
-    fill_in "Title", with: "Changed title"
-    fill_in "Summary", with: "Changed summary"
-    fill_in "Body", with: "<script>alert('hello')</script>"
-    fill_in "Opened date", with: "Not a date"
-    select "Energy", from: "Market sector"
-
-    click_button "Save as draft"
-
-    expect(page).to have_css('.elements-error-summary')
-    expect(page).to have_css('.elements-error-message')
-
-    expect(@changed_json["content_id"]).to eq(content_id)
-    expect(page).to have_content("Opened date should be formatted YYYY-MM-DD")
-    expect(page).to have_content("Body cannot include invalid Govspeak")
-    expect(page).to have_content("Please fix the following errors")
-
-    expect(page.status_code).to eq(422)
-  end
-
-  scenario "adding an attachment" do
-    visit "/cma-cases/#{content_id}"
-
-    click_link "Edit document"
-
-    click_link "Add attachment"
-    expect(page.status_code).to eq(200)
-
-    fill_in "Title", with: "New cma case image"
-    page.attach_file('attachment_file', "spec/support/images/cma_case_image.jpg")
-
-    click_button "Save attachment"
-    expect(page.status_code).to eq(200)
-
-    expect(page).to have_content("Editing Example CMA Case")
-  end
-
-  scenario "editing an attachment" do
-    visit "/cma-cases/#{content_id}"
-
-    click_link "Edit document"
-
-    find('.attachments').first(:link, "edit").click
-
-    expect(page.status_code).to eq(200)
-
-    fill_in "Title", with: "Updated cma case image"
-    page.attach_file('attachment_file', "spec/support/images/updated_cma_case_image.jpg")
-
-    click_button("Save attachment")
-
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Editing Example CMA Case")
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Editing Example CMA Case")
+    end
   end
 end
