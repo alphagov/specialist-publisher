7af27eca692a8591910d507b413e4104f623842a
Jake Benilov <benilov@gmail.com>
2016-05-30 21:18:13 +0000

    Update test data in publishing feature spec
    - use FactoryGirl instead of Payloads for test data generation
    - group the scenarios by context to bring the specs and test data closer
    - remove detail (eg the search payload) from the spec, as it's already covered
    at the unit level

--- spec/features/publishing_a_cma_case_spec.rb
+++ spec/features/publishing_a_cma_case_spec.rb
@@ -1,178 +1,179 @@
 require 'spec_helper'
 
 RSpec.feature "Publishing a CMA case", type: :feature do
-  def indexable_attributes
-    {
-      "title" => "Example CMA Case",
-      "description" => "This is the summary of an example CMA case",
-      "link" => "/cma-cases/example-cma-case",
-      "indexable_content" => "Header " + (["This is the long body of an example CMA case"] * 10).join(" "),
-      "public_timestamp" => "2015-11-16T11:53:30+00:00",
-      "opened_date" => "2014-01-01",
-      "closed_date" => nil,
-      "case_type" => "ca98-and-civil-cartels",
-      "case_state" => "open",
-      "market_sector" => ["energy"],
-      "outcome_type" => nil,
-    }
-  end
-
-  let(:cma_case) {
-    Payloads.cma_case_content_item(
-      "public_updated_at" => "2015-11-16T11:53:30+00:00",
-      "need_ids" => [],
-      "publication_state" => "draft",
-    )
-  }
-  let(:content_id) { cma_case['content_id'] }
-
-  def minor_update_item
-    cma_case.merge(
-      "title" => "Minor Update Case",
-      "update_type" => "minor"
-    )
-  end
-
-  def major_update_item
-    cma_case.merge(
-      "title" => "Major Update Case",
-      "publication_state" => "redrafted",
-    )
-  end
-
-  def live_item
-    cma_case.merge(
-      "title" => "Live Item",
-      "publication_state" => "live"
-    )
-  end
-
-  def withdrawn_item
-    cma_case.merge(
-      "title" => "Withdrawn Item",
-      "publication_state" => "withdrawn"
-    )
-  end
+  let(:content_id) { item['content_id'] }
 
   before do
     log_in_as_editor(:cma_editor)
 
-    publishing_api_has_content([cma_case, minor_update_item, major_update_item, live_item, withdrawn_item], hash_including(document_type: CmaCase.document_type))
-
-    publishing_api_has_item(cma_case)
+    publishing_api_has_content([item], hash_including(document_type: CmaCase.document_type))
+    publishing_api_has_item(item)
 
     stub_publishing_api_publish(content_id, {})
     stub_any_rummager_post_with_queueing_enabled
     email_alert_api_accepts_alert
   end
 
-  scenario "from the index" do
-    publishing_api_has_item(cma_case)
+  context "when the document is a new draft" do
+    let(:item) {
+      FactoryGirl.create(:cma_case,
+        title: "Example CMA Case",
+        base_path: "/cma-cases/example-cma-case",
+        public_updated_at: "2015-11-16T11:53:30+00:00",
+        publication_state: "draft")
+    }
 
-    visit "/cma-cases"
+    scenario "from the index" do
+      visit "/cma-cases"
+      expect(page.status_code).to eq(200)
 
-    expect(page.status_code).to eq(200)
+      click_link "Example CMA Case"
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Example CMA Case")
 
-    click_link "Example CMA Case"
+      click_button "Publish"
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Published Example CMA Case")
 
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Example CMA Case")
-    expect(page).to have_content("Publishing will email subscribers to CMA Cases.")
+      assert_publishing_api_publish(content_id)
+      assert_rummager_posted_item("link" => "/cma-cases/example-cma-case")
+      assert_email_alert_sent
+    end
 
-    click_button "Publish"
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Published Example CMA Case")
+    scenario "publish warning will indicate that an email will be sent" do
+      visit "/cma-cases"
 
-    assert_publishing_api_publish(content_id)
-    assert_rummager_posted_item(indexable_attributes)
-    assert_email_alert_sent
+      expect(page.status_code).to eq(200)
+
+      click_link "Example CMA Case"
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Example CMA Case")
+      expect(page).to have_content("Publishing will email subscribers to CMA Cases.")
+    end
+
+    scenario "writers don't see a publish button" do
+      log_in_as_editor(:cma_writer)
+
+      visit "/cma-cases"
+      click_link "Example CMA Case"
+
+      expect(page).to have_no_selector(:button, 'Publish')
+      expect(page).to have_content("You don’t have permission to publish this document.")
+    end
   end
 
-  scenario "alerts should not be sent when update type is minor" do
-    publishing_api_has_item(minor_update_item)
+  context "when the document is already published" do
+    let(:item) {
+      FactoryGirl.create(:cma_case,
+        title: "Live Item",
+        publication_state: "live")
+    }
 
-    visit "/cma-cases"
+    scenario "publish buttons aren't shown" do
+      visit "/cma-cases"
+      expect(page.status_code).to eq(200)
 
-    expect(page.status_code).to eq(200)
+      click_link "Live Item"
 
-    click_link "Minor Update Case"
-
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Minor Update Case")
-
-    click_button "Publish"
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Published Minor Update Case")
-
-    assert_publishing_api_publish(content_id)
-
-    assert_not_requested(:post, Plek.current.find('email-alert-api') + "/notifications")
+      expect(page).to have_no_selector(:button, 'Publish')
+      expect(page).to have_content("There are no changes to publish.")
+    end
   end
 
-  scenario "publish warning will appear when published document update type is major" do
-    publishing_api_has_item(major_update_item)
+  context "when there is a redrafted document with a major update" do
+    let(:item) {
+      FactoryGirl.create(:cma_case,
+        title: "Major Update Case",
+        update_type: "major",
+        publication_state: "redrafted")
+    }
 
-    visit "/cma-cases"
+    scenario "publish warning will indicate that it is a major edit" do
+      visit "/cma-cases"
 
-    expect(page.status_code).to eq(200)
+      expect(page.status_code).to eq(200)
 
-    click_link "Major Update Case"
+      click_link "Major Update Case"
 
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Major Update Case")
-    expect(page).to have_content("You are about to publish a major edit with a public change note.")
-    expect(page).to have_content("Publishing will email subscribers to CMA Cases.")
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Major Update Case")
+      expect(page).to have_content("You are about to publish a major edit with a public change note.")
+      expect(page).to have_content("Publishing will email subscribers to CMA Cases.")
+    end
   end
 
-  scenario "publish waring will appear when update type is minor" do
-    publishing_api_has_item(minor_update_item)
-    visit "/cma-cases"
+  context "when there is a redrafted document with a minor update" do
+    let(:item) {
+      FactoryGirl.create(:cma_case,
+        title: "Minor Update Case",
+        update_type: "minor",
+        publication_state: "redrafted")
+    }
 
-    expect(page.status_code).to eq(200)
+    scenario "alerts should not be sent when the item is published" do
+      visit "/cma-cases"
 
-    click_link "Minor Update Case"
+      expect(page.status_code).to eq(200)
 
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("Minor Update Case")
-    expect(page).to have_content("You are about to publish a minor edit.")
-    expect(page).not_to have_content("Publishing will email subscribers to CMA Cases.")
+      click_link "Minor Update Case"
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Minor Update Case")
+
+      click_button "Publish"
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Published Minor Update Case")
+
+      assert_publishing_api_publish(content_id)
+
+      assert_not_requested(:post, Plek.current.find('email-alert-api') + "/notifications")
+    end
+
+    scenario "publish warning will indicate that it is a minor edit" do
+      visit "/cma-cases"
+
+      expect(page.status_code).to eq(200)
+
+      click_link "Minor Update Case"
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Minor Update Case")
+      expect(page).to have_content("You are about to publish a minor edit.")
+      expect(page).to have_no_content("Publishing will email subscribers to CMA Cases.")
+    end
   end
 
-  scenario "when content item is live, there will be no publish button" do
-    publishing_api_has_item(live_item)
+  context "when the document is withdrawn" do
+    let(:item) {
+      FactoryGirl.create(:cma_case,
+        title: "Withdrawn Item",
+        publication_state: "withdrawn")
+    }
 
-    visit "/cma-cases"
+    scenario "when content item is withdrawn, there will be a publish button" do
+      visit "/cma-cases"
+      expect(page.status_code).to eq(200)
 
-    expect(page.status_code).to eq(200)
+      click_link "Withdrawn Item"
 
-    click_link "Live Item"
+      expect(page).to have_selector(:button, 'Publish')
+      expect(page).to have_no_content("There are no changes to publish.")
+    end
 
-    expect(page).not_to have_selector(:button, 'Publish')
-    expect(page).to have_content("There are no changes to publish.")
-  end
+    scenario "publish warning will indicate that an email will be sent" do
+      visit "/cma-cases"
 
-  scenario "writers don't see a publish button" do
-    log_in_as_editor(:cma_writer)
+      expect(page.status_code).to eq(200)
 
-    publishing_api_has_item(minor_update_item)
+      click_link "Withdrawn Item"
 
-    visit "/cma-cases"
-    click_link "Minor Update Case"
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Withdrawn Item")
+      expect(page).to have_content("Publishing will email subscribers to CMA Cases.")
 
-    expect(page).not_to have_selector(:button, 'Publish')
-    expect(page).to have_content("You don’t have permission to publish this document.")
-  end
-
-  scenario "when content item is withdrawn, there will be a publish button" do
-    publishing_api_has_item(withdrawn_item)
-
-    visit "/cma-cases"
-
-    expect(page.status_code).to eq(200)
-
-    click_link "Withdrawn Item"
-
-    expect(page).to have_selector(:button, 'Publish')
-    expect(page).not_to have_content("There are no changes to publish.")
+      expect(page).to have_no_content("You are about to publish a minor edit")
+      expect(page).to have_no_content("You are about to publish a major edit")
+    end
   end
 end
