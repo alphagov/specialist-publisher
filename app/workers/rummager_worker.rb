b60786218f71d6ef242cff9c1a23c5cbdd0a817c
Chris Patuzzo <chris@patuzzo.co.uk>
2016-06-10 13:42:23 +0100

    Use the govuk_sidekiq gem

--- app/workers/rummager_worker.rb
+++ app/workers/rummager_worker.rb
@@ -1,7 +1,7 @@
 class RummagerWorker
   include Sidekiq::Worker
 
-  def perform(document_type, base_path, payload)
+  def perform(document_type, base_path, payload, _ = nil)
     payload.symbolize_keys!
     Services.rummager.add_document(document_type, base_path, payload)
   end
