1e6975f793fae45422a401ef1bb6672e9378afe3
David Basalla <davidbasalla@gmail.com>
2016-06-10 13:46:05 +0000

    Create attachment collection
    An attempt to encapsulate common functionality for attachments.
    This follows from trying to set up a `has_many` association between
    Documents and Attachments, but associations come from ActiveRecord
    so not really feasible use them here. Instead we opt for a Collections
    model extracts related methods from the Document and Section classes.

---
+++ app/models/attachment_collection.rb
@@ -0,0 +1,31 @@
+class AttachmentCollection
+  include Enumerable
+
+  def initialize(attachments = [])
+    @attachments = attachments
+  end
+
+  def find(content_id)
+    @attachments.detect { |attachment| attachment.content_id == content_id }
+  end
+
+  def build(params)
+    new_attachment = Attachment.new(params)
+    @attachments << new_attachment
+    new_attachment
+  end
+
+  def upload(attachment)
+    attachment.upload if has_attachment?(attachment)
+  end
+
+  def has_attachment?(attachment)
+    !!find(attachment.content_id)
+  end
+
+  def each(&block)
+    @attachments.each do |attachment|
+      block.call(attachment)
+    end
+  end
+end
