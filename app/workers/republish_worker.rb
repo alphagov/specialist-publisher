a602cb2cbaadc08d9e68bdf2280a1acc4e6bfab0
Chris Patuzzo <chris@patuzzo.co.uk>
2016-06-15 12:36:39 +0100

    Add a RepublishWorker that republishes content

--- app/workers/republish_worker.rb
+++ app/workers/republish_worker.rb
@@ -3,7 +3,37 @@ require "services"
 class RepublishWorker
   include Sidekiq::Worker
 
-  def perform(content_id, _=nil)
+  def perform(content_id, _ = nil)
+    document = Document.find(content_id)
+    document.update_type = "republish"
 
+    publishing_api_put_content(document)
+    publishing_api_patch_links(document)
+
+    if document.publication_state == "live"
+      publishing_api_publish(document)
+      rummager_add_document(document)
+    end
+  end
+
+private
+
+  def publishing_api_put_content(document)
+    payload = DocumentPresenter.new(document).to_json
+    Services.publishing_api.put_content(document.content_id, payload)
+  end
+
+  def publishing_api_patch_links(document)
+    payload = DocumentLinksPresenter.new(document).to_json
+    Services.publishing_api.patch_links(document.content_id, payload)
+  end
+
+  def publishing_api_publish(document)
+    Services.publishing_api.publish(document.content_id, "republish")
+  end
+
+  def rummager_add_document(document)
+    payload = SearchPresenter.new(document).to_json
+    Services.rummager.add_document(document.document_type, document.base_path, payload)
   end
 end
---
+++ spec/workers/republish_worker_spec.rb
@@ -0,0 +1,69 @@
+require "rails_helper"
+
+RSpec.describe RepublishWorker do
+  let(:document) { FactoryGirl.create(:cma_case) }
+  let(:content_id) { document["content_id"] }
+
+  let(:republish_matcher) {
+    request_json_includes("update_type" => "republish")
+  }
+
+  before do
+    stub_any_publishing_api_call
+    stub_any_rummager_post
+
+    publishing_api_has_item(document)
+  end
+
+  %w(draft redrafted).each do |publication_state|
+    context "when the publication_state is '#{publication_state}'" do
+      let(:document) {
+        FactoryGirl.create(:cma_case, publication_state: publication_state)
+      }
+
+      it "sends the document to the publishing api" do
+        subject.perform(content_id)
+
+        assert_publishing_api_put_content(content_id, republish_matcher)
+        assert_publishing_api_patch_links(content_id)
+      end
+
+      it "does not publish the document" do
+        subject.perform(content_id)
+
+        expect(WebMock).not_to have_requested(:post, /publish/)
+      end
+
+      it "does not speak to rummager" do
+        subject.perform(content_id)
+
+        expect(WebMock).not_to have_requested(:post, /search/)
+      end
+    end
+  end
+
+  context "when the document is live" do
+    let(:document) {
+      FactoryGirl.create(:cma_case, publication_state: "live")
+    }
+
+    it "sends the document to the publishing api" do
+      subject.perform(content_id)
+
+      assert_publishing_api_put_content(content_id, republish_matcher)
+      assert_publishing_api_patch_links(content_id)
+    end
+
+    it "publishes the document" do
+      subject.perform(content_id)
+
+      assert_publishing_api_publish(content_id, republish_matcher)
+    end
+
+    it "sends the document to rummager" do
+      subject.perform(content_id)
+
+      assert_rummager_posted_item({})
+    end
+  end
+end
