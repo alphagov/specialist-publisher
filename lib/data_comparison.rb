d12f90e831cd154735f369fd969ad4008ce6749c
Chris Patuzzo <chris@patuzzo.co.uk>
2016-07-11 11:22:49 +0000

    Add a task for performing data comparisons
    This lets us see if there are any differences between
    data the v1 application has sent to the publishing api
    vs. data the v2 application sends as part of the
    application's save/publish/unpublish behaviour.

---
+++ lib/data_comparison.rb
@@ -0,0 +1,54 @@
+require "hashdiff"
+
+module DataComparison
+module_function
+
+  def compare(content_id)
+    Services.publishing_api.client.options[:timeout] = 30
+
+    puts "<<<<<"
+    puts "Comparing #{content_id}"
+
+    before = nil
+
+    begin
+      before = Services.publishing_api.get_content(content_id).to_hash
+    rescue GdsApi::HTTPServerError
+      puts "ERROR: Could not find content_id '#{content_id}'"
+      return
+    end
+
+    document = Document.find(content_id)
+
+    unless document.update_type
+      document.update_type = "major"
+      document.change_note = "some change note"
+    end
+
+    if document.save
+      puts "Saving document"
+    else
+      puts "ERROR: Failed to save document"
+      puts document.errors.messages.inspect
+      return
+    end
+
+    if document.live?
+      puts "Publishing document"
+      document.publish
+    elsif document.unpublished?
+      puts "Publishing document"
+      document.publish
+      puts "Unpublishing document"
+      document.unpublish
+    end
+
+    after = Services.publishing_api.get_content(content_id).to_hash
+
+    diff = HashDiff.diff(before, after)
+    puts "Differences:"
+    puts diff
+
+    puts "<<<<<"
+  end
+end
---
+++ lib/tasks/compare.rake
@@ -0,0 +1,20 @@
+desc "Compare the data for a document before and after saving it"
+task :compare, [:content_id] => :environment do |_, args|
+  DataComparison.compare(args.content_id)
+end
+
+task :compare_all, [:document_type] => :environment do |_, args|
+  results = Services
+    .publishing_api
+    .get_content_items(
+      document_type: args.document_type,
+      per_page: 99999,
+      fields: %w(content_id)
+    ).to_hash.fetch("results")
+
+  content_ids = results.map { |r| r.fetch("content_id") }
+
+  content_ids.each do |content_id|
+    DataComparison.compare(content_id)
+  end
+end
