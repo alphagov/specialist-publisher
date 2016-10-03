efa8cab05ffc34053ef5d838a88d865175a2335c
Carlos Martinez <carlosjmtz@gmail.com>
2016-10-03 12:11:51 +0100

    Specify AttachmentPresenter location
    Now needed since the latest version of Govspeak also contains an
    AttachmentPresenter class.
    
    Also made the quotes consistent.

--- spec/presenters/attachment_presenter_spec.rb
+++ spec/presenters/attachment_presenter_spec.rb
@@ -1,11 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
+require "app/presenters/attachment_presenter"
 
 RSpec.describe AttachmentPresenter do
   let(:content_id) { SecureRandom.uuid }
   let(:attachment) {
-    Attachment.new(url: 'path/to/file/in/asset/manger',
-      content_type: 'application/jpeg',
-      title: 'test specialist document attachment',
+    Attachment.new(url: "path/to/file/in/asset/manger",
+      content_type: "application/jpeg",
+      title: "test specialist document attachment",
       content_id: content_id)
   }
 
@@ -26,9 +27,9 @@ RSpec.describe AttachmentPresenter do
 
     it "renders the correct data" do
       expect(presented_data[:url]).to eq("path/to/file/in/asset/manger")
-      expect(presented_data[:content_type]).to eq('application/jpeg')
+      expect(presented_data[:content_type]).to eq("application/jpeg")
       expect(presented_data[:content_id]).to eq(content_id)
-      expect(presented_data[:title]).to eq('test specialist document attachment')
+      expect(presented_data[:title]).to eq("test specialist document attachment")
       expect(presented_data[:created_at]).to eq("2015-12-03T16:59:13+00:00")
     end
 
