64de500189abf097c8811e9c662c062ba772be78
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-10-18 16:11:25 +0000

    Add response errors and warnings to Document
    Whether to publish or save a document relies not only on
    its validity when initialized from a form but also the response
    obtained from the publishing api. Warnings do no prevent a
    document from being saved but may prevent it being published.
    Certain errors where a base path is already in use need to be
    propagated up to the view so these are also stored on the document.

--- app/helpers/publishing_helper.rb
+++ app/helpers/publishing_helper.rb
@@ -4,8 +4,18 @@ module PublishingHelper
       block.call
       true
     rescue GdsApi::HTTPErrorResponse => e
+      @publishable.errors.add(:base, e.message) if @publishable
       Airbrake.notify(e)
       false
     end
+  end
+
+  def set_errors_on(obj)
+    unless obj.class.include?(ActiveModel::Validations)
+      raise ArgumentError.new(
+        "Can only set errors on an object which includes ActiveModel::Validations")
+    end
+
+    @publishable = obj
   end
 end
