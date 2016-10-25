3ae8027a8ebd02f50f5068c3f6634a4c9d119a4a
Tijmen Brommet <tijmen@gmail.com>
2016-10-25 09:54:37 +0100

    Don't load ActionMailer framework
    This app doesn't send email directly so we don't need to load
    ActionMailer. This saves memory and makes the app boot faster.

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "sidekiq/api"
