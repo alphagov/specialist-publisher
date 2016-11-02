47b2d1d0c53024608718510d7221ca31f50472f1
Tijmen Brommet <tijmen@gmail.com>
2016-11-02 12:47:03 +0000

    Extract DocumentFinder from Document

--- app/models/document.rb
+++ app/models/document.rb
@@ -179,26 +179,8 @@ class Document
   end
 
   def self.find(content_id)
-    begin
-      response = Services.publishing_api.get_content(content_id)
-    rescue GdsApi::HTTPNotFound
-      raise RecordNotFound, "Document: #{content_id}"
-    end
-
-    attributes = response.to_hash
-    document_type = attributes.fetch("document_type")
-    document_class = document_type.camelize.constantize
-
-    if [document_class, Document].include?(self)
-      document_class.from_publishing_api(response.to_hash)
-    else
-      message = "#{self}.find('#{content_id}') returned the wrong type: '#{document_class}'"
-      raise TypeMismatchError, message
-    end
+    DocumentFinder.find(self, content_id)
   end
-
-  class RecordNotFound < StandardError; end
-  class TypeMismatchError < StandardError; end
 
   def save(validate: true)
     return false if validate && !self.valid?
