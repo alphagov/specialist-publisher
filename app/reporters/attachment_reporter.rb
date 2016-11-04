60533667101af9c8d1a894b80fb7ac1e4947cc19
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-11-04 13:58:45 +0000

    Faster AttachmentReporter report
    This refactors the AttachmentReporter to no longer have a nested loop
    and instead use ruby array methods to compare arrays. It is
    substantially quicker than the previous code and thus should resolve the
    timeout errors that have been occurring.
    
    We also make more clear attachments that have matching snippets.

--- app/reporters/attachment_reporter.rb
+++ app/reporters/attachment_reporter.rb
@@ -29,42 +29,73 @@ private
   attr_accessor :document
 
   def build_report_data
-    used = []
-    matched = []
-    unmatched = []
+    sanitised_attachment_snippets = attachment_snippets.map { |s| s[:sanitised_snippet] }
+    sanitised_body_snippets = body_snippets.map { |s| s[:sanitised_snippet] }
 
-    body_snippets.each do |b|
-      match = false
+    unused_sanitised_attachment_snippets = sanitised_attachment_snippets - sanitised_body_snippets
+    unused_sanitised_body_snippets = sanitised_body_snippets - sanitised_attachment_snippets
 
-      attachment_snippets.each do |a|
-        if presenter.snippets_match?(a, b)
-          matched.push(b)
-          used |= [a]
-          match = true
-        end
-      end
+    used = used_attachments(attachment_snippets, sanitised_attachment_snippets - unused_sanitised_attachment_snippets)
+    unused = unused_attachments(attachment_snippets, unused_sanitised_attachment_snippets)
 
-      unmatched.push(b) unless match
-    end
-    unused = attachment_snippets - used
+    matched = match_snippets(body_snippets, sanitised_body_snippets - unused_sanitised_body_snippets)
+    unmatched = match_snippets(body_snippets, unused_sanitised_body_snippets)
 
-    filenames_only([used, unused, matched, unmatched])
+    [used, unused, matched, unmatched].map { |arr| filenames_only(arr) }
   end
 
   def body_snippets
-    @body_snippets ||= presenter.snippets_in_body
+    @body_snippets ||= presenter.snippets_in_body.map do |snippet|
+      {
+        snippet: snippet,
+        sanitised_snippet: sanitise_snippet(snippet),
+        filename: filename_from_snippet(snippet)
+      }
+    end
   end
 
   def attachment_snippets
-    @attachment_snippets ||= document.attachments.map(&:snippet)
+    @attachment_snippets ||= document.attachments.map do |attachment|
+      {
+        snippet: attachment.snippet,
+        sanitised_snippet: sanitise_snippet(attachment.snippet),
+        filename: filename_from_snippet(attachment.snippet)
+      }
+    end
   end
 
-  def filenames_only(arrays)
-    arrays.map do |array|
-      array.map do |snippet|
-        snippet[/\[InlineAttachment:(.*?)\]/, 1]
-      end
+  def sanitise_snippet(snippet)
+    presenter.sanitise_snippet(snippet)
+  end
+
+  def used_attachments(attachment_snippets, matched_sanitised_snippets)
+    # only match first instance of an attachment with a non-unique sanitised
+    # snippet, as latter ones will be ignored.
+    first_attachments = attachment_snippets.uniq { |a| a[:sanitised_snippet] }
+    match_snippets(first_attachments, matched_sanitised_snippets)
+  end
+
+  def unused_attachments(attachment_snippets, matched_sanitised_snippets)
+    # include attachments that have a non-unique sanitised snippet ignoring
+    # their first occurence as they won't be matched.
+    occurences = Hash.new(0)
+    attachment_snippets.select do |s|
+      sanitised_snippet = s[:sanitised_snippet]
+      occurences[sanitised_snippet] += 1
+      matched_sanitised_snippets.include?(sanitised_snippet) || occurences[sanitised_snippet] > 1
     end
+  end
+
+  def match_snippets(snippets, matched_sanitised_snippets)
+    snippets.select { |s| matched_sanitised_snippets.include?(s[:sanitised_snippet]) }
+  end
+
+  def filenames_only(snippets)
+    snippets.map { |s| s[:filename] }
+  end
+
+  def filename_from_snippet(snippet)
+    snippet[/\[InlineAttachment:(.*?)\]/, 1]
   end
 
   def presenter
