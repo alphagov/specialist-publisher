07aa43c3ff64e074870ffd14a3670e1b0929f199
Murray Steele <muz@h-lame.com>
2017-09-05 13:55:09 +0000

    Add AllDocumentsFinder.find_each
    This talks to the publishing api and fetches every page of results for a
    document type so that callers can iterate over all the documents without
    having to handle pagination themselves.  We use `find_each` for this to
    mirror the function from AR that does something similar by batching up
    records to avoid large queries.

--- app/services/all_documents_finder.rb
+++ app/services/all_documents_finder.rb
@@ -1,9 +1,15 @@
 # Request all documents for a certain document type, paginated
 class AllDocumentsFinder
-  def self.all(page, per_page, q, document_type)
-    params = {
+  def self.default_find_params(document_type)
+    {
       publishing_app: "specialist-publisher",
       document_type: document_type,
+      order: "-last_edited_at",
+    }
+  end
+
+  def self.all(page, per_page, q, document_type)
+    params = default_find_params(document_type).merge(
       fields: [
         :base_path,
         :content_id,
@@ -15,9 +21,27 @@ class AllDocumentsFinder
       page: page,
       per_page: per_page,
       order: "-last_edited_at",
-    }
+    )
     params[:q] = q if q.present?
 
     Services.publishing_api.get_content_items(params)
+  end
+
+  def self.find_each(klass)
+    params = default_find_params(klass.document_type).merge(per_page: 50)
+
+    current_page = 1
+    loop do
+      response = Services.publishing_api.get_content_items(params.merge(page: current_page))
+
+      response['results'].each do |document|
+        yield klass.from_publishing_api(document)
+      end
+      if response['pages'] > current_page
+        current_page += 1
+      else
+        break
+      end
+    end
   end
 end
---
+++ spec/services/all_documents_finder_spec.rb
@@ -0,0 +1,131 @@
+require 'spec_helper'
+
+RSpec.describe AllDocumentsFinder do
+  subject { described_class }
+
+  describe '.find_each' do
+    it "does not yield anything if the publishing api response is empty" do
+      publishing_api_has_no_content(BusinessFinanceSupportScheme)
+
+      found_docs = []
+      subject.find_each(BusinessFinanceSupportScheme) do |doc|
+        found_docs << doc
+      end
+
+      expect(found_docs.length).to eq(0)
+    end
+
+    it "yields each document from the publishing api response" do
+      documents = [
+        FactoryGirl.create(
+          :business_finance_support_scheme,
+          base_path: '/bfss/1',
+          title: 'Scheme #1'
+        ),
+        FactoryGirl.create(
+          :business_finance_support_scheme,
+          base_path: '/bfss/2',
+          title: 'Scheme #2'
+        )
+      ]
+      publishing_api_has_content(documents, hash_including(document_type: BusinessFinanceSupportScheme.document_type, page: '1'))
+
+      found_docs = []
+      subject.find_each(BusinessFinanceSupportScheme) do |doc|
+        found_docs << doc
+      end
+
+      expect(found_docs.length).to eq(2)
+      expect(found_docs[0]).to be_a BusinessFinanceSupportScheme
+      expect(found_docs[0].title).to eq 'Scheme #1'
+      expect(found_docs[1]).to be_a BusinessFinanceSupportScheme
+      expect(found_docs[1].title).to eq 'Scheme #2'
+    end
+
+    it "fetches each page of documents and yields all of them" do
+      documents = [
+        FactoryGirl.create(
+          :business_finance_support_scheme,
+          base_path: '/bfss/1',
+          title: 'Scheme #1'
+        ),
+        FactoryGirl.create(
+          :business_finance_support_scheme,
+          base_path: '/bfss/2',
+          title: 'Scheme #2'
+        ),
+        FactoryGirl.create(
+          :business_finance_support_scheme,
+          base_path: '/bfss/3',
+          title: 'Scheme #3'
+        ),
+        FactoryGirl.create(
+          :business_finance_support_scheme,
+          base_path: '/bfss/4',
+          title: 'Scheme #4'
+        )
+      ]
+      pagination_calls = publishing_api_paginates_content(documents, 2, BusinessFinanceSupportScheme)
+
+      found_docs = []
+      subject.find_each(BusinessFinanceSupportScheme) do |doc|
+        found_docs << doc
+      end
+
+      # 1. assert that we got all the docs
+      expect(found_docs.length).to eq(4)
+
+      expect(found_docs[0]).to be_a BusinessFinanceSupportScheme
+      expect(found_docs[0].title).to eq 'Scheme #1'
+      expect(found_docs[1]).to be_a BusinessFinanceSupportScheme
+      expect(found_docs[1].title).to eq 'Scheme #2'
+
+      expect(found_docs[2]).to be_a BusinessFinanceSupportScheme
+      expect(found_docs[2].title).to eq 'Scheme #3'
+      expect(found_docs[3]).to be_a BusinessFinanceSupportScheme
+      expect(found_docs[3].title).to eq 'Scheme #4'
+
+      # 2. assert that this wasn't the result of a single api call
+      expect(pagination_calls.length).to eq(2)
+      pagination_calls.each do |pagination_call|
+        expect(pagination_call).to have_been_requested
+      end
+    end
+  end
+
+  # NOTE: we do this manually because the publishing_api_has_content test helper
+  # is too restrictive and we can't properly control pagination
+  def publishing_api_paginates_content(content_items, per_page, document_klass)
+    total_pages = content_items.length / per_page
+    total_pages += 1 unless content_items.length.remainder(per_page).zero?
+    if total_pages.zero?
+      publishing_api_has_no_content(document_klass)
+    else
+      content_items.each_slice(per_page).with_index.map do |page_items, index|
+        body = {
+          results: page_items,
+          total: content_items.length,
+          pages: total_pages,
+          current_page: index + 1
+        }
+
+        stub_request(:get, GdsApi::TestHelpers::PublishingApiV2::PUBLISHING_API_V2_ENDPOINT + '/content')
+          .with(query: hash_including(page: (index + 1).to_s, document_type: document_klass.document_type))
+          .to_return(status: 200, body: body.to_json, headers: {})
+      end
+    end
+  end
+
+  def publishing_api_has_no_content(document_klass)
+    body = {
+      results: [],
+      total: 0,
+      pages: 0,
+      current_page: 1
+    }
+
+    stub_request(:get, GdsApi::TestHelpers::PublishingApiV2::PUBLISHING_API_V2_ENDPOINT + '/content')
+      .with(query: hash_including(page: '1', document_type: document_klass.document_type))
+      .to_return(status: 200, body: body.to_json, headers: {})
+  end
+end
