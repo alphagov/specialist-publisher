96e1444131e0a9978d3466ad7e66ca0006b5df9a
Murray Steele <muz@h-lame.com>
2017-09-06 16:23:23 +0000

    Introduce NotificationsMailer for sending emails to users
    This includes the document_list email for sending them the CSV they have
    requested of some exported documents.

---
+++ app/mailers/notifications_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationsMailer < ApplicationMailer
+  def document_list(csv, user, document_klass, query)
+    attachments['document_list.csv'] = csv
+
+    @user = user
+    @document_klass = document_klass
+    @query = query
+    mail to: user.email, subject: "Your exported list of #{document_klass.title.pluralize} from GOV.UK"
+  end
+end
