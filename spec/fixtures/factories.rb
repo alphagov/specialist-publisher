ac7824aae026f83488c7902b7c88b3c2ea49a002
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-04-25 11:01:33 +0100

    Revert "Add new metadata fields to service standard reports"

--- spec/fixtures/factories.rb
+++ spec/fixtures/factories.rb
@@ -187,10 +187,7 @@ FactoryBot.define do
     transient do
       default_metadata do
         {
-          "assessment_date" => "2016-10-10",
-          "result" => "met",
-          "service_provider" => "fef4ac7c-024a-4943-9f19-e85a8369a1f3",
-          "stage" => "live",
+          "assessment_date" => "2016-10-10"
         }
       end
     end
