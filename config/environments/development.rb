988059e05e70047feca47ef89bb0709506a3a6a1
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-02-25 10:27:27 +0000

    Use GOV.UK Notify for ActionMailer messages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :file
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
