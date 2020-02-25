7b0b9ff7f5db60a9299d822d30e7430ce6c087cc
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-02-25 16:49:25 +0000

    Remove leftover delivery_method parameter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.delivery_method = :ses
 end
