560e9a0e9a2c7c62eea8b7075e7ef0bea81e6b5c
Michael Walker <mike@barrucadu.co.uk>
2020-03-06 11:32:39 +0000

    Fix Lint/NonDeterministicRequireOrder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,9 +15,10 @@ WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 Dir[Rails.root.join("features/support/**/*_helpers.rb")]
   .reject { |f| f =~ %r{/api_helpers.rb$} }
+  .sort
   .each { |f| require f }
 
 # Quiet down now Mongo
