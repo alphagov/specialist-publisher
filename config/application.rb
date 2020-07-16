061cd3d679084a2564a658bbc18df5e93ed693bf
Andrew White <andrew.white@unboxed.co>
2020-07-16 18:00:51 +0100

    Fix asset pipeline with libsass
    Using a sass css compressor causes a scss file to be processed twice
    (once to build, once to compress) which breaks the usage of "unquote"
    to use CSS that has same function names as SCSS such as max.

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,11 @@ module SpecialistPublisher
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = "London"
+
+    # Using a sass css compressor causes a scss file to be processed twice
+    # (once to build, once to compress) which breaks the usage of "unquote"
+    # to use CSS that has same function names as SCSS such as max.
+    # https://github.com/alphagov/govuk-frontend/issues/1350
+    config.assets.css_compressor = nil
   end
 end
