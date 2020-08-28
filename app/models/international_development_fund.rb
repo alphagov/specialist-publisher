ab0923e940544c160f51eb70e6852389307dbe13
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2020-08-28 11:44:33 +0100

    Move International Development Funds to FCDO

--- app/models/international_development_fund.rb
+++ app/models/international_development_fund.rb
@@ -22,6 +22,6 @@ class InternationalDevelopmentFund < Document
   end
 
   def primary_publishing_organisation
-    "db994552-7644-404d-a770-a2fe659c661f"
+    "f9fcf3fe-2751-4dca-97ca-becaeceb4b26"
   end
 end
