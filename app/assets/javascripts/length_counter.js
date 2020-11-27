b63de7d1d4edbe7dadceb88641b555012aec9bad
Kevin Dew <kevindew@me.com>
2020-11-27 20:23:22 +0000

    Automatic JS linting fixes
    npx standardx --fix 'app/assets/javascripts/*/**.js'

--- app/assets/javascripts/length_counter.js
+++ app/assets/javascripts/length_counter.js
@@ -1,52 +1,51 @@
-(function($) {
-  "use strict";
-  window.GOVUK = window.GOVUK || {};
+(function ($) {
+  'use strict'
+  window.GOVUK = window.GOVUK || {}
 
-  function LengthCounter(options){
-    this.$input = $(options.$el);
+  function LengthCounter (options) {
+    this.$input = $(options.$el)
 
-    if(this.$input.length > 0){
+    if (this.$input.length > 0) {
       this.$input.on('keyup', $.proxy(this.checkLength, this))
-      this.$message = $(this.$input.data('countMessageSelector'));
-      this.$count = this.$message.find('.count');
-      this.threshold = this.$input.data('countMessageThreshold');
-      this.hideMessage();
-      this.$input.addClass('length-input');
+      this.$message = $(this.$input.data('countMessageSelector'))
+      this.$count = this.$message.find('.count')
+      this.threshold = this.$input.data('countMessageThreshold')
+      this.hideMessage()
+      this.$input.addClass('length-input')
     } else {
-      return;
+
     }
   }
 
-  LengthCounter.prototype.checkLength = function() {
-    var length = this.$input.val().length;
-    this.$count.text('Current length: '+length);
+  LengthCounter.prototype.checkLength = function () {
+    var length = this.$input.val().length
+    this.$count.text('Current length: ' + length)
 
     if (length > this.threshold) {
-      this.addWarningClass();
-      this.showMessage();
+      this.addWarningClass()
+      this.showMessage()
     } else {
-      this.removeWarningClass();
+      this.removeWarningClass()
     }
-  };
+  }
 
-  LengthCounter.prototype.showMessage = function() {
-    this.$message.show();
-  };
+  LengthCounter.prototype.showMessage = function () {
+    this.$message.show()
+  }
 
-  LengthCounter.prototype.hideMessage = function() {
-    this.$message.hide();
-  };
+  LengthCounter.prototype.hideMessage = function () {
+    this.$message.hide()
+  }
 
-  LengthCounter.prototype.addWarningClass = function() {
-    this.$input.addClass('warning');
-    this.$message.addClass('warning');
-  };
+  LengthCounter.prototype.addWarningClass = function () {
+    this.$input.addClass('warning')
+    this.$message.addClass('warning')
+  }
 
-  LengthCounter.prototype.removeWarningClass = function() {
-    this.$input.removeClass('warning');
-    this.$message.removeClass('warning');
-  };
+  LengthCounter.prototype.removeWarningClass = function () {
+    this.$input.removeClass('warning')
+    this.$message.removeClass('warning')
+  }
 
-  GOVUK.LengthCounter = LengthCounter;
-
-}(jQuery));
+  GOVUK.LengthCounter = LengthCounter
+}(jQuery))
