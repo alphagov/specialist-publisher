640e117a8a63ec6f5b10ca2458b5f7e008ad8581
Barbara Slawinska <barbara.slawinska@digital.cabinet-office.gov.uk>
2022-02-22 12:55:17 +0000

    Add validation according to requirements
    Also added test coverage to ensure the process of creating DRCF digital markets research is working correctly.

--- app/models/drcf_digital_markets_research.rb
+++ app/models/drcf_digital_markets_research.rb
@@ -1,4 +1,8 @@
 class DrcfDigitalMarketsResearch < Document
+  validates :digital_market_research_category, presence: true
+  validates :digital_market_research_publisher, presence: true
+  validates :digital_market_research_area, presence: true
+  validates :digital_market_research_topic, presence: true
   validates :digital_market_research_publish_date,
             presence: true,
             date: true
