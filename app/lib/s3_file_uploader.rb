29bafb9fe70b3fe5dbe21cb9392b09e9dd399589
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2022-05-18 15:21:37 +0100

    Edit S3 file uploader to work with AWS S3 healthchecker
    AWS S3 healthcheck requires a `connection` method so the connection
    portion of `save_file_to_s3` has been moved into a separate
    method called `connection`.

--- app/lib/s3_file_uploader.rb
+++ app/lib/s3_file_uploader.rb
@@ -1,17 +1,19 @@
 class S3FileUploader
   def self.save_file_to_s3(filename, csv)
-    connection = Fog::Storage.new(
-      provider: "AWS",
-      region: ENV["AWS_REGION"],
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    )
-
     directory = connection.directories.get(ENV["AWS_S3_BUCKET_NAME"])
 
     directory.files.create(
       key: filename,
       body: csv,
+    )
+  end
+
+  def self.connection
+    @connection ||= Fog::Storage.new(
+      provider: "AWS",
+      region: ENV["AWS_REGION"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     )
   end
 end
