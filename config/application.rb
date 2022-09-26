af756ae283f126e2384e65be236810c0bd7e249a
Kevin Dew <kevindew@me.com>
2022-09-26 18:17:35 +0100

    Run rails app:update
    This goes through and applies all the minor changes Rails suggests
    through the update process.

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,19 @@
 require_relative "boot"
 
 require "rails"
-
+# Pick the frameworks you want:
 require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
 require "sprockets/railtie"
-require "sidekiq/api"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,15 +24,15 @@ module SpecialistPublisher
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = "London"
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
 
     # Using a sass css compressor causes a scss file to be processed twice
     # (once to build, once to compress) which breaks the usage of "unquote"
