ff71421afb5c59cf907e05eefbe3c74d14c9db89
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-01-12 16:31:13 +0000

    Pass file paths to BulkIndustrySectorsImporter
    This allows the file path methods to be made private, and to use fixture
    data in the tests.
    If no file paths are passed in, defaults are used.

--- lib/importers/licence_transaction/bulk_industry_sectors_importer.rb
+++ lib/importers/licence_transaction/bulk_industry_sectors_importer.rb
@@ -1,12 +1,19 @@
 module Importers
   module LicenceTransaction
     class BulkIndustrySectorsImporter
+      attr_accessor :data_file_path, :schema_file_path
+
+      def initialize(data_file_path: nil, schema_file_path: nil)
+        @data_file_path = (data_file_path.presence || file_path)
+        @schema_file_path = (schema_file_path.presence || schema_path)
+      end
+
       def call
         update_industry_sectors_in_schema(imported_json_data)
       end
 
       def imported_json_data
-        lines = File.new(file_path).readlines(chomp: true)
+        lines = File.new(data_file_path).readlines(chomp: true)
         formatted = lines.map do |line|
           {
             label: line.strip.to_s,
@@ -18,13 +25,15 @@ module Importers
       end
 
       def update_industry_sectors_in_schema(industry_sectors)
-        json_blob = File.new(schema_path).read
+        json_blob = File.new(schema_file_path).read
         schema = JSON.parse(json_blob)
         licence_transaction_industry = schema["facets"].select { |facet| facet["key"] == "licence_transaction_industry" }
         licence_transaction_industry.first["allowed_values"] = industry_sectors
 
-        File.write(schema_path, JSON.dump(schema))
+        File.write(schema_file_path, JSON.dump(schema))
       end
+
+    private
 
       def file_path
         Rails.root.join("lib/data/licence_transaction/industry_sectors.txt")
--- spec/lib/importers/licence_transaction/bulk_industry_sectors_importer_spec.rb
+++ spec/lib/importers/licence_transaction/bulk_industry_sectors_importer_spec.rb
@@ -3,16 +3,16 @@ require "importers/licence_transaction/bulk_industry_sectors_importer"
 
 RSpec.describe Importers::LicenceTransaction::BulkIndustrySectorsImporter do
   describe "#imported_json_data" do
-    let(:subject) { described_class.new }
+    let(:data_file_path) { Rails.root.join("spec/fixtures/licence-transaction/industry_sectors.txt") }
+    let(:schema_file_path) { Rails.root.join("spec/fixtures/document/schemas/licence-transactions.json") }
 
-    before do
-      @sectors_data_file = File.open(subject.file_path, "r")
-    end
+    let(:subject) { described_class.new(data_file_path:, schema_file_path:) }
 
     it "returns all of the level 2 sectors" do
       sectors = subject.imported_json_data
+      sectors_data_file = File.open(subject.data_file_path, "r")
 
-      expect(sectors.size).to eq(@sectors_data_file.readlines.size)
+      expect(sectors.size).to eq(sectors_data_file.readlines.size)
     end
 
     it "returns a label and value for each sector" do
