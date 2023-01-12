e61b73d2803fa4ab90020f73f1886ff09dfcc152
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-01-12 17:51:03 +0000

    Add test for writing updated sectors to JSON file

--- spec/lib/importers/licence_transaction/bulk_industry_sectors_importer_spec.rb
+++ spec/lib/importers/licence_transaction/bulk_industry_sectors_importer_spec.rb
@@ -2,12 +2,12 @@ require "spec_helper"
 require "importers/licence_transaction/bulk_industry_sectors_importer"
 
 RSpec.describe Importers::LicenceTransaction::BulkIndustrySectorsImporter do
+  let(:data_file_path) { Rails.root.join("spec/fixtures/licence-transaction/industry_sectors.txt") }
+  let(:schema_file_path) { Rails.root.join("spec/fixtures/documents/schemas/licence_transactions.json") }
+
+  let(:subject) { described_class.new(data_file_path:, schema_file_path:) }
+
   describe "#imported_json_data" do
-    let(:data_file_path) { Rails.root.join("spec/fixtures/licence-transaction/industry_sectors.txt") }
-    let(:schema_file_path) { Rails.root.join("spec/fixtures/document/schemas/licence-transactions.json") }
-
-    let(:subject) { described_class.new(data_file_path:, schema_file_path:) }
-
     it "returns all of the level 2 sectors" do
       sectors = subject.imported_json_data
       sectors_data_file = File.open(subject.data_file_path, "r")
@@ -20,6 +20,17 @@ RSpec.describe Importers::LicenceTransaction::BulkIndustrySectorsImporter do
 
       expect(sectors.first).to have_key("label")
       expect(sectors.first).to have_key("value")
+    end
+  end
+
+  describe "#update_industry_sectors_in_schema" do
+    it "writes imported sectors to JSON file" do
+      json_blob = File.new(schema_file_path).read
+      expected_schema = JSON.dump(JSON.parse(json_blob))
+      sectors = subject.imported_json_data
+
+      expect(File).to receive(:write).with(schema_file_path, expected_schema)
+      subject.update_industry_sectors_in_schema(sectors)
     end
   end
 end
