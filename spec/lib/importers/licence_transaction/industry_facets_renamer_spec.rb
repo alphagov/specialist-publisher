5c0ac7c0b9c42a3ea3c5276d697f8fdbb612724b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-02-14 14:32:22 +0000

    Fix up to remove call method

--- spec/lib/importers/licence_transaction/industry_facets_renamer_spec.rb
+++ spec/lib/importers/licence_transaction/industry_facets_renamer_spec.rb
@@ -5,42 +5,6 @@ RSpec.describe Importers::LicenceTransaction::IndustryFacetsRenamer do
   let(:csv_file_path) { Rails.root.join("spec/fixtures/licence-transaction/renamed_industries.csv") }
   let(:schema_file_path) { Rails.root.join("spec/fixtures/documents/schemas/licence_transactions_with_renamed_industries.json") }
 
-  let(:document) do
-    FactoryBot.create(
-      :licence_transaction,
-      base_path: "/find-licences/1",
-      title: "Licence #1",
-      default_metadata: {
-        "licence_transaction_industry" => %w[
-          accommodation
-          arts-and-entertainment
-        ],
-      },
-    )
-  end
-
-  let(:licence_transaction) do
-    FactoryBot.build(
-      :licence_transaction_model,
-      base_path: "/find-licences/1",
-      title: "Licence #1",
-      licence_transaction_industry: %w[
-        accommodation
-        arts-and-entertainment
-      ],
-    )
-  end
-
-  before do
-    stub_publishing_api_has_content(
-      [document],
-      hash_including(document_type: "licence_transaction", page: "1"),
-    )
-
-    stub_any_publishing_api_put_content
-    stub_any_publishing_api_patch_links
-  end
-
   describe "#update_schema" do
     it "writes imported sectors to JSON schema file" do
       json_blob = File.new(schema_file_path).read
@@ -53,6 +17,36 @@ RSpec.describe Importers::LicenceTransaction::IndustryFacetsRenamer do
 
   describe "#update_licence_transactions" do
     it "updates licences with the new industry values" do
+      document = FactoryBot.create(
+        :licence_transaction,
+        base_path: "/find-licences/1",
+        title: "Licence #1",
+        default_metadata: {
+          "licence_transaction_industry" => %w[
+            accommodation
+            arts-and-entertainment
+          ],
+        },
+      )
+
+      FactoryBot.build(
+        :licence_transaction_model,
+        base_path: "/find-licences/1",
+        title: "Licence #1",
+        licence_transaction_industry: %w[
+          accommodation
+          arts-and-entertainment
+        ],
+      )
+
+      stub_publishing_api_has_content(
+        [document],
+        hash_including(document_type: "licence_transaction", page: "1"),
+      )
+
+      stub_any_publishing_api_put_content
+      stub_any_publishing_api_patch_links
+
       described_class.new(csv_file_path:, schema_file_path:).update_licence_transactions
 
       expected_details_hash = {
