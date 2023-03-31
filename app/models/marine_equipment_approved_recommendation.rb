ee36556c9166891cd0b527f46dc850a6b657ed76
Minno Dang <minno.dang@digital.cabinet-office.gov.uk>
2023-03-31 17:54:35 +0100

    [1141] - Added new specialist publisher for Marine Equipment Approved Recommendations
    Maritime Coastguard Agency requested a new specialist publisher and finder for UK Marine Equipment Approved Recommendations.
    Added required models, views and schemas for publishing and managing the new document type.
    The fields Year Adopted, Keyword, and Reference Number are set to non-filterable. Original requirements asked for them to be filterable.
    However, due to Elasticsearch having to be able to reindex over new free text values, and they are non aggregated fields, meaning filtering on free text is currently not supported functionality on search-api.
    There is an existing search box to free text search to serve lookup requirements.

---
+++ app/models/marine_equipment_approved_recommendation.rb
@@ -0,0 +1,31 @@
+class MarineEquipmentApprovedRecommendation < Document
+  validates :category, presence: true
+  validates :year_adopted, presence: true, format: /\A[1-9][0-9]{3}\z/
+  validates :reference_number, presence: true
+  validates :keyword, presence: true
+
+  FORMAT_SPECIFIC_FIELDS = %i[
+    category
+    year_adopted
+    reference_number
+    keyword
+  ].freeze
+
+  attr_accessor(*FORMAT_SPECIFIC_FIELDS)
+
+  def initialize(params = {})
+    super(params, FORMAT_SPECIFIC_FIELDS)
+  end
+
+  def taxons
+    []
+  end
+
+  def self.title
+    "Marine Equipment Approved Recommendation"
+  end
+
+  def primary_publishing_organisation
+    "23a24aa8-1711-42b6-bf6b-47af0f230295"
+  end
+end
