e10f66a94cd0ff40daa4a89fee2068eee72549ca
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2023-05-10 14:13:13 +0100

    Adds uniqueness validator for licence identifier
    We expect each licence to either have a blank licence identifier (if
    linked to an external website) or a unique identifier if managed by a
    local authority. The validator pulls in all the licences from Publishing
    API and checks whether the identifier, if present, already exists and
    fails if it does.

---
+++ app/validators/licence_identifier_unique_validator.rb
@@ -0,0 +1,15 @@
+class LicenceIdentifierUniqueValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return if value.blank?
+
+    existing_licence_identifiers = []
+
+    record.class.find_each do |licence|
+      existing_licence_identifiers << licence.licence_transaction_licence_identifier
+    end
+
+    if existing_licence_identifiers.include?(value)
+      record.errors.add(attribute, :taken)
+    end
+  end
+end
