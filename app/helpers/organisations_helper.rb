1c362618ee88f4d46fe15704e6b9a49448b5136b
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2023-06-14 11:31:46 +0100

    Updates method to use passed in organisation id
    Makes the method more flexible as we don't need a form object to work
    out whether there is an org param present.

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -3,9 +3,7 @@ module OrganisationsHelper
     Organisation.all.map { |o| [o.title, o.content_id] }
   end
 
-  def primary_publishing_organisation_options(form)
-    return {} if form.object.primary_publishing_organisation.present?
-
-    { selected: current_user.organisation_content_id }
+  def selected_organisation_or_current(organisation)
+    (organisation.presence || current_user.organisation_content_id)
   end
 end
