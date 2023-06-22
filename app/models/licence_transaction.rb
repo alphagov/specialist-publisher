202afe44e7e22d1f49120cf43efa72075455b759
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2023-06-22 13:01:52 +0100

    Allow custom error messages
    We want to use error messages in accordance with the design system
    recommendations for the new licence document type. However, adding
    custom error messages isn't really supported in Specialist Publisher due
    to the default "<attribute> <error_message>" outputted (full_messages).
    
    We want to swap over all the error messages to the new pattern but we're
    just implementing this for the licence document type for now.
    
    Also now fetches error messages from locale files which are more
    maintainable.

--- app/models/licence_transaction.rb
+++ app/models/licence_transaction.rb
@@ -1,4 +1,21 @@
 class LicenceTransaction < Document
+  FORMAT_SPECIFIC_FIELDS = %i[
+    licence_transaction_continuation_link
+    licence_transaction_industry
+    licence_transaction_licence_identifier
+    licence_transaction_location
+    licence_transaction_will_continue_on
+  ].freeze
+
+  CUSTOM_ERROR_MESSAGE_FIELDS = FORMAT_SPECIFIC_FIELDS + %i[
+    primary_publishing_organisation
+    body
+    title
+    summary
+    update_type
+    change_note
+  ]
+
   validates_with LinkOrIdentifierValidator
   validates :primary_publishing_organisation, presence: true
   validates :licence_transaction_licence_identifier, licence_identifier_unique: true
@@ -8,14 +25,6 @@ class LicenceTransaction < Document
     with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
     allow_blank: true,
   }
-
-  FORMAT_SPECIFIC_FIELDS = %i[
-    licence_transaction_continuation_link
-    licence_transaction_industry
-    licence_transaction_licence_identifier
-    licence_transaction_location
-    licence_transaction_will_continue_on
-  ].freeze
 
   attr_accessor(*FORMAT_SPECIFIC_FIELDS, :organisations, :primary_publishing_organisation)
 
@@ -54,5 +63,9 @@ class LicenceTransaction < Document
 
   def rendering_app
     "frontend"
+  end
+
+  def custom_error_message_fields
+    CUSTOM_ERROR_MESSAGE_FIELDS
   end
 end
