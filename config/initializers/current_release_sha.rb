3eb537feb47978a8174d743f51953b98bfe28714
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2023-09-27 14:57:58 +0100

    Fix release version in footer
    This has been broken since we moved to EKS and the SHA is no longer
    provided.
    
    We can use the [SENTRY_RELEASE](https://github.com/alphagov/govuk-helm-charts/blob/077117e7f40c096a958f9e0b853a2bd76cf43f26/charts/generic-govuk-app/templates/deployment.yaml#L92C1-L92C1) environment variable which is defined for every app and takes its value from
    the container image's tag.
    
    If for whatever reason the image tag is not defined it will display
    `null`, which should make it clearer (instead of displaying `development`)
    that this data is not being pulled in successfully.

--- config/initializers/current_release_sha.rb
+++
@@ -1,7 +0,0 @@
-revision_file = Rails.root.join("REVISION")
-if File.exist?(revision_file)
-  revision = File.read(revision_file).strip
-  CURRENT_RELEASE_SHA = revision[0..10] # Just get the short SHA
-else
-  CURRENT_RELEASE_SHA = "development".freeze
-end
