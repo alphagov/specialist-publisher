6bb48281d3fa4df469e7526e06c504d198c026de
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2024-04-16 18:35:47 +0100

    Remove internal note feature
    The functionality for adding an internal note was developed for the UK Market Conformity
    Assessment Bodies document type. The finder was unpublished alongside its documents,
    and in order to support that process, the internal note served the purpose of documenting
    the reasons why a specific document was unpublished.
    
    We are now removing the finder code in order to prevent accidentally republishing it.
    Since there are no current usages of the feature as it has not been used for other
    finders and it is not a development priority or business request, we are removing all
    associated internal note implementation.

--- app/services/document_unpublisher.rb
+++ app/services/document_unpublisher.rb
@@ -2,13 +2,12 @@ require "services"
 
 # Unpublish a document. Also removes attachments.
 class DocumentUnpublisher
-  def self.unpublish(content_id, locale, _base_path, alternative_path = nil, internal_notes = nil)
+  def self.unpublish(content_id, locale, _base_path, alternative_path = nil)
     if alternative_path.blank?
       Services.publishing_api.unpublish(
         content_id,
         type: "gone",
         locale:,
-        explanation: internal_notes,
       )
     else
       Services.publishing_api.unpublish(
@@ -16,7 +15,6 @@ class DocumentUnpublisher
         type: "redirect",
         locale:,
         alternative_path:,
-        explanation: internal_notes,
       )
     end
 
