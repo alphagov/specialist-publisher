30647c7bd7866d03697632540424de551a0a1160
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-07-15 11:40:56 +0100

    Annotate FinderLoader unit tests with `describe`
    Make it clear exactly which methods we're testing.

--- spec/lib/finder_loader_spec.rb
+++ spec/lib/finder_loader_spec.rb
@@ -7,40 +7,45 @@ RSpec.describe FinderLoader do
     expect(File).to receive(:mtime).with("lib/documents/schemas/format-1.json")
                       .and_return("yesterday")
   end
-  it "returns matching finder data objects" do
-    expect(Dir).to receive(:glob).with("lib/documents/schemas/*.json").and_return(%w[
-      lib/documents/schemas/format-1.json
-      lib/documents/schemas/format-2.json
-    ])
-    expect(File).to receive(:read).with("lib/documents/schemas/format-2.json")
-      .and_return('{"name":"format-2"}')
-    expect(File).to receive(:mtime).with("lib/documents/schemas/format-2.json")
-      .and_return("today")
 
-    loader = FinderLoader.new
-    expect(loader.finders).to match_array([
-      {
-        file: { "name" => "format-1" },
-        timestamp: "yesterday",
-      },
-      {
+  describe "#finders" do
+    it "returns matching finder data objects" do
+      expect(Dir).to receive(:glob).with("lib/documents/schemas/*.json").and_return(%w[
+        lib/documents/schemas/format-1.json
+        lib/documents/schemas/format-2.json
+      ])
+      expect(File).to receive(:read).with("lib/documents/schemas/format-2.json")
+        .and_return('{"name":"format-2"}')
+      expect(File).to receive(:mtime).with("lib/documents/schemas/format-2.json")
+        .and_return("today")
 
-        file: { "name" => "format-2" },
-        timestamp: "today",
-      },
-    ])
+      loader = FinderLoader.new
+      expect(loader.finders).to match_array([
+        {
+          file: { "name" => "format-1" },
+          timestamp: "yesterday",
+        },
+        {
+
+          file: { "name" => "format-2" },
+          timestamp: "today",
+        },
+      ])
+    end
   end
 
-  it "returns one matching finder data object" do
-    expect(File).to receive(:exist?).with("lib/documents/schemas/format-1.json")
-                      .and_return(true)
+  describe "#finder" do
+    it "returns one matching finder data object" do
+      expect(File).to receive(:exist?).with("lib/documents/schemas/format-1.json")
+                        .and_return(true)
 
-    loader = FinderLoader.new
-    expect(loader.finder("format-1")).to match_array([
-      {
-        file: { "name" => "format-1" },
-        timestamp: "yesterday",
-      },
-    ])
+      loader = FinderLoader.new
+      expect(loader.finder("format-1")).to match_array([
+        {
+          file: { "name" => "format-1" },
+          timestamp: "yesterday",
+        },
+      ])
+    end
   end
 end
