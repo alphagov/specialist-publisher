1f29d24b06b0b62ed1770abdcc62dacc3f19a991
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-10-16 15:56:04 +0100

    Mount publishing components component guide
    Add the publishing components component guide to routes and correctly configure the stylesheets with dartsass.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,2 @@
 //= link_directory ../javascripts .js
 //= link_tree ../builds
-//= link application.css
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require "healthcheck/s3"
 
 Rails.application.routes.draw do
+  mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
+
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response(
     GovukHealthcheck::SidekiqRedis,
