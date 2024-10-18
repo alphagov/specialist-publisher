e90faf59c30f630ccaf462ddf7e9527a7d669982
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-10-18 12:34:30 +0100

    Introduce form to edit finder metadata
    Add route and template for form to edit finder metadata. Importing any addition publishing component dependancies needed.

--- app/assets/javascripts/es6-components.js
+++ app/assets/javascripts/es6-components.js
@@ -1,4 +1,6 @@
 //= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/checkboxes
 //= require govuk_publishing_components/components/character-count
 //= require govuk_publishing_components/components/layout-header
+//= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/skip-link
