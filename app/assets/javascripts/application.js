d884d438f493d63f4c2ee091f0eaa27261379eef
Daniel Karaj <daniel.karaj@digital.cabinet-office.gov.uk>
2024-10-18 16:20:30 +0100

    Add the multiselect autocomplete
    Copy the files for the autocomplete from whitehall, add the dependency on alphagov/accessible-autocomplete-multiselect and add `node_modules` to the asset load path.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/copy-to-clipboard
+
+//= require components/autocomplete
