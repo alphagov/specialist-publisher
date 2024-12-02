c84f152832e6c90634f9f7acfe9de858917b66fe
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2024-12-02 12:15:30 +0000

    Render fields for email alert signup options
    Filtered email signups will not be handled via the Specialist Publisher admin form and therefore just submit a "CHANGE_REQUESTED" value for the email filter attribute.
    
    Developers will contact the department offline to request more information.

--- spec/components/email_alert_fieldset_component_spec.rb
+++ spec/components/email_alert_fieldset_component_spec.rb
@@ -15,11 +15,69 @@ RSpec.describe EmailAlertFieldsetComponent, type: :component do
     expect(page).to have_checked_field("email_alerts", with: "no")
   end
 
+  it "sets the value of the radio button to 'no' if the schema does not have a signup_content_id" do
+    schema = FinderSchema.new
+    render_inline(described_class.new(schema:))
+
+    expect(page).to have_checked_field("email_alerts", with: "no")
+  end
+
   it "sets the value of the radio button to 'all_content' if the schema has a signup content id" do
     schema = FinderSchema.new
     schema.signup_content_id = "123"
     render_inline(described_class.new(schema:))
 
     expect(page).to have_checked_field("email_alerts", with: "all_content")
+    expect(page).to have_field("signup_content_id", with: "123", type: "hidden")
+  end
+
+  it "sets the value of the radio button to 'all_content' and renders a hidden input with a generated content id if the schema is missing a signup content id" do
+    schema = FinderSchema.new
+    allow(SecureRandom).to receive(:uuid).and_return("new-id")
+    render_inline(described_class.new(schema:))
+
+    expect(page).to have_checked_field("email_alerts", with: "no")
+    expect(page).to have_field("signup_content_id", with: "new-id", type: "hidden")
+  end
+
+  it "renders the email subscription topic if the all_content value is checked" do
+    schema = FinderSchema.new
+    schema.signup_content_id = "123"
+    schema.subscription_list_title_prefix = "Finder email subscription"
+    render_inline(described_class.new(schema:))
+
+    expect(page).to have_text("Email subscription topic")
+    expect(page).to have_field("subscription_list_title_prefix", with: schema.subscription_list_title_prefix)
+  end
+
+  it "does not render the email subscription topic if the all_content value is checked but the current schema value is a hash" do
+    schema = FinderSchema.new
+    schema.signup_content_id = "123"
+    schema.subscription_list_title_prefix = {}
+    render_inline(described_class.new(schema:))
+
+    expect(page).not_to have_text("Email subscription topic")
+    expect(page).not_to have_field("subscription_list_title_prefix", with: schema.subscription_list_title_prefix)
+  end
+
+  it "sets the value of the radio button to 'external' if the schema has a signup link" do
+    schema = FinderSchema.new
+    schema.signup_link = "https://example.com"
+    render_inline(described_class.new(schema:))
+
+    expect(page).to have_checked_field("email_alerts", with: "external")
+    expect(page).to have_field("signup_link", with: schema.signup_link)
+  end
+
+  it "sets the value of the radio button to 'filtered_content' if the schema has an email filter attribute" do
+    schema = FinderSchema.new
+    schema.signup_content_id = "123"
+    schema.email_filter_by = "some_facet"
+    render_inline(described_class.new(schema:))
+
+    expect(page).to have_checked_field("email_alerts", with: "filtered_content")
+    expect(page).to have_text("Selected filter: Some facet")
+    expect(page).to have_field("signup_content_id", with: schema.signup_content_id, type: "hidden")
+    expect(page).to have_unchecked_field("email_filter_by", with: "CHANGE_REQUESTED")
   end
 end
