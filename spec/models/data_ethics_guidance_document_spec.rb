61d07fbc61dd7bf7148de37d3b4352dedd7dd42a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-12-04 17:22:30 +0000

    Set up files for new "Data Ethics Guidance Hub" Specialist Finder

---
+++ spec/models/data_ethics_guidance_document_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+require "models/valid_against_schema"
+
+RSpec.describe DataEthicsGuidanceDocument do
+  let(:payload) { FactoryBot.create(:data_ethics_guidance_document) }
+  include_examples "it saves payloads that are valid against the 'specialist_document' schema"
+  subject(:document) { described_class.from_publishing_api(payload) }
+
+  it "is not exportable" do
+    expect(described_class).not_to be_exportable
+  end
+
+  describe "validations" do
+    it "is valid from the payload" do
+      expect(document).to be_valid
+    end
+
+    it "is valid if all facets are missing" do
+      document.data_ethics_guidance_document_key_reference = nil
+      document.data_ethics_guidance_document_ethical_theme = nil
+      document.data_ethics_guidance_document_organisation = nil
+      document.data_ethics_guidance_document_project_phase = nil
+      document.data_ethics_guidance_document_technology_area = nil
+      expect(document).to be_valid
+    end
+  end
+end
