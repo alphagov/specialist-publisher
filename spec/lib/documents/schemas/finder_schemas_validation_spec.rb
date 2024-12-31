8ef8f49fdae71975b87be293fddf52504540a853
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-12-31 14:25:37 +0000

    All finders with hidden_indexable_content use the shared view
    See previous commits. Now that the hidden_indexable_content field
    has been added to the finder schemas themselves, there's nothing
    unqiue about the views for these finder publishing UIs, so they
    can start using the shared view.
    
    Note that hidden_indexable_content fields tend to have a lot of
    text input (see https://www.gov.uk/api/content/asylum-support-tribunal-decisions/aw-and-am-and-bg-v-secretary-of-state-home-office-as-slash-23-slash-11-slash-45804-and-as-slash-23-slash-11-slash-45824-and-as-slash-23-slash-11-slash-45796
    for an example). So I've swapped out the input type for text,
    from `input type="text"` to an adjustable `textarea`. This should
    have a negligible impact elsewhere - publishers can still just put
    a little bit of text in rather than a lot - but this also enables
    publishers to continue to use hidden_indexable_content without
    restriction.
    
    Moreover, these forms aren't yet on the Design System - so let's
    not worry too much about how they look. We can revisit this
    properly when we migrate to the new design system.

--- spec/lib/documents/schemas/finder_schemas_validation_spec.rb
+++ spec/lib/documents/schemas/finder_schemas_validation_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Sense-checks for every finder schema" do
       # properties in the finder schema and the properties in Publishing API
       # (i.e. `expect(properties_in_publishing_api).to eq(properties_in_finder_schema)`)
       # we can't do this yet, as some Finders define properties outside of the schema,
-      # e.g. 'hidden_indexable_content'.
+      # e.g. StatutoryInstrument's `withdrawn_date`.
       # So for now, we're just checking that every property defined in the finder
       # schema is actually used in Publishing API.
       expect(properties_in_finder_schema - properties_in_publishing_api).to be_empty, "In the '#{finder_schema['filter']['format']}' finder, the following redundant properties were found: #{(properties_in_finder_schema - properties_in_publishing_api).join(', ')}"
