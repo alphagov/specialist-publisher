0a1654bc946e0d8b6bd011f7beeffc26d1e15f2b
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-12-31 13:40:43 +0000

    Add special case for flood and coastal finder, and remove config
    The _only_ finder that doesn't seem to follow the
    `{finder_name}_metadata` pattern in Publishing API is the
    flood and coastal erosion risk finder. I've added a specific
    case to handle that, and a TODO for us to tidy it up later.
    
    There was one property defined on the flood and coastal finder,
    which is not referenced in Publishing API, so can be safely
    removed.

--- spec/lib/documents/schemas/finder_schemas_validation_spec.rb
+++ spec/lib/documents/schemas/finder_schemas_validation_spec.rb
@@ -3,25 +3,28 @@ require "spec_helper"
 RSpec.describe "Sense-checks for every finder schema" do
   Dir["lib/documents/schemas/*.json"].map { |file| JSON.parse(File.read(file)) }.each do |finder_schema|
     it "has each of its facets corresponding with a property in Publishing API" do
+      # TODO: append "_metadata" to the flood_and_coastal_... finder in Publishing API so that this workaround isn't required
+      definition_key = if finder_schema["filter"]["format"] == "flood_and_coastal_erosion_risk_management_research_report"
+                         "flood_and_coastal_erosion_risk_management_research_report"
+                       else
+                         "#{finder_schema['filter']['format']}_metadata"
+                       end
+
       schema = GovukSchemas::Schema.find(
         publisher_schema: "specialist_document",
-      )["definitions"]["#{finder_schema['filter']['format']}_metadata"]
+      )["definitions"][definition_key]
 
-      if !schema
-        puts "TODO: the '#{finder_schema['filter']['format']}' finder schema does not have a corresponding hash in the Publishing API. Skipping its validation..."
-      else
-        properties_in_publishing_api = (schema["properties"].keys - %w[bulk_published]).sort
-        properties_in_finder_schema = finder_schema["facets"].map { |facet| facet["key"] }.sort
+      properties_in_publishing_api = (schema["properties"].keys - %w[bulk_published]).sort
+      properties_in_finder_schema = finder_schema["facets"].map { |facet| facet["key"] }.sort
 
-        # TODO: whilst in an ideal world we'd have a direct one-to-one mapping between the
-        # properties in the finder schema and the properties in Publishing API
-        # (i.e. `expect(properties_in_publishing_api).to eq(properties_in_finder_schema)`)
-        # we can't do this yet, as some Finders define properties outside of the schema,
-        # e.g. 'hidden_indexable_content'.
-        # So for now, we're just checking that every property defined in the finder
-        # schema is actually used in Publishing API.
-        expect(properties_in_finder_schema - properties_in_publishing_api).to be_empty, "In the '#{finder_schema['filter']['format']}' finder, the following redundant properties were found: #{(properties_in_finder_schema - properties_in_publishing_api).join(', ')}"
-      end
+      # TODO: whilst in an ideal world we'd have a direct one-to-one mapping between the
+      # properties in the finder schema and the properties in Publishing API
+      # (i.e. `expect(properties_in_publishing_api).to eq(properties_in_finder_schema)`)
+      # we can't do this yet, as some Finders define properties outside of the schema,
+      # e.g. 'hidden_indexable_content'.
+      # So for now, we're just checking that every property defined in the finder
+      # schema is actually used in Publishing API.
+      expect(properties_in_finder_schema - properties_in_publishing_api).to be_empty, "In the '#{finder_schema['filter']['format']}' finder, the following redundant properties were found: #{(properties_in_finder_schema - properties_in_publishing_api).join(', ')}"
     end
   end
 end
