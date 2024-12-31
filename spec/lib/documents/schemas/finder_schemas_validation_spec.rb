42510c0a900ae2a82a4a2fba2d1ae0647c32def0
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2024-12-31 13:49:43 +0000

    Validate 'specialist_publisher_properties' are used correctly
    These tests eliminate the risk that a facet with "allowed_values"
    is missing a corresponding "specialist_publisher_properties"
    property (which is depended on by the shared view), and also
    eliminate the risk of said "specialist_publisher_properties"
    property having an invalid value.

---
+++ spec/lib/documents/schemas/finder_schemas_validation_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+RSpec.describe "Sense-checks for every finder schema" do
+  Dir["lib/documents/schemas/*.json"].map { |file| JSON.parse(File.read(file)) }.each do |finder_schema|
+    it "has 'specialist_publisher_properties' for every facet with 'allowed_values'" do
+      facets = finder_schema["facets"]
+
+      facets.each do |facet|
+        if facet["allowed_values"]
+          expect(facet["specialist_publisher_properties"]).to be_truthy, "In the '#{finder_schema['filter']['format']}' finder, facet '#{facet['key']}' is missing 'specialist_publisher_properties' despite having 'allowed_values'"
+        end
+      end
+    end
+
+    it "has 'specialist_publisher_properties' that are valid" do
+      finder_schema["facets"].each do |facet|
+        next unless facet["specialist_publisher_properties"]
+
+        valid_values = [
+          { "select" => "one" },
+          { "select" => "multiple" },
+        ]
+        expect(valid_values).to include(facet["specialist_publisher_properties"]), "In the '#{finder_schema['filter']['format']}' finder, facet '#{facet['key']}' has an invalid 'specialist_publisher_properties' value"
+      end
+    end
+  end
+end
