15c2dd1d3ac02cb4461d35664334a77bbf92a0f7
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-01-07 15:02:11 +0000

    Fix existing tests for editing finder metadata
    - For the "deleting all fields" scenario, we were repeatedly
      asserting that the page had an empty `dt` element. The intention
      was obviously to check that EVERY field had an empty `dt`
      element, but in this case, we were just checking that one
      existed, period (i.e. asserting the same thing several times).
      This has been swapped out for an explicit check against each
      field's `<dt>` (e.g. title, slug, and so on).
    - In the same vein, the assertions in the "changing all fields"
      test were a little ambiguous, just checking that the text
      appeared _at all_, rather than checking that the correct text
      appears in the correct row. This is now rectified.
    - This highlighted an issue whereby if changes to the 'Email Alerts'
      configuration do not actually come through to the metadata
      summary card at all, the test still passes despite our
      assertion that there was a `<dt>` containing the text "no"
      (after setting "Would you like to set up email alerts for the
      finder?" to "No"). The test was still passing because Capybara
      just checks that the text is present in a `<dt>`, not that the
      `<dt>`'s entire value is that text, so in this case it was
      passing because one of the other `<dt>` elements contained the
      words "Changed document noun". "no" is present in the word
      "noun", so the assertion passed.
    - The assertions in the "deleting all fields" tests therefore now
      use regexes to test blankness, as otherwise we may get false
      positives.
    - Finally, we weren't exercising the "Organisations the finder
      should be attached to" changes, so I've added that in. Note that
      the no-JS version of this form uses a `<select multiple="multiple">`,
      which is considerably easier to write a test for than the
      accessible-autocomplete-multiselect component that takes its
      place in the JavaScript version. If we change up this test to
      be a JS-driven one [like we've done for the "request edits to
      finder filters and options" form](https://github.com/alphagov/specialist-publisher/blob/910c706cd86e797b088d0a0f2e6ad25c6304b7aa/spec/features/editing_the_cma_case_filters_and_options_spec.rb#L14),
      we'll need to give that some further consideration.

--- spec/features/editing_the_cma_case_finder_spec.rb
+++ spec/features/editing_the_cma_case_finder_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Editing the CMA case finder", type: :feature do
   let(:organisations) do
     [
       { "content_id" => "957eb4ec-089b-4f71-ba2a-dc69ac8919ea", "title" => "Competition and Markets Authority" },
+      { "content_id" => "abc123", "title" => "Some other org" },
     ]
   end
 
@@ -28,6 +29,8 @@ RSpec.feature "Editing the CMA case finder", type: :feature do
 
     fill_in "name", with: "Changed title"
     fill_in "base_path", with: "Changed slug"
+    select = page.find("select#organisations")
+    select.select "Some other org"
     fill_in "description", with: "Changed description"
     fill_in "summary", with: "Changed summary"
     fill_in "Link 1", with: "Changed link 1"
@@ -38,15 +41,14 @@ RSpec.feature "Editing the CMA case finder", type: :feature do
 
     click_button "Submit changes"
 
-    expect(page).to have_selector("dt", text: "Changed title")
-    expect(page).to have_selector("dt", text: "Changed slug")
-    expect(page).to have_selector("dt", text: "Changed description")
-    expect(page).to have_selector("dt", text: "Changed summary")
-    expect(page).to have_selector("dt", text: "Changed link 1")
-    expect(page).to have_selector("dt", text: "Changed link 2")
-    expect(page).to have_selector("dt", text: "Changed link 3")
-    expect(page).to have_selector("dt", text: "Changed document noun")
-    expect(page).to have_selector("dt", text: "No")
+    expect(page.find(".govuk-summary-list__row", text: "Title of the finder")).to have_selector("dt", text: "Changed title")
+    expect(page.find(".govuk-summary-list__row", text: "Slug or URL")).to have_selector("dt", text: "Changed slug")
+    expect(page.find(".govuk-summary-list__row", text: "Organisations the finder should be attached to")).to have_selector("dt", text: "Competition and Markets Authority,Some other org")
+    expect(page.find(".govuk-summary-list__row", text: "Short description (For search engines)")).to have_selector("dt", text: "Changed description")
+    expect(page.find(".govuk-summary-list__row", text: "Summary of the finder (Longer description shown below title)")).to have_selector("dt", text: "Changed summary")
+    expect(page.find(".govuk-summary-list__row", text: "Any related links on GOV.UK?")).to have_selector("dt", text: "Yes\nLink 1: Changed link 1\nLink 2: Changed link 2\nLink 3: Changed link 3")
+    expect(page.find(".govuk-summary-list__row", text: "The document noun (How the documents on the finder are referred to)")).to have_selector("dt", text: "Changed document noun")
+    expect(page.find(".govuk-summary-list__row", text: "Would you like to set up email alerts for the finder?")).to have_selector("dt", text: "No")
 
     click_button "Submit changes"
 
@@ -61,6 +63,9 @@ RSpec.feature "Editing the CMA case finder", type: :feature do
 
     fill_in "name", with: ""
     fill_in "base_path", with: ""
+    select = page.find("select#organisations")
+    select.unselect "Competition and Markets Authority"
+    select.unselect "Some other org"
     fill_in "description", with: ""
     fill_in "summary", with: ""
     fill_in "Link 1", with: ""
@@ -70,14 +75,13 @@ RSpec.feature "Editing the CMA case finder", type: :feature do
 
     click_button "Submit changes"
 
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
-    expect(page).to have_selector("dt", text: "")
+    expect(page.find(".govuk-summary-list__row", text: "Title of the finder")).to have_selector("dt", text: /^$/)
+    expect(page.find(".govuk-summary-list__row", text: "Slug or URL")).to have_selector("dt", text: /^$/)
+    expect(page.find(".govuk-summary-list__row", text: "Organisations the finder should be attached to")).to have_selector("dt", text: /^$/)
+    expect(page.find(".govuk-summary-list__row", text: "Short description (For search engines)")).to have_selector("dt", text: /^$/)
+    expect(page.find(".govuk-summary-list__row", text: "Summary of the finder (Longer description shown below title)")).to have_selector("dt", text: /^$/)
+    expect(page.find(".govuk-summary-list__row", text: "Any related links on GOV.UK?")).to have_selector("dt", text: "Yes")
+    expect(page.find(".govuk-summary-list__row", text: "The document noun (How the documents on the finder are referred to)")).to have_selector("dt", text: /^$/)
 
     click_button "Submit changes"
 
