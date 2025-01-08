e6bfbd66be976aa39375ce3797f3f9b3c80f794a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-01-08 15:43:17 +0000

    Get content item before/after tests passing with new config
    As expected, most of the config could be generated / derived from
    elsewhere in the finder schema, with some overrides in rare cases.
    
    I have tweaked a couple of inconsistencies between the signup
    content item and the finder itself ("Country" vs "Countries", for
    example), but nothing that will change behaviour (topic names are
    unchanged).
    
    The biggest change is in the property tribunal decisions finder.
    It seems some additional categories were added in #2360, but were
    forgotten to be added to the corresponding email signup page.
    
    I've also added unit tests for all the new functionality, which
    required exposing the `email_filter_facets` method publicly and
    also making it a static method to avoid having to initialise the
    `FinderSignupContentItemPresenter` with a schema. I'm not entirely
    pleased with the resulting code, which could do with a refactor.
    
    Finally, note that I've removed the TODO comment around removing
    'email_filter_by' when it's removed from finder-frontend. The
    comment was added in December 2019 in be8462d0551b3997fdb1c8ad48cf9ee0cec9053a
    and the field hasn't been removed yet, nor is it on anyone's
    radar as far as I can tell, so leaving the comment would just be
    misleading the reader.

--- spec/presenters/finders/finder_signup_content_item_presenter_spec.rb
+++ spec/presenters/finders/finder_signup_content_item_presenter_spec.rb
@@ -17,6 +17,285 @@ RSpec.describe FinderSignupContentItemPresenter do
     end
   end
 
+  describe ".email_filter_facets" do
+    let(:facet_one) do
+      {
+        "key": "life_saving_maritime_appliance_service_station_regions",
+        "name": "Regions in the UK",
+        "short_name": "region",
+        "type": "text",
+        "preposition": "in region",
+        "display_as_result_metadata": true,
+        "filterable": true,
+        "allowed_values": [
+          {
+            "label": "Foo",
+            "value": "foo",
+          },
+          {
+            "label": "Bar",
+            "value": "bar",
+          },
+        ],
+      }
+    end
+    let(:facet_two) do
+      {
+        "key": "some_other_facet",
+        "name": "Some other facet",
+        "filterable": true,
+        "allowed_values": [
+          {
+            "label": "Baz",
+            "value": "baz-baz",
+          },
+          {
+            "label": "Something",
+            "value": "bla",
+          },
+        ],
+      }
+    end
+    let(:facet_with_no_allowed_values) do
+      {
+        "key": "some_date",
+        "name": "Some date",
+        "filterable": true,
+        "type": "date",
+      }
+    end
+    let(:facet_with_filterable_false) do
+      {
+        "key": "some_options",
+        "name": "Some options",
+        "filterable": false,
+        "allowed_values": [
+          {
+            "label": "some value",
+            "value": "val",
+          },
+        ],
+      }
+    end
+    let(:schema) do
+      {
+        "facets" => JSON.parse([facet_one, facet_two, facet_with_no_allowed_values, facet_with_filterable_false].to_json),
+      }
+    end
+
+    it "returns an empty array if no email_filter_by is provided" do
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq([])
+    end
+
+    it "returns all filterable facets with allowed_values, if 'all_selected_facets' is passed" do
+      schema.merge!({
+        "email_filter_options" => {
+          "email_filter_by" => "all_selected_facets",
+        },
+      })
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq(
+        [
+          {
+            facet_id: "life_saving_maritime_appliance_service_station_regions",
+            facet_name: "Regions in the UK",
+            facet_choices: [
+              {
+                key: "foo",
+                prechecked: false,
+                radio_button_name: "Foo",
+                topic_name: "Foo",
+              },
+              {
+                key: "bar",
+                prechecked: false,
+                radio_button_name: "Bar",
+                topic_name: "Bar",
+              },
+            ],
+          },
+          {
+            facet_id: "some_other_facet",
+            facet_name: "Some other facet",
+            facet_choices: [
+              {
+                key: "baz-baz",
+                prechecked: false,
+                radio_button_name: "Baz",
+                topic_name: "Baz",
+              },
+              {
+                key: "bla",
+                prechecked: false,
+                radio_button_name: "Something",
+                topic_name: "Something",
+              },
+            ],
+          },
+        ],
+      )
+    end
+
+    it "returns all filterable facets with allowed_values (if 'all_selected_facets' is passed), minus any specified in 'all_selected_facets_except_for'" do
+      schema.merge!({
+        "email_filter_options" => {
+          "email_filter_by" => "all_selected_facets",
+          "all_selected_facets_except_for" => %w[life_saving_maritime_appliance_service_station_regions],
+        },
+      })
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq(
+        [
+          {
+            facet_id: "some_other_facet",
+            facet_name: "Some other facet",
+            facet_choices: [
+              {
+                key: "baz-baz",
+                prechecked: false,
+                radio_button_name: "Baz",
+                topic_name: "Baz",
+              },
+              {
+                key: "bla",
+                prechecked: false,
+                radio_button_name: "Something",
+                topic_name: "Something",
+              },
+            ],
+          },
+        ],
+      )
+    end
+
+    it "returns the filterable facet corresponding to 'email_filter_by'" do
+      schema.merge!({
+        "email_filter_options" => {
+          "email_filter_by" => "some_other_facet",
+        },
+      })
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq(
+        [
+          {
+            facet_id: "some_other_facet",
+            facet_name: "Some other facet",
+            required: true,
+            facet_choices: [
+              {
+                key: "baz-baz",
+                prechecked: false,
+                radio_button_name: "Baz",
+                topic_name: "Baz",
+              },
+              {
+                key: "bla",
+                prechecked: false,
+                radio_button_name: "Something",
+                topic_name: "Something",
+              },
+            ],
+          },
+        ],
+      )
+    end
+
+    it "downcases the generated topic name if downcase_email_alert_topic_names passed" do
+      schema.merge!({
+        "email_filter_options" => {
+          "email_filter_by" => "some_other_facet",
+          "downcase_email_alert_topic_names" => true,
+        },
+      })
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq(
+        [
+          {
+            facet_id: "some_other_facet",
+            facet_name: "Some other facet",
+            required: true,
+            facet_choices: [
+              {
+                key: "baz-baz",
+                prechecked: false,
+                radio_button_name: "Baz",
+                topic_name: "baz",
+              },
+              {
+                key: "bla",
+                prechecked: false,
+                radio_button_name: "Something",
+                topic_name: "something",
+              },
+            ],
+          },
+        ],
+      )
+    end
+
+    it "overrides the generated topic name if email_alert_topic_name_overrides passed" do
+      schema.merge!({
+        "email_filter_options" => {
+          "email_filter_by" => "some_other_facet",
+          "email_alert_topic_name_overrides" => {
+            "baz-baz" => "something ENTIRELY different",
+          },
+        },
+      })
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq(
+        [
+          {
+            facet_id: "some_other_facet",
+            facet_name: "Some other facet",
+            required: true,
+            facet_choices: [
+              {
+                key: "baz-baz",
+                prechecked: false,
+                radio_button_name: "Baz",
+                topic_name: "something ENTIRELY different",
+              },
+              {
+                key: "bla",
+                prechecked: false,
+                radio_button_name: "Something",
+                topic_name: "Something",
+              },
+            ],
+          },
+        ],
+      )
+    end
+
+    it "pre-checks any items specified in the pre_checked_email_alert_checkboxes array" do
+      schema.merge!({
+        "email_filter_options" => {
+          "email_filter_by" => "some_other_facet",
+          "pre_checked_email_alert_checkboxes" => %w[baz-baz],
+        },
+      })
+      expect(FinderSignupContentItemPresenter.new(schema).email_filter_facets).to eq(
+        [
+          {
+            facet_id: "some_other_facet",
+            facet_name: "Some other facet",
+            required: true,
+            facet_choices: [
+              {
+                key: "baz-baz",
+                prechecked: true,
+                radio_button_name: "Baz",
+                topic_name: "Baz",
+              },
+              {
+                key: "bla",
+                prechecked: false,
+                radio_button_name: "Something",
+                topic_name: "Something",
+              },
+            ],
+          },
+        ],
+      )
+    end
+  end
+
   describe "temporary tests to aid in refactoring schema" do
     Dir["lib/documents/schemas/*.json"].each do |file|
       it "generates a content item that is identical to the temporary output file generated earlier" do
