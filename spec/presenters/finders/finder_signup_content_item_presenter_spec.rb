71deaf3549772c6cc469989effecd2269195cffa
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-01-08 13:35:09 +0000

    Add temporary snapshots of current email signup content item hashes
    We'll use these as a base to compare against as we refactor the
    presenter and the underlying JSON. The output should be near
    identical by the time we've completed the refactor. When the refactor
    is complete, we'll delete these snapshots.

--- spec/presenters/finders/finder_signup_content_item_presenter_spec.rb
+++ spec/presenters/finders/finder_signup_content_item_presenter_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe FinderSignupContentItemPresenter do
       end
     end
   end
+
+  describe "temporary tests to aid in refactoring schema" do
+    Dir["lib/documents/schemas/*.json"].each do |file|
+      it "generates temporary output files we can compare against" do
+        read_file = File.read(file)
+        payload = JSON.parse(read_file)
+        if payload.key?("signup_content_id")
+          finder_signup_content_presenter = FinderSignupContentItemPresenter.new(payload, File.mtime(file))
+          presented_data = finder_signup_content_presenter.to_json
+
+          File.write("spec/presenters/finders/signup_content_items/#{file.split('/').last}", JSON.pretty_generate(presented_data))
+        end
+      end
+    end
+  end
 end
