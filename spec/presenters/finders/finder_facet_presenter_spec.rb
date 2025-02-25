41c531acae16dda078f4426aa4acda5e5aa0873c
Minno Dang <mndang@thoughtworks.com>
2025-02-25 17:51:20 +0000

    Map single nested facet from schema to two individual facets for publishing-api
    In order to minimise the customisation in Finder Frontend, we will send the data in the shape it is typically expected by Finder Frontend, i.e. the facets are stand-alone. We still need some way of referencing the parent facet value in the children, which we're doing by setting `main_facet_value`, alongside every subfacet label-value pair in the subfacet. We also send the `main_facet_label` so we can recompose the subfacet label in the format `Main facet label - Sub facet label` when JS is disabled and all the facet options show in the select; since we have duplicates such as "Not applicable" it would be unclear which main facet option the subfacet option refers to.
    
    Other required keys are:
    - the `nested_facet` (T/F) flag which indicates to the Finder Frontend rendering app that the facet should be rendered as a NestedFacet (new Facet type added by us).
    - `sub_facet_key` and `sub_facet_name` which are only set on the main facet, used for loading the js module.
    - the `key` and `name` of the subfacet come from the data stored under the main facet, in the schema.
    - all the other key values are shared between the main and sub facets, though a case could be made in the future for also allowing the customization of the subfacet's `short_name`, `preposition`, `display_as_result_metadata` or `show_option_select_filter` values.
    
    Co-authored-by: Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>

--- spec/presenters/finders/finder_facet_presenter_spec.rb
+++ spec/presenters/finders/finder_facet_presenter_spec.rb
@@ -34,5 +34,109 @@ RSpec.describe FinderFacetPresenter do
       presented_facets = FinderFacetPresenter.new(facets).to_json
       expect(presented_facets).to eq(facets_without_specialist_publisher_properties)
     end
+
+    it "returns two distinct main and sub facets for a finder with one nested facet defined in the schema" do
+      facets_from_schema = [{
+        "allowed_values" => [
+          {
+            "label" => "Allowed value 1",
+            "value" => "allowed-value-1",
+            "sub_facets" => [
+              {
+                "label" => "Allowed value 1 Sub facet Value 1",
+                "value" => "allowed-value-1-sub-facet-value-1",
+              },
+              {
+                "label" => "Allowed value 1 Sub facet Value 2",
+                "value" => "allowed-value-1-sub-facet-value-2",
+              },
+            ],
+          },
+          {
+            "label" => "Allowed value 2",
+            "value" => "allowed-value-2",
+            "sub_facets" => [
+              {
+                "label" => "Allowed value 2 Sub facet Value 1",
+                "value" => "allowed-value-2-sub-facet-value-1",
+              },
+            ],
+          },
+          {
+            "label" => "Allowed value 3",
+            "value" => "allowed-value-3",
+          },
+        ],
+        "display_as_result_metadata" => true,
+        "filterable" => true,
+        "key" => "facet_key",
+        "name" => "Facet Name",
+        "nested_facet" => true,
+        "preposition" => "Facet Name",
+        "short_name" => "Short Name",
+        "sub_facet_key" => "sub_facet_key",
+        "sub_facet_name" => "Sub Facet Name",
+        "type" => "text",
+      }]
+      main_facet_hash = {
+        "allowed_values" => [
+          {
+            "label" => "Allowed value 1",
+            "value" => "allowed-value-1",
+          },
+          {
+            "label" => "Allowed value 2",
+            "value" => "allowed-value-2",
+          },
+          {
+            "label" => "Allowed value 3",
+            "value" => "allowed-value-3",
+          },
+        ],
+        "display_as_result_metadata" => true,
+        "filterable" => true,
+        "key" => "facet_key",
+        "name" => "Facet Name",
+        "nested_facet" => true,
+        "preposition" => "Facet Name",
+        "short_name" => "Short Name",
+        "sub_facet_name" => "Sub Facet Name",
+        "sub_facet_key" => "sub_facet_key",
+        "type" => "text",
+      }
+      sub_facet_hash = {
+        "allowed_values" => [
+          {
+            "label" => "Allowed value 1 Sub facet Value 1",
+            "value" => "allowed-value-1-sub-facet-value-1",
+            "main_facet_label" => "Allowed value 1",
+            "main_facet_value" => "allowed-value-1",
+          },
+          {
+            "label" => "Allowed value 1 Sub facet Value 2",
+            "value" => "allowed-value-1-sub-facet-value-2",
+            "main_facet_label" => "Allowed value 1",
+            "main_facet_value" => "allowed-value-1",
+          },
+          {
+            "label" => "Allowed value 2 Sub facet Value 1",
+            "value" => "allowed-value-2-sub-facet-value-1",
+            "main_facet_label" => "Allowed value 2",
+            "main_facet_value" => "allowed-value-2",
+          },
+        ],
+        "display_as_result_metadata" => true,
+        "filterable" => true,
+        "key" => "sub_facet_key",
+        "name" => "Sub Facet Name",
+        "nested_facet" => true,
+        "preposition" => "Sub Facet Name",
+        "short_name" => "Sub Facet Name",
+        "type" => "text",
+      }
+
+      presented_data = FinderFacetPresenter.new(facets_from_schema).to_json
+      expect(presented_data).to eq([main_facet_hash, sub_facet_hash])
+    end
   end
 end
