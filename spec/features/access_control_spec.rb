8a4816170137dc71d50f839075205f0e931d79ee
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2025-03-03 14:57:13 +0000

    Audit access_control_spec.rb test file
    Simplifies tests by:
    
    - Removing StatutoryInstrument access control tests, and AAIB
      access control tests. These tests didn't seem to be testing any
      behaviour different to the existing CMA access tests.
    - I understand the logic behind testing the 'inverse' of the CMA
      tests (i.e. that an AAIB editor can access AAIB and not CMA, to
      verify that the CMA cases aren't "failing open") but given we
      don't test access controls of ANY of the other document types,
      it doesn't make a lot of sense to retain anything other than
      the simple set of CMA access tests.
    - Moves the general permissions error to the top of the file. It
      is the simplest case, so should really come first.
    - Also adds a proper explanation of what's being tested.
    - This test should prove sufficient coverage for the editor
      permissions, this means we can remove the editor access test in
      'creating_an_employment_appeal_tribunal_decisions_spec.rb'.
      The writer permission is sufficiently tested in the
      action_presenter_spec.

--- spec/features/access_control_spec.rb
+++ spec/features/access_control_spec.rb
@@ -1,126 +1,60 @@
 require "spec_helper"
 
 RSpec.feature "Access control", type: :feature do
-  before do
-    stub_publishing_api_has_content([], hash_including(document_type: CmaCase.document_type))
-    stub_publishing_api_has_content([], hash_including(document_type: AaibReport.document_type))
-    stub_publishing_api_has_content([], hash_including(document_type: StatutoryInstrument.document_type))
-    stub_publishing_api_has_content([], hash_including(document_type: Organisation.document_type))
+  context "as an editor of an organisation that doesn't have access to the application" do
+    before do
+      log_in_as_editor(:incorrect_id_editor)
+    end
+
+    scenario "visiting homepage shows a permissions error" do
+      visit "/"
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content("Sorry, you don't have permission to access this application")
+      expect(page).to have_content("Please contact your main GDS contact if you need access.")
+    end
   end
 
   context "as a CMA Editor" do
     before do
+      stub_publishing_api_has_content([], hash_including(document_type: CmaCase.document_type))
+      stub_publishing_api_has_content([], hash_including(document_type: Organisation.document_type))
       log_in_as_editor(:cma_editor)
     end
 
-    scenario "visiting /cma-cases" do
+    scenario "visiting /cma-cases is allowed" do
       visit "/cma-cases"
 
       expect(page.status_code).to eq(200)
       expect(page).to have_content("CMA Cases")
     end
 
-    scenario "visiting admin/cma-cases" do
+    scenario "visiting admin/cma-cases is allowed" do
       visit "admin/cma-cases"
 
       expect(page.status_code).to eq(200)
       expect(page).to have_content("CMA Case finder")
     end
 
-    scenario "visiting /aaib-reports" do
+    scenario "visiting /aaib-reports is rejected" do
       visit "/aaib-reports"
 
       expect(page.current_path).to eq("/cma-cases")
       expect(page).to have_content("You aren't permitted to access AAIB Reports")
     end
 
-    scenario "visiting admin/aaib-reports" do
+    scenario "visiting admin/aaib-reports is rejected" do
       visit "admin/aaib-reports"
 
       expect(page.current_path).to eq("/cma-cases")
       expect(page).to have_content("You aren't permitted to access AAIB Reports")
     end
 
-    scenario "visiting a format which doesn't exist" do
+    scenario "visiting a format which doesn't exist gives an error message" do
       visit "/a-format-which-doesnt-exist"
 
       expect(page.current_path).to eq("/cma-cases")
       expect(page).to have_content("That format doesn't exist.")
-    end
-  end
-
-  context "as an AAIB Editor" do
-    before do
-      log_in_as_editor(:aaib_editor)
-    end
-
-    scenario "visiting /aaib-reports" do
-      visit "/aaib-reports"
-
-      expect(page.status_code).to eq(200)
-      expect(page).to have_content("AAIB Reports")
-    end
-
-    scenario "visiting admin/aaib-reports" do
-      visit "admin/aaib-reports"
-
-      expect(page.status_code).to eq(200)
-      expect(page).to have_content("AAIB Report finder")
-    end
-
-    scenario "visiting /cma-cases" do
-      visit "/cma-cases"
-
-      expect(page.current_path).to eq("/aaib-reports")
-      expect(page).to have_content("You aren't permitted to access CMA Cases")
-    end
-
-    scenario "visiting admin/cma-cases" do
-      visit "admin/cma-cases"
-
-      expect(page.current_path).to eq("/aaib-reports")
-      expect(page).to have_content("You aren't permitted to access CMA Cases")
-    end
-  end
-
-  context "as a statutory instrument editor" do
-    before do
-      stub_publishing_api_has_content([], hash_including(document_type: Organisation.document_type))
-      log_in_as_editor(:statutory_instrument_editor)
-    end
-
-    scenario "visiting the statutory instruments format" do
-      visit "/eu-withdrawal-act-2018-statutory-instruments"
-
-      expect(page.status_code).to eq(200)
-      expect(page).to have_content("EU Withdrawal Act 2018 statutory instruments")
-    end
-
-    scenario "visiting another format" do
-      visit "/cma-cases"
-
-      expect(page.current_path).to eq("/eu-withdrawal-act-2018-statutory-instruments")
-      expect(page).to have_content("You aren't permitted to access CMA Cases")
-    end
-
-    scenario "visiting the home page" do
-      visit "/"
-
-      expect(page.current_path).to eq("/eu-withdrawal-act-2018-statutory-instruments")
-    end
-  end
-
-  context "as an editor with incorrect organisation_content_id" do
-    before do
-      log_in_as_editor(:incorrect_id_editor)
-    end
-
-    scenario "visiting /" do
-      visit "/"
-
-      expect(page.status_code).to eq(200)
-      expect(page).to have_content("Sorry, you don't have permission to access this application")
-      expect(page).to have_content("Please contact your main GDS contact if you need access.")
     end
   end
 end
