937acfec28a07042bbdbd9f98ade91ae1ea489be
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-03-12 12:03:00 +0000

    Revert presenter changes for  nested facet
    We're changing implementation to have a single facet in Finder Frontend, rather than two.
    
    We are nonetheless keeping the `main_facet_label` and `main_facet_value` references in the nested structure.

--- spec/presenters/finders/finder_facet_presenter_spec.rb
+++ spec/presenters/finders/finder_facet_presenter_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe FinderFacetPresenter do
       expect(presented_facets).to eq(facets_without_specialist_publisher_properties)
     end
 
-    it "returns two distinct main and sub facets for a finder with one nested facet defined in the schema" do
+    it "returns main facet label and value references in the sub-facet structure" do
       facets_from_schema = [{
         "allowed_values" => [
           {
@@ -43,11 +43,11 @@ RSpec.describe FinderFacetPresenter do
             "value" => "allowed-value-1",
             "sub_facets" => [
               {
-                "label" => "Allowed value 1 Sub facet Value 1",
+                "label" => "Sub facet Value 1",
                 "value" => "allowed-value-1-sub-facet-value-1",
               },
               {
-                "label" => "Allowed value 1 Sub facet Value 2",
+                "label" => "Sub facet Value 2",
                 "value" => "allowed-value-1-sub-facet-value-2",
               },
             ],
@@ -57,7 +57,7 @@ RSpec.describe FinderFacetPresenter do
             "value" => "allowed-value-2",
             "sub_facets" => [
               {
-                "label" => "Allowed value 2 Sub facet Value 1",
+                "label" => "Sub facet Value 1",
                 "value" => "allowed-value-2-sub-facet-value-1",
               },
             ],
@@ -77,15 +77,37 @@ RSpec.describe FinderFacetPresenter do
         "sub_facet_name" => "Sub Facet Name",
         "type" => "nested",
       }]
-      main_facet_hash = {
+      main_facet_hash = [{
         "allowed_values" => [
           {
             "label" => "Allowed value 1",
             "value" => "allowed-value-1",
+            "sub_facets" => [
+              {
+                "label" => "Sub facet Value 1",
+                "value" => "allowed-value-1-sub-facet-value-1",
+                "main_facet_label" => "Allowed value 1",
+                "main_facet_value" => "allowed-value-1",
+              },
+              {
+                "label" => "Sub facet Value 2",
+                "value" => "allowed-value-1-sub-facet-value-2",
+                "main_facet_label" => "Allowed value 1",
+                "main_facet_value" => "allowed-value-1",
+              },
+            ],
           },
           {
             "label" => "Allowed value 2",
             "value" => "allowed-value-2",
+            "sub_facets" => [
+              {
+                "label" => "Sub facet Value 1",
+                "value" => "allowed-value-2-sub-facet-value-1",
+                "main_facet_label" => "Allowed value 2",
+                "main_facet_value" => "allowed-value-2",
+              },
+            ],
           },
           {
             "label" => "Allowed value 3",
@@ -98,43 +120,13 @@ RSpec.describe FinderFacetPresenter do
         "name" => "Facet Name",
         "preposition" => "Facet Name",
         "short_name" => "Short Name",
-        "sub_facet_name" => "Sub Facet Name",
         "sub_facet_key" => "sub_facet_key",
+        "sub_facet_name" => "Sub Facet Name",
         "type" => "nested",
-      }
-      sub_facet_hash = {
-        "allowed_values" => [
-          {
-            "label" => "Allowed value 1 Sub facet Value 1",
-            "value" => "allowed-value-1-sub-facet-value-1",
-            "main_facet_label" => "Allowed value 1",
-            "main_facet_value" => "allowed-value-1",
-          },
-          {
-            "label" => "Allowed value 1 Sub facet Value 2",
-            "value" => "allowed-value-1-sub-facet-value-2",
-            "main_facet_label" => "Allowed value 1",
-            "main_facet_value" => "allowed-value-1",
-          },
-          {
-            "label" => "Allowed value 2 Sub facet Value 1",
-            "value" => "allowed-value-2-sub-facet-value-1",
-            "main_facet_label" => "Allowed value 2",
-            "main_facet_value" => "allowed-value-2",
-          },
-        ],
-        "display_as_result_metadata" => true,
-        "filterable" => true,
-        "key" => "sub_facet_key",
-        "name" => "Sub Facet Name",
-        "main_facet_key" => "facet_key",
-        "preposition" => "Sub Facet Name",
-        "short_name" => "Sub Facet Name",
-        "type" => "nested",
-      }
+      }]
 
       presented_data = FinderFacetPresenter.new(facets_from_schema).to_json
-      expect(presented_data).to eq([main_facet_hash, sub_facet_hash])
+      expect(presented_data).to eq(main_facet_hash)
     end
   end
 end
