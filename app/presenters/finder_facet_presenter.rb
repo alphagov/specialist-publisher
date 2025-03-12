1ef3bfeae577c1a29eb581e4e8c8cbfc7ed154e0
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-03-12 12:03:00 +0000

    Revert presenter changes for  nested facet
    We're changing implementation to have a single facet in Finder Frontend, rather than two.
    
    We are nonetheless keeping the `main_facet_label` and `main_facet_value` references in the nested structure.

--- app/presenters/finder_facet_presenter.rb
+++ app/presenters/finder_facet_presenter.rb
@@ -4,9 +4,9 @@ class FinderFacetPresenter
   end
 
   def to_json(*_args)
-    facets_without_specialist_publisher_properties(@facets).map { |facet|
-      facet["type"] == "nested" ? [main_facet_hash_without_sub_facets(facet), sub_facet_hash(facet)] : facet
-    }.flatten
+    facets_without_specialist_publisher_properties(@facets).map do |facet|
+      facet["type"] == "nested" ? hash_with_parent_references(facet) : facet
+    end
   end
 
 private
@@ -19,34 +19,13 @@ private
     end
   end
 
-  def main_facet_hash_without_sub_facets(facet_hash)
-    hash_dup = facet_hash.dup
-    hash_dup["allowed_values"] = hash_dup["allowed_values"].map { |v| v.except("sub_facets") }
-
-    hash_dup
-  end
-
-  def sub_facet_hash(facet_hash)
-    facet_hash.merge(
-      "allowed_values" => sub_facet_allowed_values(facet_hash),
-      "key" => facet_hash["sub_facet_key"],
-      "name" => facet_hash["sub_facet_name"],
-      "sub_facet_key" => nil,
-      "sub_facet_name" => nil,
-      "main_facet_key" => facet_hash["key"],
-      "short_name" => facet_hash["sub_facet_name"],
-      "preposition" => facet_hash["sub_facet_name"],
-    ).compact
-  end
-
-  def sub_facet_allowed_values(facet_hash)
-    facet_hash["allowed_values"].each_with_object([]) do |allowed_value, sub_facets|
-      main_label = allowed_value["label"]
-      main_value = allowed_value["value"]
-
+  def hash_with_parent_references(facet_hash)
+    facet_hash["allowed_values"].each do |allowed_value|
       allowed_value["sub_facets"]&.each do |sub_facet|
-        sub_facets << sub_facet.merge("main_facet_label" => main_label, "main_facet_value" => main_value)
+        sub_facet["main_facet_label"] = allowed_value["label"]
+        sub_facet["main_facet_value"] = allowed_value["value"]
       end
     end
+    facet_hash
   end
 end
