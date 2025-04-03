912e55005900966008196363fb18cd52a1484268
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-04-03 16:13:12 +0100

    Add UKHSA Finder

--- spec/fixtures/factories.rb
+++ spec/fixtures/factories.rb
@@ -892,4 +892,22 @@ FactoryBot.define do
     base_path { "/portfolio-of-assurance-techniques/example-document" }
     document_type { "ai_assurance_portfolio_technique" }
   end
+
+  factory :ukhsa_data_access_approval, parent: :document do
+    base_path { "/data-access-approvals-register/example-document" }
+    document_type { "ukhsa_data_access_approval" }
+    transient do
+      default_metadata do
+        {
+          "ukhsa_approval_status" => "active",
+          "ukhsa_access_type" => "file-transfer-to-organisation",
+          "ukhsa_applicant_organisation_name" => "Some organisation",
+          "ukhsa_applicant_organisation_type" => %w[public-health-agency],
+          "ukhsa_classification_identification_risk" => "personally-identifiable",
+          "ukhsa_dataset" => %w[childrens-hiv-and-aids-reporting-system-chars covid19-hospitalisation-in-england-surveillance-system-chess],
+          "ukhsa_approval_date" => "2023-01-01",
+        }
+      end
+    end
+  end
 end
