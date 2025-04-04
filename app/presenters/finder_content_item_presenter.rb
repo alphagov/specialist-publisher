82d5a0cd1d8caf7c3a08223c241f30be19a63965
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2025-04-04 14:50:07 +0100

    Surface show_table_of_contents property

--- app/presenters/finder_content_item_presenter.rb
+++ app/presenters/finder_content_item_presenter.rb
@@ -50,6 +50,7 @@ private
       logo_path: file.fetch("logo_path", nil),
       show_summaries: file.fetch("show_summaries", false),
       show_metadata_block: file.fetch("show_metadata_block", false),
+      show_table_of_contents: file.fetch("show_table_of_contents", false),
       signup_link: file.fetch("signup_link", nil),
       summary:,
       label_text: file.fetch("label_text", nil),
