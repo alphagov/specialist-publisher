bd8309c3d26b32ff727fe4d451d42aa387f339d6
Minno Dang <mndang@thoughtworks.com>
2025-05-06 16:26:06 +0100

    Make organisations helper methods for select dropdown legacy
    The old layout select fields expects a double array with two items to represent label and value. The new design system expects an array of hash values with label and value keys. To make way for the new design system, we need to make these old methods legacy.

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -1,21 +1,15 @@
 module OrganisationsHelper
-  def all_organisations
-    @all_organisations ||= Organisation.all
-  end
-
-  def organisations_options
-    all_organisations.map { |o| [o.title, o.content_id] }.sort_by { |title, _id| title.downcase.strip }
-  end
-
-  def organisations_options_with_all
-    [["All organisations", "all"]].concat(organisations_options)
+  def organisation_name(content_id)
+    all_organisations.select { |o| o.content_id == content_id }.first&.title
   end
 
   def selected_organisation_or_current(organisation)
     organisation.presence || current_user.organisation_content_id
   end
 
-  def organisation_name(content_id)
-    all_organisations.select { |o| o.content_id == content_id }.first&.title
+private
+
+  def all_organisations
+    @all_organisations ||= Organisation.all
   end
 end
