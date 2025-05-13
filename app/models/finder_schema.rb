30465b40939f38c56451767887844936e63c0a09
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-05-13 12:55:58 +0100

    Move `show_table_of_contents` to be in alphabetical order
    The generated schema follows the order of the fields in the schema.

--- app/models/finder_schema.rb
+++ app/models/finder_schema.rb
@@ -38,7 +38,6 @@ class FinderSchema
   attribute :email_filter_options
   attribute :facets, default: []
   attribute :filter
-  attribute :show_table_of_contents, :boolean, default: false
   attribute :label_text
   attribute :name
   attribute :organisations, default: []
@@ -48,6 +47,7 @@ class FinderSchema
   attribute :related
   attribute :show_summaries, :boolean, default: false
   attribute :show_metadata_block, :boolean, default: false
+  attribute :show_table_of_contents, :boolean, default: false
   attribute :signup_content_id
   attribute :signup_link
   attribute :subscription_list_title_prefix
