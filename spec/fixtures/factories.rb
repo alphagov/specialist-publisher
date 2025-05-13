7cce1c70b28571f25cb5cef77d2c7161f89a1b64
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-05-13 18:37:57 +0100

    Add ukhsa_request_type facet to UKHSA finder

--- spec/fixtures/factories.rb
+++ spec/fixtures/factories.rb
@@ -917,6 +917,7 @@ FactoryBot.define do
         {
           "ukhsa_approval_status" => "active",
           "ukhsa_access_type" => "file-transfer-to-organisation",
+          "ukhsa_request_type" => "surveillance-and-public-health-action",
           "ukhsa_organisation_name" => "placeholder-organisation",
           "ukhsa_applicant_organisation_type" => %w[public-health-agency],
           "ukhsa_classification_identification_risk" => "personally-identifiable",
