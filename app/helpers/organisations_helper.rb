6e3e1cd68dfc34c87bd73ded5c71f8c35a29add3
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-05-22 14:25:17 +0100

    Mark organisations select usages as legacy
    - rename existing methods to have _legacy suffix (`organisations_options`, `organisations_options_with_all`) and move them to a legacy helper
    - keep existing design system usages in the non-legacy file
    - and keep other common methods in the design system helper file (as it's already included)

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -1,16 +1,4 @@
 module OrganisationsHelper
-  def all_organisations
-    @all_organisations ||= Organisation.all
-  end
-
-  def organisations_options
-    all_organisations.map { |o| [o.title, o.content_id] }.sort_by { |title, _id| title.downcase.strip }
-  end
-
-  def organisations_options_with_all
-    [["All organisations", "all"]].concat(organisations_options)
-  end
-
   def organisation_options_for_design_system(selected_organisation_content_id)
     [
       {
@@ -33,5 +21,11 @@ module OrganisationsHelper
 
   def organisation_name(content_id)
     all_organisations.select { |o| o.content_id == content_id }.first&.title
+  end
+
+private
+
+  def all_organisations
+    @all_organisations ||= Organisation.all
   end
 end
