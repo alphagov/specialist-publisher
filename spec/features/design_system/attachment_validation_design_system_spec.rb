74e29874a488c9ff64bc6a30ee3b7578a0264082
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-07-07 17:49:03 +0100

    Move remaining tests to the design system
    - previous tests marked as legacy
    - no change required other than `log_in_as_design_system_editor` instead of `log_in_as_editor`

---
+++ spec/features/design_system/attachment_validation_design_system_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+RSpec.feature "Validating inline attachments", type: :feature do
+  before do
+    log_in_as_design_system_editor(:cma_editor)
+  end
+
+  scenario "creating a document that references attachments that don't exist" do
+    visit "/cma-cases/new"
+    fill_in "Body", with: "[InlineAttachment:missing.pdf]"
+    click_button "Save"
+
+    expect(page).to have_content("There is a problem")
+    expect(page).to have_content(
+      "Body contains an attachment that can't be found: 'missing.pdf'",
+    )
+  end
+
+  scenario "escaping inline attachments so that they are html safe" do
+    visit "/cma-cases/new"
+    fill_in "Body", with: "[InlineAttachment:<not>safe.pdf]"
+    click_button "Save"
+
+    expect(page).to have_content("There is a problem")
+
+    expect(page).to have_content(
+      "Body contains an attachment that can't be found: '<not>safe.pdf'",
+    )
+    expect(page).not_to have_content(
+      "Body contains an attachment that can't be found: '&lt;not&gt;safe.pdf'",
+    )
+  end
+end
