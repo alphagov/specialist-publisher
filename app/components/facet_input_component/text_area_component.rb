c5d0d4c033265585c90c76fe8d02b97a5ad8fca7
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-07-10 09:42:36 +0100

    Add hint for authors field
    We're using a rather custom way of implementing this textarea, so guidance is required.

--- app/components/facet_input_component/text_area_component.rb
+++ app/components/facet_input_component/text_area_component.rb
@@ -1,11 +1,12 @@
 class FacetInputComponent::TextAreaComponent < ViewComponent::Base
   include ErrorsHelper
 
-  def initialize(document, document_type, facet_key, facet_name)
+  def initialize(document, document_type, facet_key, facet_name, hint: nil)
     @document = document
     @document_type = document_type
     @facet_key = facet_key
     @facet_name = facet_name
     @error_items = errors_for(document.errors, facet_key)
+    @hint = hint
   end
 end
