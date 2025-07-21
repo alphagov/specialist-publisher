fdd3ccb379dede7d87663ab011953dbae65975cb
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-07-21 19:34:36 +0100

    Remove unused copy-to-clipboard import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/add-another
-//= require govuk_publishing_components/components/copy-to-clipboard
 //= require govuk_publishing_components/components/select-with-search
 
 //= require components/govspeak-editor
