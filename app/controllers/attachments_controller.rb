71e8d7cc1aea0e7d21e5465ee12cd037e865bdb3
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-07-21 17:11:10 +0100

    Remove DS logic from attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,9 +1,7 @@
 class AttachmentsController < ApplicationController
   before_action :check_authorisation, if: :document_type_slug
 
-  layout :get_layout
-  DESIGN_SYSTEM_MIGRATED_ACTIONS = %w[new edit confirm_delete].freeze
-  include DesignSystemHelper
+  layout "design_system"
 
   def check_authorisation
     authorize current_format
@@ -12,7 +10,6 @@ class AttachmentsController < ApplicationController
   def new
     @document = fetch_document
     @attachment = Attachment.new
-    render design_system_view(:new, "attachments/legacy/new_legacy")
   end
 
   def create
@@ -31,7 +28,6 @@ class AttachmentsController < ApplicationController
   def edit
     @document = fetch_document
     @attachment = @document.attachments.find(attachment_content_id)
-    render design_system_view(:edit, "attachments/legacy/edit_legacy")
   end
 
   def update
