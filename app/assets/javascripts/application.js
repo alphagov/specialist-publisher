36d9cc1408448d8503dacd75299fc7c68e7264b0
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-08-06 20:34:59 +0100

    Add service-navigation imports - js and scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/add-another
 //= require govuk_publishing_components/components/select-with-search
+//= require govuk_publishing_components/components/service-navigation
 
 //= require components/govspeak-editor
 //= require components/paste-html-to-govspeak
