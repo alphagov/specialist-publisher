ac9726b4ed8d5e8b1375d8a4527098d69593f447
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-08-15 20:28:01 +0100

    Add additional logging
    - add succcesful logs
    - add error logs
    - add total error count
    
    The document `save` actually registers `:base` errors on the model for 422 errors from publishing-api, which will be handy to count. We will likely get some console logs from the request itself, but a custom log will be easier to read and search for.

--- spec/lib/tasks/mod_afc_import_from_csv_spec.rb
+++ spec/lib/tasks/mod_afc_import_from_csv_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 RSpec.describe "mod_afc_import_from_csv", type: :task do
   let!(:csv_path) { "dummy_path.csv" }
   let(:task) { Rake::Task["mod_afc_import_from_csv"] }
+  let(:publishing_api) { double("publishing-api") }
 
   before(:each) do
     task.reenable
   end
 
-  it "imports documents from a CSV file" do
+  it "imports documents from a CSV file, and logs successful import" do
     csv_data = [
       {
         "Account Name" => "ABC Company",
@@ -34,6 +35,8 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
     afc_business_double = instance_double(
       ArmedForcesCovenantBusiness,
       save: true,
+      errors: [],
+      title: csv_data.first["Account Name"],
     )
 
     generated_body = generate_body("ABC Company", csv_data.first)
@@ -51,10 +54,10 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
       "armed_forces_covenant_business_date_signed": "2025-06-01",
     ).and_return(afc_business_double)
 
-    expect { task.execute(csv_file_path: csv_path) }.to output(/Imported: 1 document\(s\).*Skipped: 0 row\(s\)/m).to_stdout
+    expect { task.execute(csv_file_path: csv_path) }.to output(/.*Saved document: 'ABC Company'.*REPORT.*Imported: 1 document\(s\).*Errors on save: 0 document\(s\).*Skipped: 0 row\(s\)/m).to_stdout
   end
 
-  it "imports documents in reverse order" do
+  it "imports documents in reverse order, and logs successful imports" do
     csv_data = [
       {
         "Account Name" => "ABC Company",
@@ -97,10 +100,19 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
     ]
     allow(CSV).to receive(:foreach).with(csv_path, headers: true).and_return(csv_data.each)
     attachments_double = double("attachments")
-    afc_business_double = instance_double(
+    afc_business_double_abc = instance_double(
       ArmedForcesCovenantBusiness,
       attachments: attachments_double,
       save: true,
+      errors: [],
+      title: "ABC Company",
+    )
+    afc_business_double_def = instance_double(
+      ArmedForcesCovenantBusiness,
+      attachments: attachments_double,
+      save: true,
+      errors: [],
+      title: "DEF Company",
     )
     expect(ArmedForcesCovenantBusiness).to receive(:new).with(
       "title": "DEF Company",
@@ -112,7 +124,7 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
       "armed_forces_covenant_business_industry": "transportation",
       "armed_forces_covenant_business_ownership": "non-profit",
       "armed_forces_covenant_business_date_signed": "2026-06-01",
-    ).and_return(afc_business_double).ordered
+    ).and_return(afc_business_double_def).ordered
     expect(ArmedForcesCovenantBusiness).to receive(:new).with(
       "title": "ABC Company",
       "summary": generate_summary,
@@ -123,9 +135,9 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
       "armed_forces_covenant_business_industry": "healthcare",
       "armed_forces_covenant_business_ownership": "private",
       "armed_forces_covenant_business_date_signed": "2025-06-01",
-    ).and_return(afc_business_double).ordered
+    ).and_return(afc_business_double_abc).ordered
 
-    expect { task.execute(csv_file_path: csv_path) }.to output(/Imported: 2 document\(s\).*Skipped: 0 row\(s\)/m).to_stdout
+    expect { task.execute(csv_file_path: csv_path) }.to output(/Saved document: 'DEF Company'.*Saved document: 'ABC Company'.*REPORT.*Imported: 2 document\(s\).*Errors on save: 0 document\(s\).*Skipped: 0 row\(s\)/m).to_stdout
   end
 
   ["Account Name", "Date AFC Signed", "Account Region", "Company Size", "Industry", "Ownership"]
@@ -171,6 +183,9 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
     afc_business_double = instance_double(
       ArmedForcesCovenantBusiness,
       attachments: attachments_double,
+      valid?: true,
+      errors: [],
+      title: csv_data.first["Account Name"],
     )
     allow(attachments_double).to receive(:build)
     allow(ArmedForcesCovenantBusiness).to receive(:new).and_return(afc_business_double)
@@ -218,11 +233,41 @@ RSpec.describe "mod_afc_import_from_csv", type: :task do
       },
     ]
     allow(CSV).to receive(:foreach).with(csv_path, headers: true).and_return(csv_data.each)
-    afc_business_double = double(save: true)
+    afc_business_double = double(save: true, valid?: true, errors: [], title: "Good Company")
     expect(ArmedForcesCovenantBusiness).to receive(:new).once.and_return(afc_business_double)
 
     expect {
       task.execute(csv_file_path: csv_path.to_s, dry_run: true)
     }.to output(/\[DRY RUN\] Imported: 1 document\(s\).*Skipped: 3 row\(s\).*Line 3: Missing title \(Title: N\/A\).*Line 4: Missing armed_forces_covenant_business_date_signed \(Title: ABC Company\).*Line 5: Missing armed_forces_covenant_business_company_size, armed_forces_covenant_business_industry, armed_forces_covenant_business_ownership \(Title: DEF Company\)/m).to_stdout
+  end
+
+  it "logs save errors and final error count" do
+    csv_data = [
+      {
+        "Account Name" => "ABC Company",
+        "Date AFC Signed" => "2025/06/01",
+        "Account Region" => "Wessex",
+        "Company Size" => "250-500 Large",
+        "Industry" => "Healthcare",
+        "Ownership" => "Private",
+        "Armed Forces Friendly" => "Pledged",
+        "UK Service Veterans And Leavers" => "Pledged",
+        "UK Service Spouses And Partners" => "0",
+        "Flexible Leave For Spouses" => "0",
+        "UK Reservists" => "0",
+        "UK Cadets" => "0",
+        "Armed Forces Day" => "0",
+        "Reserve Forces Day" => "0",
+        "Discounts	Armed Forces Charities" => "0",
+        "UK Wounded, Injured And Sick" => "0",
+        "Bespoke Pledges" => "-Custom Pledge text",
+      },
+    ]
+    allow(CSV).to receive(:foreach).with(csv_path, headers: true).and_return(csv_data.each)
+    stub_any_publishing_api_put_content.and_raise(GdsApi::HTTPErrorResponse.new(422, "Unprocessable Entity"))
+
+    expect {
+      task.execute(csv_file_path: csv_path.to_s, dry_run: false)
+    }.to output(/Error when saving document: 'ABC Company'.*REPORT.*Imported: 0 document\(s\).*Errors on save: 1 document\(s\).*Skipped: 0 row\(s\)/m).to_stdout
   end
 end
