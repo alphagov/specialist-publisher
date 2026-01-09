d22d51010ea480182e98eb65b7a5f7dac0a6399c
Tony McDonald <anthony.mcdonald@digital.cabinet-office.gov.uk>
2026-01-09 14:51:38 +0000

    Update date validation to match Publishing API
    This fixes an issue where a user had input a year (20206) that was outside the range specified by Publishing API (^[1-9][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[0-1])$ === 1000-01-01 to 9999-12-31). The validator has been updated to check the range for the year. Also the parse operation has been removed because the strptime function we use already performs stricter validation.
    
    The unit tests were also passing in the dates in the wrong format. The tests still passed by coincidence rather than intention, so these have also been fixed.

--- app/validators/date_validator.rb
+++ app/validators/date_validator.rb
@@ -2,8 +2,10 @@ class DateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    Date.parse(value)
-    Date.strptime(value, "%Y-%m-%d")
+    date = Date.strptime(value, "%Y-%m-%d")
+    unless date.year.between?(1000, 9999)
+      record.errors.add(attribute, "must be between year 1000 and 9999")
+    end
   rescue ArgumentError, RangeError
     record.errors.add(attribute, "is not a valid date")
   end
